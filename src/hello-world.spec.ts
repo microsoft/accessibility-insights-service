331df0d4f22c14f6f48eb2a5b7cf90cf068c256b
Devaradhan Murugesan <demuruge@microsoft.com>
2019-01-29 17:31:54 -0800

    remove sample test files

--- src/hello-world.spec.ts
+++
@@ -1,7 +0,0 @@
-import { message } from './hello-world';
-
-describe('Hello world', () => {
-    it('should expose message', async () => {
-        expect(message).toBe('hello world');
-    });
-});
