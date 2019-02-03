2d91a568bbf7048c33c9e597ab3f9967f3dbcf9e
Longhan Li <lonli@microsoft.com>
2019-02-03 11:26:08 -0800

    Added axe result conveter.

---
+++ src/scanner/scan-url/scan-result.ts
@@ -0,0 +1,80 @@
+export enum ProductType {
+    web,
+}
+
+export enum SourceName {
+    accessibility,
+}
+
+export interface Tool {
+    name: string;
+    fullName: string;
+    version: string;
+    semanticVersion: string;
+}
+
+export enum ResultLevel {
+    error,
+    pass,
+}
+
+export interface Product {
+    type: ProductType;
+    id: string;
+    serviceTreeId: string;
+    name: string;
+    baseUrl: string;
+    version: string;
+}
+
+export interface ScanInfo {
+    totalResultCount: number;
+    passedResultCount: number;
+    failedResultCount: number;
+}
+
+export interface Run {
+    version: string;
+    product: Product;
+    scanInfo: ScanInfo;
+    source: {
+        name: SourceName;
+    };
+    pipeline: {
+        name: string;
+    };
+}
+
+export interface PhysicalLocation {
+    fileLocation: {
+        uri: string;
+    };
+    region: {
+        snippet: {
+            text: string;
+        };
+    };
+}
+
+export interface Location {
+    physicalLocation: PhysicalLocation;
+    fullyQualifiedLogicalName: string;
+}
+
+export interface Result {
+    ruleId: string;
+    level: ResultLevel;
+    locations: Location[];
+    fingerprints: {
+        'value0/v1': string;
+    };
+}
+
+export interface ScanResult {
+    id: string;
+    lastUpdated: string;
+    productId: string;
+    tool: Tool;
+    run: Run;
+    result: Result;
+}
