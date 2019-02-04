dda2bba4d0fd2154339abf2551ddc8478615a468
devaradhanm <demuruge@microsoft.com>
2019-02-04 10:41:28 -0800

    Using custom container image for azure function (#9)
    * using docker image for azure functions
    Make webpack not bundle external dependencies.
    
    * fix failing tests & fix tslint issues

--- src/scanner/scan-url/index.ts
+++ src/scanner/scan-url/index.ts
@@ -1,10 +1,11 @@
 import { Context } from '@azure/functions';
 import * as Puppeteer from 'puppeteer';
-import { AxePuppeteerFactory } from './AxePuppeteerFactory';
 
+import { AxePuppeteerFactory } from './AxePuppeteerFactory';
 import { Scanner } from './scanner';
 
 export async function run(context: Context, url: string): Promise<void> {
-    await new Scanner(Puppeteer.launch.bind(Puppeteer), new AxePuppeteerFactory(), context).scan(url);
+    context.log(`starting scan ${url}`);
+    await new Scanner(Puppeteer, new AxePuppeteerFactory(), context).scan(url);
     context.log(`successfully scanned ${url}`);
 }
