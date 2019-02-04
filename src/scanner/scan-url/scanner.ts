288a39019bd38a34087edbd6d008fecd9fa2c8a2
Longhan Li <lonli@microsoft.com>
2019-02-03 16:51:07 -0800

    Included sha256 js module

--- src/scanner/scan-url/scanner.ts
+++ src/scanner/scan-url/scanner.ts
@@ -2,6 +2,10 @@ import { Context } from '@azure/functions';
 import { AxePuppeteer } from 'axe-puppeteer';
 import * as Puppeteer from 'puppeteer';
 import { AxePuppeteerFactory } from './AxePuppeteerFactory';
+import { ResultConverter } from './result-converter';
+import { Product, ProductType } from './scan-result';
+//import * as fs from 'fs';
+import * as sha256 from './sha256';
 
 export class Scanner {
     constructor(
@@ -20,10 +24,33 @@ export class Scanner {
 
         await page.goto(url);
         const axePuppeteer: AxePuppeteer = this.axePuppeteerFactory.getInstance(page);
-        const results = await axePuppeteer.analyze();
-        this.context.log(results);
+        const axeResults = await axePuppeteer.analyze();
 
+        const results = new ResultConverter().convert(axeResults, this.buildFakeProductInfo(url));
+        this.context.log(results.length);
+        // tslint:disable-next-line:no-any
+        // fs.writeFile('./convertedResult.json', JSON.stringify(results), (err: any) => {
+        //     if (err) {
+        //         this.context.log(err);
+
+        //         return;
+        //     }
+        //     this.context.log('Converted File has been created');
+        // });
+
+        this.context.log('output from sha256: ' + sha256.computeHash('abcdefg'));
         await page.close();
         await browser.close();
+    }
+
+    private buildFakeProductInfo(url: string): Product {
+        return {
+            type: ProductType.web,
+            id: 'product id',
+            serviceTreeId: 'serviceTree id',
+            name: 'product name',
+            baseUrl: url,
+            version: 'product version',
+        };
     }
 }
