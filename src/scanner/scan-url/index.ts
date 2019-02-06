74cfa60ce04ffe462c10b9f5a28d9fed9b9a73b8
Longhan Li <lonli@microsoft.com>
2019-02-06 15:21:16 -0800

    Update index.ts to use hash-id-generator

--- src/scanner/scan-url/index.ts
+++ src/scanner/scan-url/index.ts
@@ -3,6 +3,7 @@ import * as Puppeteer from 'puppeteer';
 
 import * as shaJs from 'sha.js';
 import { AxePuppeteerFactory } from './axe-puppeteer-factory';
+import { HashIdGenerator } from './hash-id-generator';
 import { IssueFinder } from './issue-finder';
 import { ResultConverter } from './result-converter';
 import { Scanner } from './scanner';
@@ -11,7 +12,7 @@ export async function run(context: Context, url: string): Promise<void> {
     context.log(`starting scan ${url}`);
     const issueFinder: IssueFinder = new IssueFinder(
         new Scanner(Puppeteer, new AxePuppeteerFactory()),
-        new ResultConverter(shaJs('sha256')),
+        new ResultConverter(new HashIdGenerator(shaJs('sha256'))),
         context,
     );
     await issueFinder.findIssues(url);
