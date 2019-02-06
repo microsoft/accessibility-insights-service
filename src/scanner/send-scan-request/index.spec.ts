8c2465566eb8dd0941befd43d6c4bd25d9a212a0
Manish Satwani <masatwan@microsoft.com>
2019-02-06 13:33:23 -0800

    fromatting

--- src/scanner/send-scan-request/index.spec.ts
+++ src/scanner/send-scan-request/index.spec.ts
@@ -6,25 +6,27 @@ describe('SendScanRequest', () => {
     let testData: ScanRequest[];
     beforeEach(() => {
         //tslint:disable-next-line: no-object-literal-type-assertion no-any no-empty
-        contextStub = { bindings: {}, log: (() => { }) as any } as Context;
-        testData = [{
-            id: '123',
-            count: 68,
-            websites: [
-                {
-                    id: '7113152f-4d38-4443-a0d8-a07a6aab32f9',
-                    name: 'Azure',
-                    baseUrl: 'https://azure.microsoft.com/',
-                    serviceTreeId: '7113152f-4d38-4443-a0d8-a07a5aab32f9'
-                },
-                {
-                    id: '7113152f-4d38-4443-a0d8-b07a6aab32f9',
-                    name: 'Channel9',
-                    baseUrl: '"https://channel9.msdn.com/',
-                    serviceTreeId: '7113152f-4d38-4443-v0d8-a07a5aab32f9'
-                }
-            ]
-        }];
+        contextStub = { bindings: {}, log: (() => {}) as any } as Context;
+        testData = [
+            {
+                id: '123',
+                count: 68,
+                websites: [
+                    {
+                        id: '7113152f-4d38-4443-a0d8-a07a6aab32f9',
+                        name: 'Azure',
+                        baseUrl: 'https://azure.microsoft.com/',
+                        serviceTreeId: '7113152f-4d38-4443-a0d8-a07a5aab32f9',
+                    },
+                    {
+                        id: '7113152f-4d38-4443-a0d8-b07a6aab32f9',
+                        name: 'Channel9',
+                        baseUrl: '"https://channel9.msdn.com/',
+                        serviceTreeId: '7113152f-4d38-4443-v0d8-a07a5aab32f9',
+                    },
+                ],
+            },
+        ];
     });
 
     it('should send crawl requests', async () => {
@@ -33,14 +35,14 @@ describe('SendScanRequest', () => {
                 id: '7113152f-4d38-4443-a0d8-a07a6aab32f9',
                 name: 'Azure',
                 baseUrl: 'https://azure.microsoft.com/',
-                serviceTreeId: '7113152f-4d38-4443-a0d8-a07a5aab32f9'
+                serviceTreeId: '7113152f-4d38-4443-a0d8-a07a5aab32f9',
             },
             {
                 id: '7113152f-4d38-4443-a0d8-b07a6aab32f9',
                 name: 'Channel9',
                 baseUrl: '"https://channel9.msdn.com/',
-                serviceTreeId: '7113152f-4d38-4443-v0d8-a07a5aab32f9'
-            }
+                serviceTreeId: '7113152f-4d38-4443-v0d8-a07a5aab32f9',
+            },
         ] as WebSite[];
         run(contextStub, undefined, testData);
         expect(contextStub.bindings.outputQueueItem).toBeDefined();
