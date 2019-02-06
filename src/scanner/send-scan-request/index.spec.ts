2516b92efda27ff68e34022c0392dfd27886a78b
Manish Satwani <masatwan@microsoft.com>
2019-02-06 13:38:30 -0800

    refactored.

--- src/scanner/send-scan-request/index.spec.ts
+++ src/scanner/send-scan-request/index.spec.ts
@@ -11,26 +11,21 @@ describe('SendScanRequest', () => {
             {
                 id: '123',
                 count: 68,
-                websites: [
-                    {
-                        id: '7113152f-4d38-4443-a0d8-a07a6aab32f9',
-                        name: 'Azure',
-                        baseUrl: 'https://azure.microsoft.com/',
-                        serviceTreeId: '7113152f-4d38-4443-a0d8-a07a5aab32f9',
-                    },
-                    {
-                        id: '7113152f-4d38-4443-a0d8-b07a6aab32f9',
-                        name: 'Channel9',
-                        baseUrl: '"https://channel9.msdn.com/',
-                        serviceTreeId: '7113152f-4d38-4443-v0d8-a07a5aab32f9',
-                    },
-                ],
+                websites: getWebSitesData(),
             },
         ];
     });
 
     it('should send crawl requests', async () => {
-        const websites = [
+        const websites = getWebSitesData();
+
+        run(contextStub, undefined, testData);
+
+        expect(contextStub.bindings.outputQueueItem).toEqual(websites);
+    });
+
+    function getWebSitesData(): WebSite[] {
+        return [
             {
                 id: '7113152f-4d38-4443-a0d8-a07a6aab32f9',
                 name: 'Azure',
@@ -44,8 +39,5 @@ describe('SendScanRequest', () => {
                 serviceTreeId: '7113152f-4d38-4443-v0d8-a07a5aab32f9',
             },
         ] as WebSite[];
-        run(contextStub, undefined, testData);
-        expect(contextStub.bindings.outputQueueItem).toBeDefined();
-        expect(contextStub.bindings.outputQueueItem).toEqual(websites);
-    });
+    }
 });
