2cd2df5d7fc133141fc993fe777bff2ac7f454c0
Devaradhan Murugesan <demuruge@microsoft.com>
2019-02-06 17:57:24 -0800

    adding support to build image in PR & push image in CI

--- src/scanner/dev-scripts/build-function-image.js
+++ src/scanner/dev-scripts/build-function-image.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const shellJs = require('shelljs');
 const shellUtils = require('./shell-utils');
+const fs = require('fs');
 
 const currentDir = path.resolve(__dirname);
 const scannerDir = path.resolve(currentDir, '../');
@@ -10,15 +11,17 @@ shellJs.pushd(process.cwd());
 try {
     shellUtils.echoCommentBanner('install function dependencies');
     shellJs.cd(path.resolve(scannerDir, 'dist'));
-    shellUtils.executeCommand('func extensions install');
+    shellUtils.executeCommand('npx func extensions install');
 
     shellUtils.echoCommentBanner('building base image');
     shellJs.cd('../');
     shellUtils.executeCommand(`docker build -t scanner .`);
 
-    shellUtils.echoCommentBanner('building dev image');
-    shellJs.cd('dev-docker');
-    shellUtils.executeCommand(`docker build -t scanner-dev -f dev.DockerFile .`);
+    if (fs.existsSync('./dev-docker/dev.DockerFile')) {
+        shellUtils.echoCommentBanner('building dev image');
+        shellJs.cd('dev-docker');
+        shellUtils.executeCommand(`docker build -t scanner-dev -f dev.DockerFile .`);
+    }
 } finally {
     shellJs.popd();
 }
