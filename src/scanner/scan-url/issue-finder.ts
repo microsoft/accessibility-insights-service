c3445a6b432b7224599bd2dc8253cc6d4204772a
Longhan Li <lonli@microsoft.com>
2019-02-06 16:50:04 -0800

    Get meta data from azure function input and save them in cosmos db.

--- src/scanner/scan-url/issue-finder.ts
+++ src/scanner/scan-url/issue-finder.ts
@@ -1,5 +1,6 @@
 import { Context } from '@azure/functions';
 import { AxeResults } from 'axe-core';
+import { ScanRequest } from '../crawl-url/simple-crawler';
 import { ResultConverter } from './result-converter';
 import { ScanResult } from './scan-result';
 import { Scanner } from './scanner';
@@ -7,10 +8,10 @@ import { Scanner } from './scanner';
 export class IssueFinder {
     constructor(private readonly scanner: Scanner, private readonly resultConverter: ResultConverter, private readonly context: Context) {}
 
-    public async findIssues(url: string): Promise<void> {
-        const axeResults: AxeResults = await this.scanner.scan(url);
+    public async findIssues(request: ScanRequest): Promise<void> {
+        const axeResults: AxeResults = await this.scanner.scan(request.scanUrl);
         this.context.log(`axe results count ${axeResults.violations.length}.`);
-        const scanResults: ScanResult[] = this.resultConverter.convert(axeResults);
+        const scanResults: ScanResult[] = this.resultConverter.convert(axeResults, request);
         this.context.log(`converted results count ${scanResults.length}.`);
         this.context.bindings.scanIssues = JSON.stringify(scanResults);
     }
