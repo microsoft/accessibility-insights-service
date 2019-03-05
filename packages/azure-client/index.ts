64cf25ff2f492a3cbf560cc43e6fdfbafefef7d7
Maxim Laikine <mlaikine@microsoft.com>
2019-03-05 00:39:01 -0800

    Batch job manager with storage queue feed

--- packages/azure-client/index.ts
+++ packages/azure-client/index.ts
@@ -4,8 +4,8 @@ import { CosmosClientWrapper } from './cosmos-client-wrapper';
 export { CosmosClientWrapper } from './cosmos-client-wrapper';
 
 export function createDefaultCosmosClientWrapper(): CosmosClientWrapper {
-    const endpoint = process.env.cosmosdbEndpoint;
-    const masterKey = process.env.cosmosdbMasterKey;
+    const endpoint = process.env.AZURE_COSMOS_DB_URL;
+    const masterKey = process.env.AZURE_COSMOS_DB_KEY;
     const client = new cosmos.CosmosClient({ endpoint, auth: { masterKey } });
 
     return new CosmosClientWrapper(client);
