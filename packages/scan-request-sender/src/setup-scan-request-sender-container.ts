7b1cdc237c5aa7bb94e91352310d68d6d69289bb
Devaradhan Murugesan <demuruge@microsoft.com>
2019-04-25 16:34:02 -0700

    using logger in scan-request-sender

--- packages/scan-request-sender/src/setup-scan-request-sender-container.ts
+++ packages/scan-request-sender/src/setup-scan-request-sender-container.ts
@@ -2,11 +2,13 @@ import 'reflect-metadata';
 
 import { CosmosClientWrapper, registerAxisStorageToContainer, StorageClient } from 'axis-storage';
 import { Container } from 'inversify';
+import { registerLoggerToContainer } from 'logger';
 import { ScanRequestSender } from './sender/request-sender';
 import { SeedSource } from './source/seed-source';
 
 export function setupScanRequestSenderContainer(): Container {
     const container = new Container();
+    registerLoggerToContainer(container);
     registerAxisStorageToContainer(container);
 
     container.bind(StorageClient).toDynamicValue(context => {
