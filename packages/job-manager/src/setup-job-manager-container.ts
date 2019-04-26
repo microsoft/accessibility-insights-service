f05432af05259c02e4c32343ed73b0d16b364268
Devaradhan Murugesan <demuruge@microsoft.com>
2019-04-25 17:24:22 -0700

    fix build failure

--- packages/job-manager/src/setup-job-manager-container.ts
+++ packages/job-manager/src/setup-job-manager-container.ts
@@ -3,12 +3,14 @@ import 'reflect-metadata';
 import { registerAxisStorageToContainer } from 'axis-storage';
 import { ServiceClient, SharedKeyCredentials } from 'azure-batch';
 import * as inversify from 'inversify';
+import { registerLoggerToContainer } from 'logger';
 import { Batch } from './batch/batch';
 import { BatchConfig } from './batch/batch-config';
 import { TaskParameterBuilder } from './batch/task-parameter-builder';
 
 export function setupJobManagerContainer(): inversify.Container {
     const container = new inversify.Container();
+    registerLoggerToContainer(container);
     registerAxisStorageToContainer(container);
 
     container
