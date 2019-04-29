64515254c05c1efb668ff3c0ad1d3d285d2e6f10
Devaradhan Murugesan <demuruge@microsoft.com>
2019-04-29 12:31:03 -0700

    adding logs in scripts

--- packages/job-manager/run-script/run-job-manager.sh
+++ packages/job-manager/run-script/run-job-manager.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 export NODE_PATH=$AZ_BATCH_NODE_SHARED_DIR/batch-job-manager/node_modules
-echo "Running job manager"
+
+echo "Installing dependencies"
 npm install yargs@13.2.1
+
+echo "Running job manager"
 nodejs index.js
--- packages/scan-request-sender/run-script/run-scan-request-sender.sh
+++ packages/scan-request-sender/run-script/run-scan-request-sender.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 export NODE_PATH=$AZ_BATCH_NODE_SHARED_DIR/batch-scan-request-sender/node_modules
+
 echo "Running scan request sender"
 npm install yargs@13.2.1
+
+echo "Running Scan request sender"
 nodejs sender.js
