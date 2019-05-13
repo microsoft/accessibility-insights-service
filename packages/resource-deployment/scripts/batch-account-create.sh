cfecaf455d0731c66fd279607293c09fb41f0d8a
Maxim Laikine <mlaikine@microsoft.com>
2019-05-13 13:52:45 -0700

    Get Azure batch account name from deployment

--- packages/resource-deployment/scripts/batch-account-create.sh
+++ packages/resource-deployment/scripts/batch-account-create.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -eo pipefail
-IFS=$'\n\t'
+
+# This script will deploy Azure Batch account in user subscription mode and
+# enable managed identity for Azure on Batch pools
 
 # Set default ARM Batch account template files
 batchTemplateFile="./templates/batch-account.template.json"
 batchTemplateParametersFile="./templates/batch-account.parameters.json"
 
 # Read script arguments
-while getopts "s:r:a:p:k:" option; do
+while getopts "s:r:k:t:p:" option; do
 case $option in
     s) subscription=${OPTARG};;
     r) resourceGroup=${OPTARG};;
-    a) account=${OPTARG};;
     k) keyVault=${OPTARG};;
     t) batchTemplateFile=${OPTARG};;
     p) batchTemplateParametersFile=${OPTARG};;
@@ -19,10 +20,10 @@ esac
 done
 
 # Print script usage help
-if [[ -z $subscription ]] || [[ -z $resourceGroup ]]  || [[ -z $account ]] || [[ -z $keyVault ]]; then
+if [[ -z $subscription ]] || [[ -z $resourceGroup ]]  || [[ -z $keyVault ]]; then
     echo \
 "
-Usage: $0 -s <subscription> -r <resource group> -a <batch account> -k <key vault> [-t <batch template file>] [-p <batch template parameters file>]
+Usage: $0 -s <subscription> -r <resource group> -k <key vault> [-t <batch template file>] [-p <batch template parameters file>]
 
 Prerequisites:
     Azure Key Vault
@@ -43,10 +44,32 @@ az account set --subscription $subscription
 # Configure Azure subscription account to support Batch in user subscription mode
 source ${0%/*}/account-set-batch-app.sh
 
-echo "Creating '$account' Azure Batch account"
-az group deployment create --resource-group $resourceGroup --template-file $batchTemplateFile --parameters $batchTemplateParametersFile 1> /dev/null
+# Deploy Azure Batch account using resource manager template
+echo "Deploying Azure Batch account"
+resources=$(az group deployment create \
+    --resource-group $resourceGroup \
+    --template-file $batchTemplateFile \
+    --parameters $batchTemplateParametersFile \
+    --query "properties.outputResources[].id" \
+    -o tsv)
 
-# Login into '$account' Azure Batch account
+# Get batch account name
+re="^/subscriptions/$subscription/resourceGroups/$resourceGroup/providers/Microsoft.Batch/batchAccounts/(.[^/]+)"
+for resource in $resources
+do
+    if [[ $resource =~ $re ]]; then
+        account="${BASH_REMATCH[1]}"
+        break
+    fi
+done
+
+if [[ -z $account ]]; then
+    echo "ERROR: The $batchTemplateFile ARM template deployment return no Batch account resource id."
+fi
+
+echo "The '$account' Azure Batch account deployed successfully"
+
+# Login into Azure Batch account
 echo "Logging into '$account' Azure Batch account"
 az batch account login --name $account --resource-group $resourceGroup
 
@@ -54,7 +77,7 @@ export vmssResourceGroup
 export vmssName
 export systemAssignedIdentity
 
-# Enable managed identity for all Batch pools
+# Enable managed identity on Batch pools
 pools=$(az batch pool list --query "[].id" -o tsv)
 for pool in $pools
 do
--- packages/resource-deployment/scripts/key-vault-enable-msi.sh
+++ packages/resource-deployment/scripts/key-vault-enable-msi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script will grant permissions to the system-assigned managed identity to access key vault
+# This script will grant permissions to the managed identity to access key vault
 
 if [[ -z $keyVault ]] || [[ -z $systemAssignedIdentity ]]; then
     echo \
@@ -13,6 +13,6 @@ The $0 script expects following variables to be defined:
     exit 1
 fi
 
-# Grant permissions to the VMSS system-assigned managed identity
+# Grant permissions to the managed identity
 echo "Granting '$systemAssignedIdentity' managed identity permissions to '$keyVault' key vault"
 az keyvault set-policy --name $keyVault --object-id $systemAssignedIdentity --secret-permissions get list 1> /dev/null
