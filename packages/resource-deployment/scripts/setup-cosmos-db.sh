d44a8e5085141daed0048a8f2898b73a9472d3d1
devaradhanm <demuruge@microsoft.com>
2019-05-20 22:50:13 -0700

    Adding scripts to create cosmos resources & queues (#112)
    * adding script to create queues
    
    * adding script to create cosmosdb resources

---
+++ packages/resource-deployment/scripts/setup-cosmos-db.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -eo pipefail
+
+createCosmosCollection() {
+    collectionName=$1
+    dbName=$2
+    cosmosAccountName=$3
+    resourceGroupName=$4
+
+    echo "Checking if collection '$collectionName' exists in db '$dbName' of cosmosAccount '$cosmosAccountName' in resource group '$resourceGroupName'"
+    collectionExists=$(az cosmosdb collection exists --collection-name "$collectionName" --db-name "$dbName" --name "$cosmosAccountName" --resource-group-name "$resourceGroupName")
+
+    if [ "$collectionExists" = true ]; then
+        echo "Collection '$collectionName' already exists"
+    else
+        az cosmosdb collection create --collection-name "$collectionName" --db-name "$dbName" --name "$cosmosAccountName" --resource-group-name "$resourceGroupName" --partition-key-path "/partitionKey" --throughput 10000
+        echo "Successfully created collection '$collectionName'"
+    fi
+}
+
+createCosmosDatabase() {
+    dbName=$1
+    cosmosAccountName=$2
+    resourceGroupName=$3
+
+    echo "Checking if database '$dbName' exists in cosmosAccount '$cosmosAccountName' in resource group '$resourceGroupName'"
+    databaseExists=$(az cosmosdb database exists --db-name "$dbName" --name "$cosmosAccountName" --resource-group-name "$resourceGroupName")
+
+    if [ "$databaseExists" = true ]; then
+        echo "Database $dbName already exists"
+    else
+        az cosmosdb database create --db-name "$dbName" --name "$cosmosAccountName" --resource-group-name "$resourceGroupName"
+        echo "Successfully created database $dbName"
+    fi
+}
+
+exitWithUsageInfo() {
+    echo \
+        "
+Usage: $0 -a <cosmosAccountName> -r <resource group>
+"
+    exit 1
+}
+
+# Read script arguments
+while getopts "a:r:" option; do
+    case $option in
+    a) cosmosAccountName=${OPTARG} ;;
+    r) resourceGroupName=${OPTARG} ;;
+    *) exitWithUsageInfo ;;
+    esac
+done
+
+# Print script usage help
+if [[ -z $cosmosAccountName ]] || [[ -z $resourceGroupName ]]; then
+    exitWithUsageInfo
+fi
+
+dbName="scanner"
+createCosmosDatabase "$dbName" "$cosmosAccountName" "$resourceGroupName"
+createCosmosCollection "a11yIssues" "$dbName" "$cosmosAccountName" "$resourceGroupName"
+createCosmosCollection "webPagesToScan" "$dbName" "$cosmosAccountName" "$resourceGroupName"
