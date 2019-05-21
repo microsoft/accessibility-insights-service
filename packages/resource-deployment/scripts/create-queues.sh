834a7838ac4ddd976b47d1e626e79dccadc2851c
Devaradhan Murugesan <demuruge@microsoft.com>
2019-05-20 19:07:30 -0700

    Addressing PR comments

--- packages/resource-deployment/scripts/create-queues.sh
+++ packages/resource-deployment/scripts/create-queues.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 set -eo pipefail
 
-# shellcheck disable=SC1091
-. './utilities/create-queue.sh'
+createQueue() {
+    queue=$1
+    storageAccountName=$2
+
+    echo "Checking if queue $queue exists in storage account $storageAccountName"
+    queueExists=$(az storage queue exists --name "$queue" --account-name "$storageAccountName" --query "exists")
+
+    if [ "$queueExists" = true ]; then
+        echo "Queue '$queue' already exists"
+    else
+        az storage queue create --name "$queue" --account-name "$storageAccountName"
+        echo "Successfully created queue '$queue'"
+    fi
+}
 
 exitWithUsageInfo() {
     echo \
--- packages/resource-deployment/scripts/setup-cosmos-db.sh
+++ packages/resource-deployment/scripts/setup-cosmos-db.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 set -eo pipefail
 
-# shellcheck disable=SC1091
-. './utilities/cosmos-utilities.sh'
+createCosmosCollection() {
+    collectionName=$1
+    dbName=$2
+    cosmosAccountName=$3
+    resourceGroupName=$4
+
+    echo "Checking if collection '$collectionName' exists in db '$dbName' of cosmosAccount '$cosmosAccountName' in resource group '$resourceGroupName'"
+    collectionExists=$(az cosmosdb collection exists --collection-name "$collectionName" --db-name "$dbName" --name "$cosmosAccountName" --resource-group-name "$resourceGroupName")
+
+    if [ "$collectionExists" = true ]; then
+        echo "Collection '$collectionName' already exists"
+    else
+        az cosmosdb collection create --collection-name "$collectionName" --db-name "$dbName" --name "$cosmosAccountName" --resource-group-name "$resourceGroupName" --partition-key-path "/partitionKey" --throughput 10000
+        echo "Successfully created collection '$collectionName'"
+    fi
+}
+
+createCosmosDatabase() {
+    dbName=$1
+    cosmosAccountName=$2
+    resourceGroupName=$3
+
+    echo "Checking if database '$dbName' exists in cosmosAccount '$cosmosAccountName' in resource group '$resourceGroupName'"
+    databaseExists=$(az cosmosdb database exists --db-name "$dbName" --name "$cosmosAccountName" --resource-group-name "$resourceGroupName")
+
+    if [ "$databaseExists" = true ]; then
+        echo "Database $dbName already exists"
+    else
+        az cosmosdb database create --db-name "$dbName" --name "$cosmosAccountName" --resource-group-name "$resourceGroupName"
+        echo "Successfully created database $dbName"
+    fi
+}
 
 exitWithUsageInfo() {
     echo \
@@ -13,9 +43,9 @@ Usage: $0 -c <cosmosAccountName> -r <resource group>
 }
 
 # Read script arguments
-while getopts "c:r:" option; do
+while getopts "a:r:" option; do
     case $option in
-    c) cosmosAccountName=${OPTARG} ;;
+    a) cosmosAccountName=${OPTARG} ;;
     r) resourceGroupName=${OPTARG} ;;
     *) exitWithUsageInfo ;;
     esac
