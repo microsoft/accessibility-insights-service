02fb9edd717741f29b34e6f3a12bef9146ad2d6f
Devaradhan Murugesan <demuruge@microsoft.com>
2019-05-23 18:10:32 -0700

    addressing PR comments

--- packages/resource-deployment/scripts/app-insights-create.sh
+++ packages/resource-deployment/scripts/app-insights-create.sh
@@ -14,7 +14,7 @@ Usage: $0 -r <resource group> -s <subscription name or id>
 }
 
 # Read script arguments
-while getopts "r:s:l:" option; do
+while getopts "r:s:" option; do
     case $option in
     r) resourceGroupName=${OPTARG} ;;
     s) subscription=${OPTARG} ;;
@@ -22,8 +22,10 @@ while getopts "r:s:l:" option; do
     esac
 done
 
+echo "Installing application-insights extension for azure-cli"
 az extension add -n application-insights
 
+echo "Creating Application Insights resource using ARM template"
 export resourceName
 resources=$(az group deployment create \
     --subscription "$subscription" \
@@ -34,6 +36,7 @@ resources=$(az group deployment create \
 
 . "${0%/*}/get-resource-name-from-resource-paths.sh" -p "Microsoft.insights/components" -r "$resources"
 appInsightsName=$resourceName
+echo "Successfully created application insights $appInsightsName"
 
 appInsightsKey=$(az monitor app-insights component show --app "$appInsightsName" --resource-group "$resourceGroupName" --query "instrumentationKey" -o tsv)
 echo "App Insights Key fetched - $appInsightsKey"
