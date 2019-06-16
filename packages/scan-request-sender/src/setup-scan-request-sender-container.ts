685f0dcfd6f7d443708db74d7cdc3fffea9e9ca9
Manish Satwani <manishsat@users.noreply.github.com>
2019-06-15 18:43:26 -0700

    Added dispatcher to dispatch requests to scan in the runner. (#156)
    * Added dispatcher to dispatch requests to scan in the runner.
    
    * Fixed comments. Added runTime is both the cases.
    
    * Changed the log message, when queue reached to capacity.

--- packages/scan-request-sender/src/setup-scan-request-sender-container.ts
+++ packages/scan-request-sender/src/setup-scan-request-sender-container.ts
@@ -1,22 +1,17 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { CosmosClientWrapper, registerAzureServicesToContainer, StorageClient } from 'azure-services';
-import { Container } from 'inversify';
+import * as inversify from 'inversify';
 import { Logger, registerLoggerToContainer } from 'logger';
-import { ScanRequestSender } from './sender/request-sender';
-import { SeedSource } from './source/seed-source';
 
-export function setupScanRequestSenderContainer(): Container {
-    const container = new Container();
+export function setupScanRequestSenderContainer(): inversify.Container {
+    const container = new inversify.Container({ autoBindInjectable: true });
     registerLoggerToContainer(container);
     registerAzureServicesToContainer(container);
 
     container.bind(StorageClient).toDynamicValue(context => {
-        return new StorageClient(context.container.get(CosmosClientWrapper), 'scanner', 'webPagesToScan', context.container.get(Logger));
+        return new StorageClient(context.container.get(CosmosClientWrapper), 'scanner', 'a11yIssues', context.container.get(Logger));
     });
-
-    container.bind(SeedSource).toSelf();
-    container.bind(ScanRequestSender).toSelf();
 
     return container;
 }
