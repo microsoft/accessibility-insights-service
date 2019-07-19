941295543ff80c8fd137f1bd75570d7834fc15b4
Manish Satwani <masatwan@microsoft.com>
2019-07-19 15:10:40 -0700

    Separated out AxePuppeteerFactory so we can disable rules at central place.

---
+++ packages/runner/src/factories/axe-puppeteer-factory.spec.ts
@@ -0,0 +1,18 @@
+import 'reflect-metadata';
+
+import * as Puppeteer from 'puppeteer';
+import { IMock, Mock } from 'typemoq';
+import { AxePuppeteerFactory } from './axe-puppeteer-factory';
+
+describe('AxePuppeteerFactory', () => {
+    let page: IMock<Puppeteer.Page>;
+    let testSubject: AxePuppeteerFactory;
+    beforeEach(() => {
+        page = Mock.ofType<Puppeteer.Page>();
+        testSubject = new AxePuppeteerFactory();
+    });
+    it('create axe puppeteer instance', () => {
+        const axePuppeteer = testSubject.createAxePuppteteer(page.object);
+        expect(axePuppeteer).toBeDefined();
+    });
+});
