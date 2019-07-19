500b7a40b8446912e735bda6f3576ae679145387
Haonan Liu <haonliu@microsoft.com>
2019-07-19 14:52:47 -0700

    change profile path

--- packages/common/src/configuration/service-configuration.ts
+++ packages/common/src/configuration/service-configuration.ts
@@ -34,12 +34,12 @@ export interface RuntimeConfig {
 
 @injectable()
 export class ServiceConfiguration {
-    public static readonly profilePath = './runtime-config/runtime-config.json';
+    public static readonly profilePath = `${__dirname}/runtime-config.json`;
     private readonly fileSystem: typeof fs;
     private loadConfigPromise: Promise<convict.Config<RuntimeConfig>>;
     private readonly convictModule: typeof convict;
 
-    constructor(fileSystem: typeof fs = fs, convictModule: typeof convict) {
+    constructor(fileSystem: typeof fs = fs, convictModule: typeof convict = convict) {
         this.fileSystem = fileSystem;
         this.convictModule = convictModule;
     }
