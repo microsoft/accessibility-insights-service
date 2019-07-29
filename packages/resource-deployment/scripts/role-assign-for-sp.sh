f04349a65eff36505496badce6a1798a838befb2
Liu Haonan <haonliu@microsoft.com>
2019-07-29 11:03:40 -0700

    fix bug in role assign script (#218)
    * fix bug in role assign script
    
    * fix indentation
    
    * fix the command
    
    * add log
    
    * remove retry count
    
    * revert log

--- packages/resource-deployment/scripts/role-assign-for-sp.sh
+++ packages/resource-deployment/scripts/role-assign-for-sp.sh
@@ -33,21 +33,23 @@ fi
 echo "Granting role '$role' to the resource group '$resourceGroupName' for service principal '$principalId'"
 
 end=$((SECONDS + 300))
+
 printf " - Running .."
+
 while [ $SECONDS -le $end ]; do
-    az role assignment create --role "$role" --resource-group "$resourceGroupName" --assignee-object-id "$principalId" 1>/dev/null
-    principalIdResponse=$(az role assignment list --resource-group "$resourceGroupName" --assignee "$principalId" --query "[?principalId=='$principalId'].principalId" -o tsv)
+    response=$(az role assignment create --role "$role" --resource-group "$resourceGroupName" --assignee-object-id "$principalId" --query "roleDefinitionId") || true
 
-    if [[ $principalIdResponse == "$principalId" ]]; then
-        break
-    fi
-
+    if [[ -n $response ]]; then	
+        break	
+    else	
+        printf "."
+    fi	
+    
     sleep 5
-    printf "."
 done
 echo "  ended"
 
-if [[ $principalIdResponse != "$principalId" ]]; then
+if [[ -z $response ]]; then
     echo "Unable to create role assignment '$role' for service principal '$principalId'"
 
     exit 1
