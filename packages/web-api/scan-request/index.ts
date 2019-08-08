1298661ab73de3714854ef11e3a7e75270d51cb4
Manish Satwani <masatwan@microsoft.com>
2019-08-08 16:12:59 -0700

    copy right info

--- packages/web-api/scan-request/index.ts
+++ packages/web-api/scan-request/index.ts
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 import { Context } from '@azure/functions';
 import { ScanRequest } from './scan-request-type';
 export async function run(context: Context, scanRequest: ScanRequest): Promise<void> {
