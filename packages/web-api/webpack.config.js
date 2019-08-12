9c36170ca993ff9561e78536cd9a3fb86afdb55f
Manish Satwani <manishsat@users.noreply.github.com>
2019-08-12 11:24:24 -0700

    First sketch for scan request function (#231)
    * Basic skeleton for scan reqiest function.
    
    * webpack changes
    
    * copy right info
    
    * excluded web-api package
    
    * comments incorporated
    
    * added http methods supported by function.

---
+++ packages/web-api/webpack.config.js
@@ -0,0 +1,79 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+const path = require('path');
+const webpack = require('webpack');
+
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const copyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = env => {
+    const version = env ? env.version : 'dev';
+    console.log(`Building for version : ${version}`);
+
+    return {
+        devtool: 'cheap-source-map',
+        externals: [],
+        entry: {
+            ['scan-request']: path.resolve('./scan-request/index.ts'),
+        },
+        mode: 'development',
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: [
+                        {
+                            loader: 'ts-loader',
+                            options: {
+                                transpileOnly: true,
+                                experimentalWatchApi: true,
+                            },
+                        },
+                    ],
+                    exclude: ['/node_modules/', /\.(spec|e2e)\.ts$/],
+                },
+            ],
+        },
+        name: 'web-api',
+        node: {
+            __dirname: false,
+        },
+        output: {
+            path: path.resolve('./dist'),
+            filename: '[name]/index.js',
+            libraryTarget: 'commonjs2',
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                __IMAGE_VERSION__: JSON.stringify(version),
+            }),
+            new ForkTsCheckerWebpackPlugin(),
+            new copyWebpackPlugin([
+                {
+                    context: './run-script',
+                    from: '**/*.sh',
+                    to: '',
+                    ignore: ['dist/**', 'node_modules/**'],
+                },
+                {
+                    context: './',
+                    from: '**/function.json',
+                    to: '',
+                    ignore: ['dist/**'],
+                },
+                {
+                    from: 'package.json',
+                    to: '',
+                },
+                {
+                    from: '../../yarn.lock',
+                    to: '',
+                },
+            ]),
+        ],
+        resolve: {
+            extensions: ['.ts', '.js', '.json'],
+        },
+        target: 'node',
+    };
+};
