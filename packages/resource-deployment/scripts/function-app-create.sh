36b993007ff3f4ee0cdbcbb7df7278d4ca5b6c30
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2019-08-23 21:14:04 -0700

    address comments

--- packages/resource-deployment/scripts/function-app-create.sh
+++ packages/resource-deployment/scripts/function-app-create.sh
@@ -3,60 +3,38 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-set -euo pipefail
+# shellcheck disable=SC1090
+set -eo pipefail
 
-export subscription=""
 export resourceGroupName=""
-export location=""
 export functionAppName
-export appInsightsName
-export storageAccountName
+export resourceName
 
 exitWithUsageInfo() {
     echo "
-Usage: $0 -s <subscription name or id> -r <resource group> -l <location>
+Usage: $0 -r <resource group>
 "
     exit 1
 }
 
 # Set default ARM Function App template files
-templateFilePath="../templates/function-app-template.json"
+templateFilePath="${0%/*}/../templates/function-app-template.json"
 
 # Read script arguments
-while getopts "s:r:l:"  option; do
+while getopts "r:"  option; do
     case $option in
-		s) subscription=${OPTARG} ;;
 		r) resourceGroupName=${OPTARG} ;;
-		l) location=${OPTARG} ;;
 		*) exitWithUsageInfo ;;
 		esac
 done
 
-if [ -z "$subscription" ] || [ -z "$resourceGroupName" ] || [ -z "$location" ]; then
+if [ -z "$resourceGroupName" ]; then
 	echo "Either one of subscription, resourceGroupName or location is empty"
 	exitWithUsageInfo
 fi
 
-# Login to Azure account if required
-if ! az account show 1>/dev/null; then
-    az login
-fi
-
-# Set the default subscription id
-az account set --subscription $subscription
-
-# Check for existing RG
-if [ $(az group exists --name $resourceGroupName) = false ]; then
-	echo "Resource group with name" $resourceGroupName "could not be found. Creating new resource group.."
-	az group create --name $resourceGroupName --location $location 1> /dev/null
-	echo "Resource group has been successfully created"
-else
-	echo "Using existing resource group..."
-fi
-
 # Start deployment
 echo "Deploying Function App using ARM template"
-export resourceName
 resources=$(az group deployment create \
     --resource-group "$resourceGroupName" \
     --template-file "$templateFilePath" \
@@ -64,20 +42,14 @@ resources=$(az group deployment create \
     -o tsv)
 
 
-. "get-resource-name-from-resource-paths.sh" -p "Microsoft.Web/sites" -r "$resources"
+. "${0%/*}/get-resource-name-from-resource-paths.sh" -p "Microsoft.Web/sites" -r "$resources"
 functionAppName=$resourceName
 echo "Successfully deployed Function App '$functionAppName'"
 
-. "get-resource-name-from-resource-paths.sh" -p "Microsoft.Storage/storageAccounts" -r "$resources"
-storageAccountName=$resourceName
-echo "Successfully created Storage Account '$storageAccountName'"
-
-. "get-resource-name-from-resource-paths.sh" -p "microsoft.insights/components" -r "$resources"
-appInsightsName=$resourceName
-echo "Successfully created Application Insights '$appInsightsName'"
-
 # Start publishing
 echo "Publishing API Functions to '$functionAppName' Function App"
+
+# Install the Azure Functions Core Tools
 
 # Install the Microsoft package repository GPG key, to validate package integrity
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
@@ -91,7 +63,7 @@ sudo apt-get update
 sudo apt-get install azure-functions-core-tools
 
 # change directory to the functions folder to publish
-cd "../../web-api/dist"
+cd "${0%/*}/../../web-api/dist"
 
 #publish the functions to the functionAppName
 func azure functionapp publish $functionAppName --javascript
