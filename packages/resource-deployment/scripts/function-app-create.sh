f584cb29d7b5efdfabbd57c8b39fe6b0588d0daf
Haonan Liu <haonliu@microsoft.com>
2019-08-30 11:10:29 -0700

    grant key vault access to function app

--- packages/resource-deployment/scripts/function-app-create.sh
+++ packages/resource-deployment/scripts/function-app-create.sh
@@ -9,10 +9,11 @@ set -eo pipefail
 export resourceGroupName
 export functionAppName
 export resourceName
+export keyVault
 
 exitWithUsageInfo() {
     echo "
-Usage: $0 -r <resource group>
+Usage: $0 -r <resource group> -k <the keyVault azure function app needs access to >
 "
     exit 1
 }
@@ -21,14 +22,15 @@ Usage: $0 -r <resource group>
 templateFilePath="${0%/*}/../templates/function-app-template.json"
 
 # Read script arguments
-while getopts "r:" option; do
+while getopts "r:k:" option; do
     case $option in
     r) resourceGroupName=${OPTARG} ;;
+    k) keyVault=${OPTARG} ;;
     *) exitWithUsageInfo ;;
     esac
 done
 
-if [ -z "$resourceGroupName" ]; then
+if [[ -z $resourceGroupName ]] || [[ -z $keyVault ]]; then
     exitWithUsageInfo
 fi
 
@@ -50,26 +52,4 @@ echo "Adding managed identity to Function App '$functionAppName'"
 principalId=$(az webapp identity assign --name "$functionAppName" --resource-group "$resourceGroupName" --query principalId -o tsv)
 echo "Successfully Added managed identity to Function App '$functionAppName'"
 
-# Start publishing
-echo "Publishing API functions to '$functionAppName' Function App"
-
-# Install the Azure Functions Core Tools: https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local#v2
-
-# Install the Microsoft package repository GPG key, to validate package integrity
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-
-# Verify your Ubuntu server is running one of the appropriate versions from the table below. To add the apt source, run
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs)-prod $(lsb_release -cs) main" > /etc/apt/sources.list.d/dotnetdev.list'
-sudo apt-get update
-
-# Install the Core Tools package
-sudo apt-get install azure-functions-core-tools
-
-# change directory to the functions folder to publish
-cd "${0%/*}/../../../web-api/dist"
-
-#publish the functions to the functionAppName
-func azure functionapp publish $functionAppName --node
-
-echo "Successfully published API functions to '$functionAppName' Function App"
+. "${0%/*}/key-vault-enable-msi.sh"
