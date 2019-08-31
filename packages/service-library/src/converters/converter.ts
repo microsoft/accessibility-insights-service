eeba07c9d2da466cfa452c47c18ac78e0652a7cc
Devaradhan Murugesan <demuruge@microsoft.com>
2019-08-30 21:00:34 -0700

    adding copyrights

--- packages/service-library/src/converters/converter.ts
+++ packages/service-library/src/converters/converter.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 export interface Converter<DocumentType, ViewModelType> {
     convertToDocument(item: ViewModelType): DocumentType;
     convertToViewModel(item: DocumentType): ViewModelType;
