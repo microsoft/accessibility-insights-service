012bef014ba77e714f0f4c5cccce41518ea697ae
Haonan Liu <haonliu@microsoft.com>
2019-09-05 17:46:32 -0700

    refactor

--- packages/resource-deployment/scripts/function-app-create.sh
+++ packages/resource-deployment/scripts/function-app-create.sh
@@ -21,6 +21,7 @@ addReplyUrlIfNotExist() {
     functionAppName=$2
 
     # Get existing reply urls of the app registration
+    echo "Fetching existing replyUrls..."
     replyUrls=$(az ad app show --id $clientId --query "replyUrls" -o tsv)
     replyUrl="https://${functionAppName}.azurewebsites.net/.auth/login/aad/callback"
 
@@ -31,25 +32,19 @@ addReplyUrlIfNotExist() {
         fi
     done
 
-    echo "Adding replyUrl to app registration..."
+    echo "Adding replyUrl $replyUrl to app registration..."
     az ad app update --id $clientId --add replyUrls $replyUrl
     echo "  Successfully added replyUrl."
 }
 
-createAppRegistrationIfNotExist() {
-    clientId=$1
-    appRegistrationName=$2
-    resourceGroupName=$3
-    environment=$4
+createAppRegistration() {
+    resourceGroupName=$1
+    environment=$2
 
-    if [ ! -z "$clientId" ] && (az ad app show --id "$clientId" 1>/dev/null); then
-        appRegistrationName=$(az ad app show --id "$clientId" --query "displayName" -o tsv)
-        echo "'$appRegistrationName' App Registration with Client ID '$clientId' already exists"
-    else
-        appRegistrationName="allyappregistration-${resourceGroupName}-${environment}"
-        clientId=$(az ad app create --display-name "$appRegistrationName" --query "appId" -o tsv)
-        echo "Successfully created '$appRegistrationName' App Registration with Client ID '$clientId'"
-    fi
+    appRegistrationName="allyappregistration-$resourceGroupName-$environment"
+    echo "Creating a new AppRegistration with display name $appRegistrationName..."
+    clientId=$(az ad app create --display-name "$appRegistrationName" --query "appId" -o tsv)
+    echo "  Successfully created '$appRegistrationName' App Registration with Client ID '$clientId'"
 }
 
 # Set default ARM Function App template files
@@ -69,15 +64,16 @@ if [ -z $resourceGroupName ] || [ -z $environment ]; then
 fi
 
 # Will return the function name if we deployed before the function app template on this resource group
-echo "Checking if the function app already exists..."
+echo "Checking if the function app and AppRegistration already exists..."
 functionAppName=$(az group deployment show -g "$resourceGroupName" -n "function-app-template" --query "properties.parameters.name.value" -o tsv)
+clientId=$(az webapp auth show -n "$functionAppName" -g "$resourceGroupName" --query "clientId" -o tsv) || true
 
-# Check if the Function App name is valid and the Function App exists
-if [ ! -z "$functionAppName" ] && (az functionapp show -n "$functionAppName" -g "$resourceGroupName" 1>/dev/null); then
-    echo "function app does not exist, creating a new one..."
-    # Get the Client (App) ID for the App Registration that's used for this Function App authentication
-    clientId=$(az webapp auth show -n "$functionAppName" -g "$resourceGroupName" --query "clientId" -o tsv)
-    createAppRegistrationIfNotExist $clientId $appRegistrationName $resourceGroupName $environment
+if [[ ! -n $clientId ]]; then
+    createAppRegistration $resourceGroupName $environment
+else
+    echo "AppRegistration already exists. Fetching displayName of the AppRegistration..."
+    appRegistrationName=$(az ad app show --id "$clientId" --query "displayName" -o tsv)
+    echo "  Successfully got AppRegistration display name: $appRegistrationName"
 fi
 
 # Start function app deployment
