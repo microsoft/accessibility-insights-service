905848145d5d794164401c12e3181a271d8fed8f
Haonan Liu <haonliu@microsoft.com>
2019-09-06 13:28:31 -0700

    add empty lines

--- packages/resource-deployment/scripts/function-app-create.sh
+++ packages/resource-deployment/scripts/function-app-create.sh
@@ -105,16 +105,22 @@ echo "  Successfully fetched principalId $principalId"
 echo "Publishing API functions to '$functionAppName' Function App"
 
 # Install the Azure Functions Core Tools: https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local#v2
+
 # Install the Microsoft package repository GPG key, to validate package integrity
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+
 # Verify your Ubuntu server is running one of the appropriate versions from the table below. To add the apt source, run
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs)-prod $(lsb_release -cs) main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-get update
+
 # Install the Core Tools package
 sudo apt-get install azure-functions-core-tools
+
 # change directory to the functions folder to publish
 cd "${0%/*}/../../../web-api/dist"
+
 #publish the functions to the functionAppName
 func azure functionapp publish $functionAppName --node
+
 echo "Successfully published API functions to '$functionAppName' Function App"
