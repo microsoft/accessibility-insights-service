5bbc65d035112d839a209422c4b161afe12bd5c0
Liu Haonan <haonliu@microsoft.com>
2019-09-06 14:08:35 -0700

    Grant key vault access to function app (#274)
    * grant key vault access to function app
    
    * add empty lines

--- packages/resource-deployment/scripts/function-app-create.sh
+++ packages/resource-deployment/scripts/function-app-create.sh
@@ -8,10 +8,11 @@ set -eo pipefail
 
 export resourceGroupName
 export resourceName
+export keyVault
 
 exitWithUsageInfo() {
     echo "
-Usage: $0 -r <resource group> -e <environment>
+Usage: $0 -r <resource group> -e <environment> -k <the keyVault azure function app needs access to>
 "
     exit 1
 }
@@ -51,19 +52,20 @@ createAppRegistration() {
 templateFilePath="${0%/*}/../templates/function-app-template.json"
 
 # Read script arguments
-while getopts "r:e:" option; do
+while getopts "r:e:k:" option; do
     case $option in
     r) resourceGroupName=${OPTARG} ;;
     e) environment=${OPTARG} ;;
+    k) keyVault=${OPTARG} ;;
     *) exitWithUsageInfo ;;
     esac
 done
 
-if [ -z $resourceGroupName ] || [ -z $environment ]; then
+if [ -z $resourceGroupName ] || [ -z $environment ] || [ -z $keyVault ]; then
     exitWithUsageInfo
 fi
 
-# Will return the function name if we deployed before the function app template on this resource group
+# Create app registration if not exists
 echo "Checking if the function app already exists..."
 functionAppName=$(az group deployment show -g "$resourceGroupName" -n "function-app-template" --query "properties.parameters.name.value" -o tsv) || true
 echo "Checking if the App Registration exists..."
@@ -89,7 +91,15 @@ resources=$(az group deployment create \
 functionAppName=$resourceName
 echo "Successfully deployed Function App '$functionAppName'"
 
+# Add reply url to app registration
 addReplyUrlIfNotExists $clientId $functionAppName
+
+# Grant key vault access to function app
+echo "Fetching principalId of the azure function..."
+principalId=$(az functionapp identity show --name $functionAppName --resource-group $resourceGroupName --query "principalId" -o tsv)
+echo "  Successfully fetched principalId $principalId"
+
+. "${0%/*}/key-vault-enable-msi.sh"
 
 # Start publishing
 echo "Publishing API functions to '$functionAppName' Function App"
