04d4b96ab3bbca8327388194b9c737955447309b
AhmedAbdoOrtiga <45858632+AhmedAbdoOrtiga@users.noreply.github.com>
2019-09-11 11:01:40 -0700

    Client ID isn't required (#283)
    * Client ID isn't required

--- packages/resource-deployment/scripts/install.sh
+++ packages/resource-deployment/scripts/install.sh
@@ -111,11 +111,11 @@ az account set --subscription "$subscription"
 
 . "${0%/*}/push-secrets-to-key-vault.sh"
 
-. "${0%/*}/function-app-create.sh"
-
 # shellcheck disable=SC2154
 keyVaultUrl=$(az keyvault show --name "$keyVault" --resource-group "$resourceGroupName" --query "properties.vaultUri" -o tsv)
 echo "Fetched keyvault url $keyVaultUrl"
+
+. "${0%/*}/function-app-create.sh"
 
 . "${0%/*}/job-schedule-create.sh"
 
