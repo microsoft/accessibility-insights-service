cbd67d890b141e9baa0cd1c1ce70e6243de66aef
Maxim Laikine <la.maks@hotmail.com>
2019-09-10 19:05:01 -0700

    Configure worker function

--- packages/workers/scan-batch-requests-feed-func/index.ts
+++ packages/workers/scan-batch-requests-feed-func/index.ts
@@ -3,7 +3,17 @@
 import 'reflect-metadata';
 
 import { Context } from '@azure/functions';
+import { ItemType, OnDemandPageScanBatchRequest } from 'storage-documents';
 
-export async function run(context: Context): Promise<void> {
+// tslint:disable-next-line: no-any
+export async function run(context: Context, documents: OnDemandPageScanBatchRequest[]): Promise<void> {
+    if (documents.length === 0) {
+        return;
+    }
+
+    const batchRequestDocuments = documents.find(d => d.itemType === ItemType.scanRunBatchRequest);
+
+    console.log(JSON.stringify(batchRequestDocuments));
+
     return;
 }
