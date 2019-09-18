8893336ca4ecdc27db57debd5c50d4f6e85da6ba
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2019-09-18 13:27:07 -0700

    address aswhin comments, add logging info

--- packages/web-api-scan-runner/src/runner/runner.ts
+++ packages/web-api-scan-runner/src/runner/runner.ts
@@ -5,15 +5,7 @@ import { Logger } from 'logger';
 import { Browser } from 'puppeteer';
 import { AxeScanResults } from 'scanner';
 import { OnDemandPageScanRunResultProvider } from 'service-library';
-import {
-    OnDemandPageScanReport,
-    OnDemandPageScanResult,
-    OnDemandPageScanRunResult,
-    OnDemandPageScanRunState,
-    OnDemandScanResult,
-    ReportFormat,
-    ScanState,
-} from 'storage-documents';
+import { OnDemandPageScanReport, OnDemandPageScanResult, OnDemandPageScanRunResult, OnDemandScanResult } from 'storage-documents';
 import { ScanMetadataConfig } from '../scan-metadata-config';
 import { ScannerTask } from '../tasks/scanner-task';
 import { WebDriverTask } from '../tasks/web-driver-task';
@@ -22,10 +14,10 @@ import { WebDriverTask } from '../tasks/web-driver-task';
 export class Runner {
     constructor(
         @inject(ScanMetadataConfig) private readonly scanMetadataConfig: ScanMetadataConfig,
-        @inject(Logger) private readonly logger: Logger,
         @inject(ScannerTask) private readonly scannerTask: ScannerTask,
         @inject(OnDemandPageScanRunResultProvider) private readonly onDemandPageScanRunResultProvider: OnDemandPageScanRunResultProvider,
         @inject(WebDriverTask) private readonly webDriverTask: WebDriverTask,
+        @inject(Logger) private readonly logger: Logger,
     ) {}
 
     public async run(): Promise<void> {
@@ -38,6 +30,7 @@ export class Runner {
         let axeScanResults: AxeScanResults;
 
         try {
+            this.logger.logInfo(`Reading Page Scan Run id= ${scanMetadata.id}`);
             pageScanRunResult = (await this.onDemandPageScanRunResultProvider.readScanRuns([scanMetadata.id]))[0];
 
             onDemandScanResult = {
@@ -54,33 +47,32 @@ export class Runner {
             // set scanned page run state to running
             onDemandPageScanRunResult = {
                 state: 'running',
-                timestamp: new Date()
-                    .toJSON()
-                    .valueOf()
-                    .toString(),
+                timestamp: new Date().toJSON(),
                 error: '',
             };
 
             pageScanRunResult.run = onDemandPageScanRunResult;
 
+            this.logger.logInfo(`Changing page status to running`);
+            this.logger.logInfo(`Updating page scan in database`);
             await this.onDemandPageScanRunResultProvider.updateScanRun(pageScanRunResult);
             pageScanRunResult = (await this.onDemandPageScanRunResultProvider.readScanRuns([scanMetadata.id]))[0];
 
             // start new web driver process
             browser = await this.webDriverTask.launch();
 
+            this.logger.logInfo(`Running Scan`);
             // scan page for accessibility issues
             try {
                 axeScanResults = await this.scannerTask.scan(scanMetadata.url);
             } catch (error) {
                 onDemandPageScanRunResult.error = (error as Error).message;
+                this.logger.logInfo(`Scan failed ${error}`);
             }
 
+            this.logger.logInfo(`Changing page status to completed`);
             onDemandPageScanRunResult.state = 'completed';
-            onDemandPageScanRunResult.timestamp = new Date()
-                .toJSON()
-                .valueOf()
-                .toString();
+            onDemandPageScanRunResult.timestamp = new Date().toJSON();
 
             if (axeScanResults !== undefined) {
                 if (axeScanResults.results !== undefined) {
@@ -96,6 +88,7 @@ export class Runner {
             pageScanRunResult.reports = [onDemandPageScanReport];
             pageScanRunResult.scanResult = onDemandScanResult;
 
+            this.logger.logInfo(`Updating page scan in database`);
             await this.onDemandPageScanRunResultProvider.updateScanRun(pageScanRunResult);
         } finally {
             await this.webDriverTask.close();
