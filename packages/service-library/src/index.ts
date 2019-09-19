a12fc8b5b78052b6d1e92f372b7913ae9a6e47dd
devaradhanm <demuruge@microsoft.com>
2019-09-19 16:50:50 -0700

    fix build break

--- packages/service-library/src/index.ts
+++ packages/service-library/src/index.ts
@@ -13,4 +13,3 @@ export { WebControllerDispatcher } from './web-api/web-controller-dispatcher';
 export { PageScanRequestProvider } from './data-providers/page-scan-request-provider';
 export { PageScanRunReportService } from './data-service/page-scan-run-report-service';
 export { PartitionKeyFactory } from './factories/partition-key-factory';
-export { PageScanRunReportService } from './data-service/page-scan-run-report-service';
