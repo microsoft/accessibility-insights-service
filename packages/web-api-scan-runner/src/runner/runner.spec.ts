3707b7d6696d9774459b63fc0cb5ab9af009d8b8
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2019-09-25 18:44:05 -0700

    add more UT details

--- packages/web-api-scan-runner/src/runner/runner.spec.ts
+++ packages/web-api-scan-runner/src/runner/runner.spec.ts
@@ -88,6 +88,11 @@ describe(Runner, () => {
             passes: [],
             violations: [
                 {
+                    description: 'rule with 2 violations',
+                    nodes: [{}, {}],
+                },
+                {
+                    description: 'rule with one violation',
                     nodes: [{}],
                 },
             ],
@@ -351,7 +356,7 @@ describe(Runner, () => {
             error: undefined,
         };
         result.scanResult = {
-            issueCount: axeScanResultsWithViolations.results.violations.length,
+            issueCount: 3,
             state: 'fail',
         };
 
