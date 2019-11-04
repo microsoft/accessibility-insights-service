646a0d1c399c916a1dc6acb77e0e7f1ec3f52a18
Kelsey Pownell <kepownel@microsoft.com>
2019-11-04 13:14:40 -0800

    add timeout to resource group creation

--- packages/resource-deployment/scripts/create-resource-group.sh
+++ packages/resource-deployment/scripts/create-resource-group.sh
@@ -60,11 +60,35 @@ if [[ -z $resourceGroupName ]] || [[ -z $location ]]; then
     exitWithUsageInfo
 fi
 
-resourceGroupExists=$(az group exists -n "$resourceGroupName")
+resourceGroupExistsQuery="az group exists -n $resourceGroupName"
+resourceGroupExists=$($resourceGroupExistsQuery)
 
 if [ "$resourceGroupExists" = false ]; then
     echo "Creating resource group $resourceGroupName under $location"
     az group create --name "$resourceGroupName" --location "$location" 1>/dev/null
 else
     echo "Resource group $resourceGroupName already exists"
+fi
+
+resourceGroupExists=$($resourceGroupExistsQuery)
+# Wait until we are certain the resource group exists
+waiting=false
+timeout=600 # timeout after ten minutes
+end=$((SECONDS + $timeout))
+while [ "$resourceGroupExists" = false ] && [ $SECONDS -le $end ]; do
+    if [ "$waiting" != true ]; then
+        waiting=true
+        echo "Waiting for resource group $resourceGroupName"
+        printf " - Running .."
+    fi
+
+    sleep 5
+    printf "."
+    resourceGroupExists=$($resourceGroupExistsQuery)
+done
+
+# Exit if timed out
+if [ "$resourceGroupExists" = false ]; then
+    echo "Could not find resource group $resourceGroupName after $timeout seconds"
+    exit 1
 fi
