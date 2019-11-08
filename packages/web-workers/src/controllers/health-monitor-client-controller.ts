e39779c51d5268c3cad82ee0b97778ccea4e0de0
Kelsey Pownell <kepownel@microsoft.com>
2019-11-07 17:21:13 -0800

    placeholders for health monitor client activities

--- packages/web-workers/src/controllers/health-monitor-client-controller.ts
+++ packages/web-workers/src/controllers/health-monitor-client-controller.ts
@@ -4,6 +4,7 @@ import { ServiceConfiguration } from 'common';
 import { inject, injectable } from 'inversify';
 import { ContextAwareLogger } from 'logger';
 import { WebController } from 'service-library';
+import { A11yServiceClient } from 'web-api-client';
 
 // tslint:disable: no-any
 
@@ -15,15 +16,62 @@ export class HealthMonitorClientController extends WebController {
     public constructor(
         @inject(ServiceConfiguration) protected readonly serviceConfig: ServiceConfiguration,
         @inject(ContextAwareLogger) contextAwareLogger: ContextAwareLogger,
+//        @inject(A11yServiceClient) protected webApiClient: A11yServiceClient,
     ) {
         super(contextAwareLogger);
     }
 
     public async handleRequest(...args: any[]): Promise<void> {
-        this.contextAwareLogger.logInfo(`Executing ${args[0]} activity action.`);
+        const activityActionName = args[0];
+        this.contextAwareLogger.logInfo(`Executing ${activityActionName} activity action.`);
+        switch (activityActionName) {
+            case 'createScanRequest': {
+                this.createScanRequest();
+                break;
+            }
+            case 'getScanResult': {
+                this.getScanResult();
+                break;
+            }
+            case 'getScanResultBatch': {
+                this.getScanResultBatch();
+                break;
+            }
+            case 'getScanReport': {
+                this.getScanReport();
+                break;
+            }
+            case 'getHealthStatus': {
+                this.getHealthStatus();
+                break;
+            }
+            default: {
+                this.contextAwareLogger.logInfo(`Unrecognized activity action ${activityActionName}.`);
+            }
+        }
     }
 
     protected validateRequest(...args: any[]): boolean {
         return true;
+    }
+
+    private createScanRequest(): void {
+        return;
+    }
+
+    private getScanResult(): void {
+        return;
+    }
+
+    private getScanResultBatch(): void {
+        return;
+    }
+
+    private getScanReport(): void {
+        return;
+    }
+
+    private getHealthStatus(): void {
+        return;
     }
 }
