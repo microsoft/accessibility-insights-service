cc066c7e208b98b27d4ad3bc43a8259a0b28be3e
Haonan Liu <haonliu@microsoft.com>
2019-12-17 17:30:02 -0800

    remove some typings

--- packages/functional-tests/src/request-data.ts
+++ packages/functional-tests/src/request-data.ts
@@ -1,31 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { AvailabilityTelemetry } from 'logger';
 import { Url } from 'url';
-
-export interface ActivityRequestData {
-    activityName: string;
-    data?: unknown;
-}
-
-export interface CreateScanRequestData {
-    scanUrl: string;
-    priority: number;
-}
-
-export interface GetScanResultData {
-    scanId: string;
-}
-
-export interface GetScanReportData {
-    scanId: string;
-    reportId: string;
-}
-
-export interface TrackAvailabilityData {
-    name: string;
-    telemetry: AvailabilityTelemetry;
-}
 
 interface SerializableRequest {
     uri: Url;
