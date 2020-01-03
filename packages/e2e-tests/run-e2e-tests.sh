ca86e443a40fab51958cb1c3724f769a5ce482d7
Kelsey Pownell <kepownel@microsoft.com>
2020-01-03 15:24:32 -0800

    fetch base url from resource group

--- packages/e2e-tests/run-e2e-tests.sh
+++ packages/e2e-tests/run-e2e-tests.sh
@@ -14,26 +14,44 @@ export authorityUrl
 
 exitWithUsageInfo() {
     echo "
-Usage: $0 -s <scan url> -b <api base url> -a <api version> -c <client id> -t <client secret> -u <authority url>
+Usage: $0 -s <subscription> -g <resource group> -u <scan url> -a <api version> -c <client id> -t <client secret> -o <authority url>
 "
     exit 1
 }
 
+getApiBaseUrl() {
+    # Login to Azure if required
+    if ! az account show 1>/dev/null; then
+        az login
+    fi
+
+    az account set --subscription "$subscription"
+
+    baseUrl=$(az apim list --resource-group kepowneltest --query "[].gatewayUrl" -o tsv)
+
+    if [ -z $baseUrl ]; then
+        echo "Could not find base url for api management instance"
+    fi
+}
+
 # Read script arguments
-while getopts ":s:b:a:c:t:u:" option; do
+while getopts ":s:g:u:b:a:c:t:o:" option; do
     case $option in
-    s) scanUrl=${OPTARG} ;;
-    b) baseUrl=${OPTARG} ;;
+    s) subscription=${OPTARG} ;;
+    g) resourceGroup=${OPTARG} ;;
+    u) scanUrl=${OPTARG} ;;
     a) apiVersion=${OPTARG} ;;
     c) clientId=${OPTARG} ;;
     t) clientSecret=${OPTARG} ;;
-    u) authorityUrl=${OPTARG} ;;
+    o) authorityUrl=${OPTARG} ;;
     *) exitWithUsageInfo ;;
     esac
 done
 
-if [[ -z $scanUrl ]] || [[ -z $baseUrl ]] || [[ -z $apiVersion ]] || [[ -z $clientId ]] || [[ -z $clientSecret ]] || [[ -z $authorityUrl ]]; then
+if [[ -z $subscription ]] || [[ -z $resourceGroup ]] || [[ -z $scanUrl ]] || [[ -z $apiVersion ]] || [[ -z $clientId ]] || [[ -z $clientSecret ]] || [[ -z $authorityUrl ]]; then
     exitWithUsageInfo
 fi
+
+getApiBaseUrl
 
 yarn e2e
