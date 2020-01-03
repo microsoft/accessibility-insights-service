27d57158cc9c0a6c01814c1bdf7e1655f014694f
Haonan Liu <haonliu@microsoft.com>
2020-01-03 15:16:52 -0800

    handle string error

--- packages/web-api/src/converters/scan-run-error-converter.ts
+++ packages/web-api/src/converters/scan-run-error-converter.ts
@@ -4,14 +4,15 @@ import { injectable } from 'inversify';
 import { isNil } from 'lodash';
 import { scanErrorNameToErrorMap, ScanRunErrorCode, ScanRunErrorCodes } from 'service-library';
 import { ScanError } from 'storage-documents';
+import { isString } from 'util';
 
 @injectable()
 export class ScanRunErrorConverter {
     public getScanRunErrorCode(scanError: string | ScanError): ScanRunErrorCode {
-        if (isNil(scanError)) {
+        if (isNil(scanError) || isString(scanError)) {
             return ScanRunErrorCodes.internalError;
         }
 
-        return scanErrorNameToErrorMap[(<ScanError>scanError).errorType];
+        return scanErrorNameToErrorMap[scanError.errorType];
     }
 }
