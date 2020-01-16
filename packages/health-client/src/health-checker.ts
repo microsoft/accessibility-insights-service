5dc38d5456069c6331527a2f9a5f2c2fb541058f
Maxim Laikine <40576301+lamaks@users.noreply.github.com>
2020-01-15 18:54:07 -0800

    Health client (#472)

---
+++ packages/health-client/src/health-checker.ts
@@ -0,0 +1,70 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import 'reflect-metadata';
+
+import { System } from 'common';
+import { TestRunResult } from 'service-library';
+import { A11yServiceClient, A11yServiceCredential } from 'web-api-client';
+import * as yargs from 'yargs';
+
+// tslint:disable: radix no-any
+type Argv = {
+    clientId: string;
+    clientSecret: string;
+    authorityUrl: string;
+    waitTimeBeforeEvaluationInMinutes: string;
+    evaluationIntervalInMinutes: string;
+    releaseId: string;
+    baseUrl: string;
+};
+
+console.log('[health-client] Start evaluation of functional tests result.');
+
+const argv: Argv = yargs.argv as any;
+const cred = new A11yServiceCredential(argv.clientId, argv.clientSecret, argv.clientId, argv.authorityUrl);
+const client = new A11yServiceClient(cred, argv.baseUrl);
+const testTimeoutInMinutes = 30;
+const waitTimeBeforeEvaluation = parseInt(argv.waitTimeBeforeEvaluationInMinutes) * 60000;
+const evaluationInterval = parseInt(argv.evaluationIntervalInMinutes) * 60000;
+
+console.log(`[health-client] Waiting for ${argv.waitTimeBeforeEvaluationInMinutes} minutes before evaluating functional tests result.`);
+
+const isTestTimeout = (startTime: Date, currentTime: Date, timeout: number): boolean => {
+    return currentTime.getTime() - startTime.getTime() > timeout;
+};
+
+let healthStatus: TestRunResult;
+
+setTimeout(async () => {
+    const startTime = new Date();
+    while (healthStatus !== 'pass') {
+        try {
+            console.log('[health-client] Retrieving functional tests result.');
+
+            const response = await client.checkHealth(`/release/${argv.releaseId}`);
+            if (response.statusCode !== 200) {
+                throw new Error(JSON.stringify(response));
+            }
+
+            console.log(`[health-client] Functional tests result: ${JSON.stringify(response.body)}`);
+
+            healthStatus = response.body.healthStatus;
+        } catch (error) {
+            console.log(`[health-client] Failed to retrieve functional tests result. ${error}`);
+        }
+
+        if (healthStatus !== 'pass') {
+            if (isTestTimeout(startTime, new Date(), testTimeoutInMinutes * 60000)) {
+                console.log('[health-client] Functional tests result validation timed out.');
+
+                throw new Error('[health-client] Functional tests result validation timed out.');
+            }
+
+            console.log('[health-client] Functional tests are failing. Waiting for next evaluation result.');
+
+            await System.wait(evaluationInterval);
+        } else {
+            console.log('[health-client] Functional tests succeeded.');
+        }
+    }
+}, waitTimeBeforeEvaluation);
