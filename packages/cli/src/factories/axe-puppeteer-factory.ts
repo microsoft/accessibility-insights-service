e6749d1b8ae2c6ad67f2140585ec8367047a89fe
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2020-02-24 15:25:57 -0800

    add more unit test

--- packages/cli/src/factories/axe-puppeteer-factory.ts
+++ packages/cli/src/factories/axe-puppeteer-factory.ts
@@ -3,19 +3,20 @@
 import { AxePuppeteer } from 'axe-puppeteer';
 import * as fs from 'fs';
 import { injectable } from 'inversify';
-import { isEmpty, isNil } from 'lodash';
-import * as path from 'path';
+import { isEmpty } from 'lodash';
 import * as Puppeteer from 'puppeteer';
 import { RuleExclusion } from './rule-exclusion';
 
 @injectable()
 export class AxePuppeteerFactory {
+    constructor(private readonly fileSystemObj: typeof fs = fs) {}
+
     public async createAxePuppeteer(page: Puppeteer.Page, sourcePath?: string): Promise<AxePuppeteer> {
         const ruleExclusionList = new RuleExclusion();
 
-        if (!isNil(sourcePath) && !isEmpty(sourcePath)) {
+        if (!isEmpty(sourcePath)) {
             // tslint:disable-next-line: non-literal-fs-path
-            const content = fs.readFileSync(sourcePath);
+            const content = this.fileSystemObj.readFileSync(sourcePath);
 
             return new AxePuppeteer(page, content.toString()).disableRules(ruleExclusionList.accessibilityRuleExclusionList);
         }
