12f66723438b900fdfc74c9e68fc3a33cbf9dff9
Devaradhan Murugesan <demuruge@microsoft.com>
2020-02-25 12:23:39 -0800

    remove why-is-node-running typings

--- packages/common/external/external.d.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-declare module 'why-is-node-running' {
-    const log: () => void;
-    export = log;
-}
