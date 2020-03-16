8fa5138b16c3a48945894ad59059b4549b93859c
devaradhanm <demuruge@microsoft.com>
2020-03-16 16:23:17 -0700

    chore: adding packages for send notification job manager & runner

---
+++ packages/web-api-send-notification-runner/src/index.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import 'reflect-metadata';
+
+(async () => {
+    console.log('not implemented');
+})().catch(error => {
+    console.log('Exception thrown in send notification runner: ', error);
+    process.exit(1);
+});
