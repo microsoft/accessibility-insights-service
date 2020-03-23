9c23fcca45fc1aeb9011683429e4a4fdab17bc9c
Kelsey Pownell <kepownel@microsoft.com>
2020-03-23 13:01:51 -0700

    Validate retry interval

--- packages/azure-services/src/storage/cosmos-container-client.spec.ts
+++ packages/azure-services/src/storage/cosmos-container-client.spec.ts
@@ -46,7 +46,7 @@ beforeEach(() => {
     );
     MockDate.set(startTime);
     systemUtilsMock
-        .setup(s => s.wait(It.isAny()))
+        .setup(s => s.wait(retryOptions.intervalMilliseconds))
         .callback((millis: number) => {
             MockDate.set(Date.now() + millis);
         });
