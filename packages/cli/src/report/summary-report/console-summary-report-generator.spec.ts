e0d030e4dd20a91289de71d53ee8ec96b1ec505f
devaradhanm <demuruge@microsoft.com>
2020-05-01 11:27:24 -0700

    chore: add console & json summary report generators. (#766)
    These report generators are not yet used.

---
+++ packages/cli/src/report/summary-report/console-summary-report-generator.spec.ts
@@ -0,0 +1,38 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import 'reflect-metadata';
+
+import { ConsoleSummaryReportGenerator } from './console-summary-report-generator';
+import { SummaryReportData } from './summary-report-data';
+
+// tslint:disable: no-object-literal-type-assertion
+
+describe(ConsoleSummaryReportGenerator, () => {
+    let testSubject: ConsoleSummaryReportGenerator;
+
+    beforeEach(() => {
+        testSubject = new ConsoleSummaryReportGenerator();
+    });
+
+    test.each([
+        [
+            'has violations',
+            {
+                urlToReportMap: { url1: 'file1' },
+                violationCountByRuleMap: {
+                    rule1WithALongName11111111111111111: 10,
+                    rule2: 3,
+                },
+            } as SummaryReportData,
+        ],
+        [
+            'has no violations',
+            {
+                urlToReportMap: { url1: 'file1' },
+                violationCountByRuleMap: {},
+            } as SummaryReportData,
+        ],
+    ])('logs summary when %s', async (testCaseName, testCase) => {
+        expect(testSubject.generateReport(testCase)).toMatchSnapshot();
+    });
+});
---
+++ packages/cli/src/report/summary-report/summary-report-data.ts
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+export interface ViolationCountMap {
+    [key: string]: number;
+}
+
+export interface UrlToReportMap {
+    [key: string]: string;
+}
+
+export interface SummaryReportData {
+    violationCountByRuleMap: ViolationCountMap;
+    urlToReportMap: UrlToReportMap;
+}
