847d3b26726968a479b863207e8ef2fce551afe9
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2020-05-07 11:26:30 -0700

    address comments

--- packages/cli/src/report/report-disk-writer.ts
+++ packages/cli/src/report/report-disk-writer.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import * as filenamify from 'filenamify-url';
+import * as filenamify from 'filenamify';
+import * as filenamifyUrl from 'filenamify-url';
 import * as fs from 'fs';
 import { injectable } from 'inversify';
 import { isEmpty } from 'lodash';
@@ -19,9 +20,9 @@ export class ReportDiskWriter {
         let reportFileName;
 
         try {
-            reportFileName = `${directory}/${filenamify(fileName, { replacement: '_' })}.${format}`;
+            reportFileName = `${directory}/${filenamifyUrl(fileName, { replacement: '_' })}.${format}`;
         } catch {
-            reportFileName = `${directory}/${fileName}.${format}`;
+            reportFileName = `${directory}/${filenamify(fileName, { replacement: '_' })}.${format}`;
         }
 
         if (!this.fileSystemObj.existsSync(directory)) {
