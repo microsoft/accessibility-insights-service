3cbf0c4a481f8ba2d3b94d27d2a7447d12869042
Maxim Laikine <la.maks@hotmail.com>
2020-06-02 23:28:56 -0700

    Added batch task retry

--- packages/azure-services/src/azure-batch/batch-task-config-generator.ts
+++ packages/azure-services/src/azure-batch/batch-task-config-generator.ts
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
 import { BatchServiceModels } from '@azure/batch';
 import { inject, injectable } from 'inversify';
 import * as moment from 'moment';
@@ -12,14 +11,18 @@ export class BatchTaskConfigGenerator {
 
     public async getTaskConfig(taskId: string, messageText: string): Promise<BatchServiceModels.TaskAddParameter> {
         const commandLine = this.batchTaskCommandLineProvider.getCommandLine(messageText);
-        const maxTaskDurationInMinutes = (await this.batchTaskCommandLineProvider.getDefaultTaskConfig()).taskTimeoutInMinutes;
+        const batchTaskConfig = await this.batchTaskCommandLineProvider.getDefaultTaskConfig();
 
         return {
             id: taskId,
             commandLine: commandLine,
             resourceFiles: this.batchTaskCommandLineProvider.getResourceFiles(),
             environmentSettings: this.batchTaskCommandLineProvider.getAllEnvironmentSettings(messageText),
-            constraints: { maxWallClockTime: moment.duration({ minute: maxTaskDurationInMinutes }).toISOString() },
+            constraints: {
+                maxWallClockTime: moment.duration({ minute: batchTaskConfig.taskTimeoutInMinutes }).toISOString(),
+                retentionTime: moment.duration({ day: batchTaskConfig.retentionTimeInDays }).toISOString(),
+                maxTaskRetryCount: batchTaskConfig.maxTaskRetryCount,
+            },
         };
     }
 }
