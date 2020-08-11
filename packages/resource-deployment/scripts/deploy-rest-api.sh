64fd9fd9485c4cdb3f6596140a13615f591e214d
Maxim Laikine <la.maks@hotmail.com>
2020-08-11 15:59:06 -0700

    Fix MacOS related script and APIM rules

--- packages/resource-deployment/scripts/deploy-rest-api.sh
+++ packages/resource-deployment/scripts/deploy-rest-api.sh
@@ -16,11 +16,11 @@ deployRestApi() {
 restrictWebApiAccess() {
     echo "Restricting function app access to APIM IP address"
     apimIpAddress=$(az apim show --name "$apiManagementName" --resource-group "$resourceGroupName" --query "publicIpAddresses" -o tsv)
-    az functionapp config access-restriction remove -g "$resourceGroupName" \
+    result=$(az functionapp config access-restriction remove -g "$resourceGroupName" \
         -n "$webApiFuncAppName" \
         --rule-name "APIM" \
         --action "Allow" \
-        --ip-address "$apimIpAddress" 1>/dev/null
+        --ip-address "$apimIpAddress" --priority 100) || echo "No rule found with the specified criteria"
 
     az functionapp config access-restriction add -g "$resourceGroupName" \
         -n "$webApiFuncAppName" \
