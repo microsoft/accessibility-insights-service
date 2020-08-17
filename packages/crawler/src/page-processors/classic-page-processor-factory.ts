79d74324f6fee0e19d5f9eb8249666396244fe91
Kelsey Pownell <kepownel@microsoft.com>
2020-08-17 14:23:17 -0700

    extract and test helper functions from PageProcessorFactoryBase

--- packages/crawler/src/page-processors/classic-page-processor-factory.ts
+++ packages/crawler/src/page-processors/classic-page-processor-factory.ts
@@ -1,19 +1,23 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { ClassicPageProcessor } from './classic-page-processor';
+import { getDiscoveryPattern } from './get-discovery-pattern';
 import { PageProcessor, PageProcessorOptions } from './page-processor-base';
-import { PageProcessorFactoryBase } from './page-processor-factory';
+import { PageProcessorFactory } from './page-processor-factory';
 import { PageProcessorHelper } from './page-processor-helper';
 
-export class ClassicPageProcessorFactory extends PageProcessorFactoryBase {
-    public createPageProcessor(pageProcessorOptions: PageProcessorOptions): PageProcessor {
+export class ClassicPageProcessorFactory implements PageProcessorFactory {
+    public createPageProcessor(
+        pageProcessorOptions: PageProcessorOptions,
+        getDiscoveryPatternFunc: typeof getDiscoveryPattern = getDiscoveryPattern,
+    ): PageProcessor {
         const helper = new PageProcessorHelper(pageProcessorOptions.logger);
 
         return new ClassicPageProcessor(
             pageProcessorOptions.requestQueue,
             pageProcessorOptions.logger,
             helper,
-            this.getDiscoveryPattern(pageProcessorOptions.baseUrl, pageProcessorOptions.discoveryPatterns),
+            getDiscoveryPatternFunc(pageProcessorOptions.baseUrl, pageProcessorOptions.discoveryPatterns),
         );
     }
 }
