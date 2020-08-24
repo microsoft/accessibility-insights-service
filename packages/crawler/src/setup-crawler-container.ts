1092297ecfa175d5dab89cba071fbc4fd2c91b4e
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2020-08-24 15:30:12 -0700

    refactor the ioc setup

--- packages/crawler/src/setup-crawler-container.ts
+++ packages/crawler/src/setup-crawler-container.ts
@@ -8,40 +8,18 @@ import { ConsoleLoggerClient, GlobalLogger } from 'logger';
 import { ApifyResourceCreator } from './apify-resources/resource-creator';
 import { AxePuppeteerFactory } from './axe-puppeteer/axe-puppeteer-factory';
 import { CrawlerConfiguration } from './crawler/crawler-configuration';
-import { CrawlerEngine } from './crawler/crawler-engine';
 import { CrawlerFactory } from './crawler/crawler-factory';
-import { AccessibilityScanOperation } from './page-operations/accessibility-scan-operation';
-import { ClickElementOperation } from './page-operations/click-element-operation';
-import { EnqueueActiveElementsOperation } from './page-operations/enqueue-active-elements-operation';
-import { ClassicPageProcessor } from './page-processors/classic-page-processor';
-import { PageProcessorBase } from './page-processors/page-processor-base';
 import { PageProcessorFactory } from './page-processors/page-processor-factory';
-import { SimulatorPageProcessor } from './page-processors/simulator-page-processor';
-import { PageScanner } from './scanner-operations/page-scanner';
-import { LocalBlobStore } from './storage/local-blob-store';
-import { LocalDataStore } from './storage/local-data-store';
-import { ActiveElementsFinder } from './utility/active-elements-finder';
 
 export function setupCrawlerContainer(): inversify.Container {
     const container = new inversify.Container({ autoBindInjectable: true });
-    container.bind(CrawlerEngine).toSelf().inSingletonScope();
     container.bind(CrawlerConfiguration).toSelf().inSingletonScope();
     container.bind(CrawlerFactory).toSelf().inSingletonScope();
     container.bind(ApifyResourceCreator).toSelf().inSingletonScope();
-    container.bind(AccessibilityScanOperation).toSelf().inSingletonScope();
-    container.bind(ClickElementOperation).toSelf().inSingletonScope();
-    container.bind(EnqueueActiveElementsOperation).toSelf().inSingletonScope();
-    container.bind(ClassicPageProcessor).toSelf().inSingletonScope();
     container.bind(PageProcessorFactory).toSelf().inSingletonScope();
-    container.bind(SimulatorPageProcessor).toSelf().inSingletonScope();
-    container.bind(PageProcessorBase).toSelf().inSingletonScope();
-    container.bind(PageScanner).toSelf().inSingletonScope();
     container.bind('ReporterFactory').toConstantValue(reporterFactory);
     container.bind<AxePuppeteerFactory>(AxePuppeteerFactory).toSelf().inSingletonScope();
 
-    container.bind(LocalBlobStore).toSelf().inSingletonScope();
-    container.bind(LocalDataStore).toSelf().inSingletonScope();
-    container.bind(ActiveElementsFinder).toSelf().inSingletonScope();
     container.bind(GlobalLogger).toConstantValue(new GlobalLogger([new ConsoleLoggerClient(new ServiceConfiguration(), console)], process));
 
     return container;
