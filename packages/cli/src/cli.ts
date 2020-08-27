9fc95590586afe05b27f3caf0c2c68bb2e4afbba
Maxim Laikine <la.maks@hotmail.com>
2020-08-26 22:21:29 -0700

    Enable webpack. Update cli help. Fix module export

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -2,11 +2,14 @@
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+
 import 'reflect-metadata';
 // tslint:disable-next-line: no-import-side-effect
 import './module-name-mapper';
 
 import { CrawlerEntryPoint, setupCrawlerContainer } from 'accessibility-insights-crawler';
+// @ts-ignore
+import * as cheerio from 'cheerio';
 import { isEmpty } from 'lodash';
 import * as yargs from 'yargs';
 import { CliEntryPoint } from './cli-entry-point';
@@ -16,28 +19,25 @@ import { setupCliContainer } from './setup-cli-container';
 // tslint:disable-next-line:max-func-body-length
 (async () => {
     const scanArguments = (yargs
-        .usage(
-            'Usage: $0 --crawl <crawl> --url <url> --simulate <simulate> [--selectors <selector1 ...>] --output <output> --maxUrls <maxUrls> --restart <restart> --snapshot <snapshot> --memoryMBytes <memoryMBytes> --silentMode <silentMode> [--existingUrls <url1 ...>] [--discoveryPatterns <pattern1 ...>]',
-        )
+        .wrap(yargs.terminalWidth())
         .options({
             crawl: {
                 type: 'boolean',
-                describe: 'Crawl if true',
-                demandOption: true,
+                describe: 'Crawl web site under the provided URL.',
                 default: false,
             },
             url: {
                 type: 'string',
-                describe: 'The URL to scan for accessibility issues',
+                describe: '<url> The URL to scan (and crawl if --crawl option is selected) for accessibility issues.',
             },
             simulate: {
                 type: 'boolean',
-                describe: 'Simulate user click on elements that match to the specified selectors',
+                describe: 'Simulate user click on elements that match to the specified selectors.',
                 default: false,
             },
             selectors: {
                 type: 'array',
-                describe: `List of CSS selectors to match against. Default selector is 'button'`,
+                describe: `List of CSS selectors to match against, separated by space. Default selector is 'button'.`,
                 default: [],
             },
             output: {
@@ -46,58 +46,56 @@ import { setupCliContainer } from './setup-cli-container';
             },
             maxUrls: {
                 type: 'number',
-                describe: ` Maximum number of pages that the crawler will open. The crawl will stop when this limit is reached.
-                            The default is 100.
-                            Note that in cases of parallel crawling, the actual number of pages visited might be slightly higher than this value.`,
+                describe: `Maximum number of pages that the crawler will open. The crawl will stop when this limit is reached. Default is 100.
+                           Note that in cases of parallel crawling, the actual number of pages visited might be slightly higher than this value.`,
                 default: 100,
             },
             restart: {
                 type: 'boolean',
                 describe:
-                    'If this flag is set, clear the queue of all pending and handled requests before crawling, otherwise resume the crawl from the last request in the queue.',
+                    'Clear the pending crawl queue and start crawl from the provided URL when set to true, otherwise resume the crawl from the last request in the queue.',
                 default: false,
             },
             snapshot: {
                 type: 'boolean',
-                describe:
-                    'Save snapshot of the crawled page, if no value is not provided, it will be true  if simulation is enabled, otherwise false',
+                describe: 'Save snapshot of the crawled page. Enabled by default if simulation option is selected, otherwise false.',
             },
             memoryMBytes: {
                 type: 'number',
-                describe: 'The maximum number of megabytes to be used by the crawler',
+                describe: 'The maximum number of megabytes to be used by the crawler.',
             },
             silentMode: {
                 type: 'boolean',
-                describe: 'Set to false if you want the browser to open the webpages while crawling',
+                describe: 'Open browser window while crawling when set to true.',
                 default: true,
             },
             inputFile: {
                 type: 'string',
-                describe: 'List of URLs to crawl in addition to URLs discovered from crawling base URL',
+                describe: 'List of URLs to crawl in addition to URLs discovered from crawling the provided URL.',
             },
             existingUrls: {
                 type: 'array',
-                describe: `List of URLs to crawl in addition to URLs discovered from crawling base URL`,
+                describe: `List of URLs to crawl in addition to URLs discovered from crawling the provided URL, separated by space.`,
             },
             discoveryPatterns: {
                 type: 'array',
-                describe: `List of patterns to crawl in addition to the base url`,
+                describe: `List of RegEx patterns to crawl in addition to the provided URL, separated by space.`,
             },
         })
         .check((args) => {
             if (args.crawl) {
                 if ((isEmpty(args.url) && isEmpty(args.inputFile)) || (!isEmpty(args.url) && !isEmpty(args.inputFile))) {
-                    throw new Error('You should provide either url or inputFile parameter only!');
+                    throw new Error('Provide either --url or --inputFile option.');
                 }
             } else {
                 if (isEmpty(args.url)) {
-                    throw new Error('You should provide a url to crawl!');
+                    throw new Error('The --url option is required.');
                 }
             }
 
             return true;
         })
-        .describe('help', 'show help').argv as unknown) as ScanArguments;
+        .describe('help', 'Show help').argv as unknown) as ScanArguments;
 
     if (!scanArguments.crawl) {
         const cliEntryPoint = new CliEntryPoint(setupCliContainer());
@@ -119,6 +117,6 @@ import { setupCliContainer } from './setup-cli-container';
         });
     }
 })().catch((error) => {
-    console.log('Exception thrown in scanner: ', error);
+    console.log('Exception occurred while running the tool: ', error);
     process.exit(1);
 });
--- packages/crawler/src/global-overrides.ts
+++ packages/crawler/src/global-overrides.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+export {};
+
 // tslint:disable: no-any no-unsafe-any no-require-imports no-var-requires no-function-expression
 function overrideCheckPrototypeUtilsFunc(exports: any): any {
     const originalFunc = exports.checkParamPrototypeOrThrow;
@@ -23,8 +25,8 @@ function overrideExports(moduleName: string, exports: any): any {
     return exports;
 }
 
-const moduleRefCrawler = require('module');
-moduleRefCrawler.prototype.require = new Proxy(moduleRefCrawler.prototype.require, {
+const moduleRef = require('module');
+moduleRef.prototype.require = new Proxy(moduleRef.prototype.require, {
     apply(target: any, thisArg: any, argumentsList: any): any {
         const moduleName = argumentsList[0];
         const exports = Reflect.apply(target, thisArg, argumentsList);
@@ -33,13 +35,12 @@ moduleRefCrawler.prototype.require = new Proxy(moduleRefCrawler.prototype.requir
     },
 });
 
-const osCrawler = require('os');
-moduleRefCrawler._resolveFilename = new Proxy(moduleRefCrawler._resolveFilename, {
+moduleRef._resolveFilename = new Proxy(moduleRef._resolveFilename, {
     apply(target: any, thisArg: any, argumentsList: any): any {
         const moduleName = argumentsList[0] as string;
         let path = Reflect.apply(target, thisArg, argumentsList) as string;
         if (moduleName.startsWith('@uifabric/styling')) {
-            if (osCrawler.type() === 'Windows_NT') {
+            if (require('os').type() === 'Windows_NT') {
                 path = path.replace('\\lib\\', '\\lib-commonjs\\');
             } else {
                 path = path.replace('/lib/', '/lib-commonjs/');
