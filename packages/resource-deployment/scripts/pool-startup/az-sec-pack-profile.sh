11c8a9894d68193dcc4202dca47748858fdacbf0
Maxim Laikine <la.maks@hotmail.com>
2020-09-22 13:23:34 -0700

    temp

---
+++ packages/resource-deployment/scripts/pool-startup/az-sec-pack-profile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export azSecPackGcsEnvironment="Test"
+export azSecPackGcsAccount="A11YServiceDev"
+export azSecPackGcsConfigVersion="1.1"
+export azSecPackEventVersion="1"
+export azSecPackNamespace="A11YServiceCI"
+export azSecPackTimestamp="2019-06-20T00:00:00Z"
+export azSecPackMoniker="a11yservicecisecurity"
