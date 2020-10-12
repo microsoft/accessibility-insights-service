4424e813771f87d97fd6832378f2980ecfb2db88
Maxim Laikine <la.maks@hotmail.com>
2020-10-12 16:47:59 -0700

    Added report group contracts

--- packages/service-library/src/web-api/api-contracts/scan-run-request.ts
+++ packages/service-library/src/web-api/api-contracts/scan-run-request.ts
@@ -1,11 +1,25 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+
+/**
+ * Defines REST API HTTP POST scan request contract
+ */
 export interface ScanRunRequest {
     url: string;
+    site?: Website;
+    reportGroups?: ReportGroup[];
     scanNotifyUrl?: string;
     /**
      * Priority values can range from -1000 to 1000, with -1000 being the lowest priority and 1000 being the highest priority.
      * The default value is 0.
      */
     priority?: number;
+}
+
+export interface Website {
+    baseUrl: string;
+}
+
+export interface ReportGroup {
+    consolidatedId: string;
 }
--- packages/storage-documents/src/index.ts
+++ packages/storage-documents/src/index.ts
@@ -1,15 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-export * from './issue-scan-result';
 export * from './item-type';
-export * from './page-scan-result';
 export * from './on-demand-page-scan-result';
 export * from './states';
-export * from './website-page';
-export * from './website';
 export * from './run-result';
 export * from './storage-document';
-export * from './scan-request-message';
 export * from './on-demand-page-scan-batch-request';
 export * from './partition-key';
 export * from './on-demand-page-scan-request';
