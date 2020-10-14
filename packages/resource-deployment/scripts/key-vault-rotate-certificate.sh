ae49769fe390bf114522154baac0062dbf70a6e7
Maxim Laikine <40576301+lamaks@users.noreply.github.com>
2020-10-14 09:29:55 -0700

    Added script to rotate certificate in Azure key vault (#1084)
    * Cert rotation script
    
    * Update script
    
    * Update script login

---
+++ packages/resource-deployment/scripts/key-vault-rotate-certificate.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+set -eo pipefail
+
+certificateName="azSecPackCert"
+
+exitWithUsageInfo() {
+    echo "
+Usage: $0 -r <resource group> [-k <key vault>] [-n <key vault certificate name>] [-s <subscription name or id>]
+"
+    exit 1
+}
+
+loginToAzure() {
+    # Login to Azure if required
+    if ! az account show 1>/dev/null; then
+        az login
+    fi
+}
+
+getCurrentUserDetails() {
+    userType=$(az account show --query "user.type" -o tsv) || true
+    principalName=$(az account show --query "user.name" -o tsv) || true
+
+    if [[ $userType == "user" ]]; then
+        echo "Running script using current user credentials"
+    else
+        echo "Running script using service principal identity"
+    fi
+}
+
+grantUserAccessToKeyVault() {
+    if [[ $userType == "user" ]]; then
+        echo "Granting access to key vault for current user account"
+        az keyvault set-policy --name "$keyVault" --upn "$principalName" --certificate-permissions get list create 1>/dev/null
+    else
+        echo "Granting access to key vault for service principal account"
+        az keyvault set-policy --name "$keyVault" --spn "$principalName" --certificate-permissions get list create 1>/dev/null
+    fi
+}
+
+revokeUserAccessToKeyVault() {
+    if [[ $userType == "user" ]]; then
+        echo "Revoking access to key vault for current user account"
+        az keyvault delete-policy --name "$keyVault" --upn "$principalName" 1>/dev/null || true
+    else
+        echo "Revoking access to key vault for service principal account"
+        az keyvault delete-policy --name "$keyVault" --spn "$principalName" 1>/dev/null || true
+    fi
+}
+
+# Read script arguments
+while getopts ":s:r:k:n:" option; do
+    case $option in
+    s) subscription=${OPTARG} ;;
+    r) resourceGroupName=${OPTARG} ;;
+    k) keyVault=${OPTARG} ;;
+    n) certificateName=${OPTARG} ;;
+    *) exitWithUsageInfo ;;
+    esac
+done
+
+if [[ -z $resourceGroupName ]] || [[ -z $certificateName ]]; then
+    exitWithUsageInfo
+fi
+
+if [[ ! -z $subscription ]]; then
+    az account set --subscription "$subscription"
+fi
+
+if [[ -z $keyVault ]]; then
+    . "${0%/*}/get-resource-names.sh"
+fi
+
+getCurrentUserDetails
+trap 'revokeUserAccessToKeyVault' EXIT
+
+loginToAzure
+grantUserAccessToKeyVault
+
+echo "Creating new version of certificate..."
+az keyvault certificate create --vault-name "$keyVault" --name "$certificateName" --policy "$(az keyvault certificate get-default-policy)" 1>/dev/null
+
+thumbprint=$(az keyvault certificate show --name "$certificateName" --vault-name "$keyVault" --query "x509ThumbprintHex" -o tsv)
+echo "Created new version of $certificateName certificate with thumbprint $thumbprint"
