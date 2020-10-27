98c63170d8bf534f6d1bb7f3f171fe21847476a7
Maxim Laikine <la.maks@hotmail.com>
2020-10-27 16:11:35 -0700

    Added tests

---
+++ packages/service-library/src/data-providers/data-providers-common.spec.ts
@@ -0,0 +1,38 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import 'reflect-metadata';
+
+import { IMock, Mock } from 'typemoq';
+import { GuidGenerator } from 'common';
+import { DataProvidersCommon } from './data-providers-common';
+
+let guidGeneratorMock: IMock<GuidGenerator>;
+let dataProvidersCommon: DataProvidersCommon;
+let fileCreatedTime: Date;
+
+describe(DataProvidersCommon, () => {
+    beforeEach(() => {
+        fileCreatedTime = new Date();
+        guidGeneratorMock = Mock.ofType<GuidGenerator>();
+        dataProvidersCommon = new DataProvidersCommon(guidGeneratorMock.object);
+    });
+
+    afterEach(() => {
+        guidGeneratorMock.verifyAll();
+    });
+
+    it('create report blob name', () => {
+        const fileId = 'fileId';
+        guidGeneratorMock
+            .setup((o) => o.getGuidTimestamp(fileId))
+            .returns(() => fileCreatedTime)
+            .verifiable();
+        const expectedBlobName = `${fileCreatedTime.getUTCFullYear()}/${
+            fileCreatedTime.getUTCMonth() + 1
+        }/${fileCreatedTime.getUTCDate()}/${fileCreatedTime.getUTCHours()}/${fileId}`;
+
+        const actualBlobName = dataProvidersCommon.getReportBlobName(fileId);
+
+        expect(actualBlobName).toEqual(expectedBlobName);
+    });
+});
