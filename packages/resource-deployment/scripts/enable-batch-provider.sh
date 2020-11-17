a3d2248cab9efcb112be85c9d9f3adf0df5e5043
AhmedAbdoOrtiga <45858632+AhmedAbdoOrtiga@users.noreply.github.com>
2020-11-17 12:23:40 -0800

    feat (PME-migration): Update tenant Id and Microsoft Batch object Id (#1132)

--- packages/resource-deployment/scripts/enable-batch-provider.sh
+++ packages/resource-deployment/scripts/enable-batch-provider.sh
@@ -7,6 +7,27 @@ set -eo pipefail
 
 # The script will configure the default Azure subscription account to support Batch user subscription mode
 
+exitWithUsageInfo() {
+    echo "
+Usage: $0 -e <environment>
+"
+    exit 1
+}
+
+# Read script arguments
+while getopts ":e:" option; do
+    case $option in
+    e) environment=${OPTARG} ;;
+    *) exitWithUsageInfo ;;
+    esac
+done
+
+# Print script usage help
+if [[ -z $environment ]]; then
+    exitWithUsageInfo
+fi
+
+
 # Get the default subscription
 subscription=$(az account show --query "id" -o tsv)
 
@@ -39,8 +60,23 @@ fi
 # Allow Azure Batch service to access the subscription
 #   Name: Microsoft Azure Batch
 #   Application ID: ddbf3205-c6bd-46ae-8127-60eb93363864
-#   Object ID: f520d84c-3fd3-4cc8-88d4-2ed25b00d27a
-roleDefinitionName=$(az role assignment list --query "[?principalId=='f520d84c-3fd3-4cc8-88d4-2ed25b00d27a'].roleDefinitionName" -o tsv)
+#   Object ID: 
+#               - Microsoft: f520d84c-3fd3-4cc8-88d4-2ed25b00d27a
+#               - PME: 8ad17ea0-4c88-4465-b8ec-a827df84f896
+
+
+
+
+# Microsoft AAD tenant when env is not ppe or prod
+objectId='f520d84c-3fd3-4cc8-88d4-2ed25b00d27a'
+
+# PME AAD tenant when env is ppe or prod
+if [ $environment = "prod" ] || [ $environment = "ppe" ]; then
+    objectId='8ad17ea0-4c88-4465-b8ec-a827df84f896'
+fi
+
+
+roleDefinitionName=$(az role assignment list --query "[?principalId=='$objectId'].roleDefinitionName" -o tsv)
 if [[ $roleDefinitionName != "Contributor" ]]; then
     echo "Granting Azure Batch service access to the '$subscription' Azure subscription"
     az role assignment create --assignee ddbf3205-c6bd-46ae-8127-60eb93363864 --role contributor 1>/dev/null
