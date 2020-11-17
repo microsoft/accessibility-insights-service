abc87504f9b4da9c8852ea151c3a4beda86ba18b
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2020-11-16 18:30:13 -0800

    fix azure batch object id

--- packages/resource-deployment/scripts/enable-batch-provider.sh
+++ packages/resource-deployment/scripts/enable-batch-provider.sh
@@ -39,8 +39,23 @@ fi
 # Allow Azure Batch service to access the subscription
 #   Name: Microsoft Azure Batch
 #   Application ID: ddbf3205-c6bd-46ae-8127-60eb93363864
-#   Object ID: f520d84c-3fd3-4cc8-88d4-2ed25b00d27a
-roleDefinitionName=$(az role assignment list --query "[?principalId=='f520d84c-3fd3-4cc8-88d4-2ed25b00d27a'].roleDefinitionName" -o tsv)
+#   Object ID: 
+#               - Microsoft: f520d84c-3fd3-4cc8-88d4-2ed25b00d27a
+#               - PME: 8ad17ea0-4c88-4465-b8ec-a827df84f896
+
+
+
+
+# Microsoft directory in case if env is not ppe or prod
+objectId = 'f520d84c-3fd3-4cc8-88d4-2ed25b00d27a'
+
+#PME directory in case if env is ppe or prod
+if [ $environment = "prod" ] || [ $environment = "ppe" ]; then
+    objectId = '8ad17ea0-4c88-4465-b8ec-a827df84f896'
+fi
+
+
+roleDefinitionName=$(az role assignment list --query "[?principalId=='$objectId'].roleDefinitionName" -o tsv)
 if [[ $roleDefinitionName != "Contributor" ]]; then
     echo "Granting Azure Batch service access to the '$subscription' Azure subscription"
     az role assignment create --assignee ddbf3205-c6bd-46ae-8127-60eb93363864 --role contributor 1>/dev/null
