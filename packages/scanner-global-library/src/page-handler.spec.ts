a43de60610736faecec6d7ca6153bd85775daf6a
Maxim Laikine <40576301+lamaks@users.noreply.github.com>
2020-11-24 09:27:58 -0800

    Fixed puppeteer obsolete API `waitFor` warning message (#1145)
    * Fixed puppeteer warning message
    
    * Updated test

--- packages/scanner-global-library/src/page-handler.spec.ts
+++ packages/scanner-global-library/src/page-handler.spec.ts
@@ -32,7 +32,8 @@ describe(PageHandler, () => {
             .returns(() => Promise.resolve(1024))
             .verifiable(Times.exactly(minCheckBreakCount + 1));
         pageMock
-            .setup(async (o) => o.waitFor(checkIntervalMsecs))
+            // @ts-ignore
+            .setup(async (o) => o.waitForTimeout(checkIntervalMsecs))
             .returns(() => Promise.resolve())
             .verifiable(Times.exactly(minCheckBreakCount));
 
@@ -50,7 +51,8 @@ describe(PageHandler, () => {
             .returns(() => Promise.resolve(contentSize))
             .verifiable(Times.exactly(validationCallCount));
         pageMock
-            .setup(async (o) => o.waitFor(checkIntervalMsecs))
+            // @ts-ignore
+            .setup(async (o) => o.waitForTimeout(checkIntervalMsecs))
             .returns(() => Promise.resolve())
             .verifiable(Times.exactly(validationCallCount));
 
