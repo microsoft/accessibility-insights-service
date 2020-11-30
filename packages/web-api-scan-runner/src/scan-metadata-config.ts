992245a81699a6ed61c8ef8d578b2c8bb9262160
Maxim Laikine <40576301+lamaks@users.noreply.github.com>
2020-11-30 11:10:13 -0800

    Remove yargs from logger packge (#1152)

--- packages/web-api-scan-runner/src/scan-metadata-config.ts
+++ packages/web-api-scan-runner/src/scan-metadata-config.ts
@@ -1,13 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { inject, injectable } from 'inversify';
-import { loggerTypes } from 'logger';
-import { Arguments, Argv } from 'yargs';
+import { injectable } from 'inversify';
+import yargs, { Arguments, Argv } from 'yargs';
 import { ScanMetadata } from './types/scan-metadata';
 
 @injectable()
 export class ScanMetadataConfig {
-    constructor(@inject(loggerTypes.Argv) private readonly argvObj: Argv) {}
+    constructor(private readonly argvObj: Argv = yargs) {}
 
     public getConfig(): ScanMetadata {
         this.argvObj.env().demandOption(['id', 'url']);
