315f182134e1c3e6a2236350d442de9f56042387
Kelsey Pownell <kepownel@microsoft.com>
2020-12-03 14:15:29 -0800

    add combined results to WebsiteScanResult

--- packages/axe-result-converter/src/index.ts
+++ packages/axe-result-converter/src/index.ts
@@ -4,3 +4,4 @@ export * from './axe-result-types';
 export * from './scan-result-data';
 export { AxeResultsReducer } from './axe-results-reducer';
 export { CombinedReportDataConverter } from './combined-report-data-converter';
+export { AxeCoreResults, AxeResults } from './axe-result-types';
--- packages/storage-documents/src/website-scan-result.ts
+++ packages/storage-documents/src/website-scan-result.ts
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { AxeCoreResults, UrlCount } from 'axe-result-converter';
 import { StorageDocument } from './storage-document';
 import { ItemType } from './item-type';
 import { ReportFormat } from './on-demand-page-scan-result';
@@ -10,6 +11,7 @@ export interface WebsiteScanResult extends StorageDocument {
     baseUrl: string;
     pageScans?: PageScan[];
     reports?: WebsiteScanReport[];
+    websiteScanResults?: CombinedScanResult;
 }
 
 export interface WebsiteScanReport {
@@ -22,4 +24,9 @@ export interface PageScan {
     scanId: string;
     url: string;
     timestamp: string;
+}
+
+interface CombinedScanResult {
+    urlCount: UrlCount;
+    combinedAxeResults: AxeCoreResults;
 }
