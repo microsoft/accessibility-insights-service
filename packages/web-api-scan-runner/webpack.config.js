dee9a81888a4de60fe4f0812ad04f47bc4bc28e3
Dan Bjorge <danielbj@microsoft.com>
2020-12-17 16:46:52 -0500

    feat(axe-core-4.1.1): upgrade to axe-core 4.1.1 (including report support) (#1168)
    #### Description of changes
    
    This updates `axe-core`, `@axe-core/puppeteer`, and `accessibility-insights-report` in lockstep, including axe-core yarn resolutions.
    
    It adds the new axe-core best-practice rules (from the rule table linked in the feature description) to the rule-exclusion list.
    
    It also adds `accessibility-insights-report` as an external in the `web-api-scan-runner` webpack configuration; this means that rather than bundling it, the docker container will fetch the report package during its `npm install` phase. This prevents an error where the `report` package introduced a new runtime dependency which was not in turn treated as an external in the web-api-scan-runner; now, the scan runner will no longer need to care when the report package introduces new transitive deps.
    
    #### Pull request checklist
    
    - [x] Addresses an existing issue: Fixes 1797801
    - [n/a] Added relevant unit test for your changes. (`yarn test`)
    - [n/a] Verified code coverage for the changes made. Check coverage report at: `<rootDir>/test-results/unit/coverage`
    - [x] Ran precheckin (`yarn precheckin`)

--- packages/web-api-scan-runner/webpack.config.js
+++ packages/web-api-scan-runner/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env) => {
 
     return {
         devtool: 'cheap-source-map',
-        externals: ['puppeteer', 'yargs', 'axe-core', '@axe-core/puppeteer', 'applicationinsights'],
+        externals: ['puppeteer', 'yargs', 'axe-core', '@axe-core/puppeteer', 'applicationinsights', 'accessibility-insights-report'],
         entry: {
             ['web-api-scan-runner']: path.resolve('./src/index.ts'),
         },
