cc4c9125b7620c053c73467b5c02238bd5544a00
JGibson2019 <jacqueline.gibson@utexas.edu>
2020-12-21 16:36:48 -0800

    added e2e-web-apis folder and moved scan-notification-client-func there

---
+++ packages/e2e-web-apis/scan-notification-client-func/index.ts
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import 'reflect-metadata';
+
+import { Context } from '@azure/functions';
+
+export async function run(context: Context): Promise<void> {
+    context.res = {
+        status: 200,
+        body: 'Hello World!',
+    };
+    context.done();
+}
