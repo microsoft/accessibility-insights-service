8ce40ea1408795b94dda0d37d027a7370da423a3
Kelsey Pownell <kepownel@microsoft.com>
2021-01-12 11:03:12 -0800

    Set up and run Apify basic crawler in SimpleCrawlerEngine
    Co-authored-by: Kelsey Pownell <kepownel@microsoft.com>
    Co-authored-by: Brock Taylor <brocktaylor@microsoft.com>

--- packages/crawler/src/crawler/simple-crawler-engine.spec.ts
+++ packages/crawler/src/crawler/simple-crawler-engine.spec.ts
@@ -1,29 +1,116 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import Apify from 'apify';
+import { Page } from 'puppeteer';
 import 'reflect-metadata';
 
+import { IMock, It, Mock } from 'typemoq';
 import { CrawlerRunOptions } from '../types/crawler-run-options';
-import { SimpleCrawlerEngine } from './simple-crawler-engine';
+import { ApifyRequestQueueProvider } from '../types/ioc-types';
+import { CrawlerConfiguration } from './crawler-configuration';
+import { CrawlerFactory } from './crawler-factory';
+import { SimpleCrawlerEngine, SimpleCrawlerRunOptions } from './simple-crawler-engine';
 
 describe(SimpleCrawlerEngine, () => {
+
+    let requestQueueProviderMock: IMock<ApifyRequestQueueProvider>;
+    let crawlerFactoryMock: IMock<CrawlerFactory>;
+    let crawlerConfiguration: IMock<CrawlerConfiguration>;
+    let requestQueueStub: Apify.RequestQueue;
+    let basicCrawlerMock: IMock<Apify.BasicCrawler>;
+    let enqueueLinksExtMock: IMock<typeof Apify.utils.enqueueLinks>;
+
     let testSubject: SimpleCrawlerEngine;
     let crawlerRunOptions: CrawlerRunOptions;
 
-    beforeAll(() => {
-        testSubject = new SimpleCrawlerEngine();
+    const localOutputDir = 'local output dir';
+    const maxRequestsPerCrawl = 10;
+    const pageStub = {} as Page;
+    const discoveryPatterns = ['discovery pattern'];
+
+    beforeEach(() => {
+        requestQueueStub = {} as Apify.RequestQueue;
+        requestQueueProviderMock = Mock.ofInstance(() => null);
+        crawlerFactoryMock = Mock.ofType<CrawlerFactory>();
+        crawlerConfiguration = Mock.ofType<CrawlerConfiguration>();
+        basicCrawlerMock = Mock.ofType<Apify.BasicCrawler>();
+        enqueueLinksExtMock = Mock.ofType<typeof Apify.utils.enqueueLinks>();
+
+        testSubject = new SimpleCrawlerEngine(
+            requestQueueProviderMock.object,
+            crawlerFactoryMock.object,
+            crawlerConfiguration.object,
+            enqueueLinksExtMock.object,
+        );
         crawlerRunOptions = {
-            localOutputDir: 'localOutputDir',
+            localOutputDir: localOutputDir,
             memoryMBytes: 100,
             silentMode: true,
             debug: false,
-        } as CrawlerRunOptions;
+            page: pageStub,
+        } as SimpleCrawlerRunOptions;
     });
 
-    it('returns list of urls', async () => {
+    afterEach(() => {
+        crawlerConfiguration.verifyAll();
+        crawlerFactoryMock.verifyAll();
+        requestQueueProviderMock.verifyAll();
+        basicCrawlerMock.verifyAll();
+        enqueueLinksExtMock.verifyAll();
+    });
+
+    it.each([true, false])('returns list of urls with debug = %s', async (debug) => {
+        crawlerRunOptions.debug = debug;
+        setupCrawlerConfig();
+        if (debug) {
+            crawlerConfiguration.setup(cc => cc.setSilentMode(false)).verifiable();
+        }
+        requestQueueProviderMock.setup(rqp => rqp()).returns(() => Promise.resolve(requestQueueStub)).verifiable();
+
+        const expectedCrawlOptions = getCrawlerOptions(debug);
+        crawlerFactoryMock
+            .setup(cf => cf.createBasicCrawler(It.isObjectWith(expectedCrawlOptions)))
+            .returns(() => basicCrawlerMock.object)
+            .verifiable();
+        basicCrawlerMock.setup(bc => bc.run()).verifiable();
+        setupEnqueueLinks();
+
         const expectedUrls: string[] = [];
 
         const discoveredUrls = await testSubject.start(crawlerRunOptions);
 
         expect(discoveredUrls).toEqual(expectedUrls);
     });
+
+    function setupCrawlerConfig(): void {
+        crawlerConfiguration.setup(cc => cc.setDefaultApifySettings()).verifiable();
+        crawlerConfiguration.setup(cc => cc.setLocalOutputDir(localOutputDir)).verifiable();
+        crawlerConfiguration.setup(cc => cc.setMemoryMBytes(crawlerRunOptions.memoryMBytes)).verifiable();
+        crawlerConfiguration.setup(cc => cc.setSilentMode(crawlerRunOptions.silentMode)).verifiable();
+        crawlerConfiguration.setup(cc => cc.maxRequestsPerCrawl()).returns(() => maxRequestsPerCrawl);
+    }
+
+    function getCrawlerOptions(debug: boolean): Partial<Apify.BasicCrawlerOptions> {
+        const options: Partial<Apify.BasicCrawlerOptions> = {
+            handleRequestTimeoutSecs: 300,
+            requestQueue: requestQueueStub,
+            maxRequestsPerCrawl: maxRequestsPerCrawl,
+        };
+
+        if (debug) {
+            options.maxConcurrency = 1;
+        }
+
+        return options;
+    }
+
+    function setupEnqueueLinks(): void {
+        const expectedOptions = {
+            page: pageStub,
+            requestQueue: requestQueueStub,
+            pseudoUrls: discoveryPatterns,
+        };
+        crawlerConfiguration.setup(cc => cc.discoveryPatterns()).returns(() => discoveryPatterns);
+        enqueueLinksExtMock.setup(el => el(expectedOptions)).verifiable();
+    }
 });
--- packages/crawler/src/crawler/simple-crawler-engine.ts
+++ packages/crawler/src/crawler/simple-crawler-engine.ts
@@ -1,14 +1,69 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 // @ts-ignore
 import * as cheerio from 'cheerio';
+import Apify from 'apify';
+import { Page } from 'puppeteer';
+import { isNil } from 'lodash';
 import { CrawlerRunOptions } from '../types/crawler-run-options';
+import { ApifyRequestQueueProvider, iocTypes } from '../types/ioc-types';
 import { CrawlerEngine } from './crawler-engine';
+import { CrawlerFactory } from './crawler-factory';
+import { CrawlerConfiguration } from './crawler-configuration';
+
+export type SimpleCrawlerRunOptions = CrawlerRunOptions & {
+    page?: Page;
+};
 
 @injectable()
 export class SimpleCrawlerEngine implements CrawlerEngine<string[]> {
-    public async start(crawlerRunOptions: CrawlerRunOptions): Promise<string[]> {
+    public constructor(
+        @inject(iocTypes.ApifyRequestQueueProvider) protected readonly requestQueueProvider: ApifyRequestQueueProvider,
+        @inject(CrawlerFactory) private readonly crawlerFactory: CrawlerFactory,
+        @inject(CrawlerConfiguration) private readonly crawlerConfiguration: CrawlerConfiguration,
+        private readonly enqueueLinksExt: typeof Apify.utils.enqueueLinks = Apify.utils.enqueueLinks,
+    ) {}
+
+    public async start(crawlerRunOptions: SimpleCrawlerRunOptions): Promise<string[]> {
+        this.crawlerConfiguration.setDefaultApifySettings();
+        this.crawlerConfiguration.setLocalOutputDir(crawlerRunOptions.localOutputDir);
+        this.crawlerConfiguration.setMemoryMBytes(crawlerRunOptions.memoryMBytes);
+        this.crawlerConfiguration.setSilentMode(crawlerRunOptions.silentMode);
+
+        const requestQueue = await this.requestQueueProvider();
+
+        const basicCrawlerOptions: Apify.BasicCrawlerOptions = {
+            handleRequestTimeoutSecs: 300,
+            requestQueue: requestQueue,
+            handleRequestFunction: () => null, //TODO: implement
+            handleFailedRequestFunction: () => null,
+            maxRequestsPerCrawl: this.crawlerConfiguration.maxRequestsPerCrawl(),
+        };
+
+        if (crawlerRunOptions.debug === true) {
+            this.crawlerConfiguration.setSilentMode(false);
+
+            basicCrawlerOptions.maxConcurrency = 1;
+        }
+
+        this.prepareQueue(requestQueue, crawlerRunOptions);
+
+        const crawler = this.crawlerFactory.createBasicCrawler(basicCrawlerOptions);
+        await crawler.run();
+
         return [];
+    }
+
+    private async prepareQueue(requestQueue: Apify.RequestQueue, crawlerRunOptions: SimpleCrawlerRunOptions): Promise<void> {
+        if (!isNil(crawlerRunOptions.page)) {
+            const discoveryPatterns = this.crawlerConfiguration.discoveryPatterns();
+
+            await this.enqueueLinksExt({
+                page: crawlerRunOptions.page,
+                requestQueue: requestQueue,
+                pseudoUrls: discoveryPatterns,
+            });
+        }
     }
 }
