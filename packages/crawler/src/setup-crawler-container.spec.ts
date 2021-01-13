63bda955e2b88747644e7922053232d3e888ac2f
Kelsey Pownell <kepownel@microsoft.com>
2021-01-13 14:51:34 -0800

    Add function to register cloud container dependencies Co-authored-by: Kelsey Pownell <kepownel@microsoft.com> Co-authored-by: Brock Taylor <brocktaylor@microsoft.com>

--- packages/crawler/src/setup-crawler-container.spec.ts
+++ packages/crawler/src/setup-crawler-container.spec.ts
@@ -3,9 +3,10 @@
 import 'reflect-metadata';
 
 import * as inversify from 'inversify';
+import { registerLoggerToContainer } from 'logger';
 import { CrawlerConfiguration } from './crawler/crawler-configuration';
 import { DataBase } from './level-storage/data-base';
-import { setupLocalCrawlerContainer } from './setup-crawler-container';
+import { setupCloudCrawlerContainer, setupLocalCrawlerContainer } from './setup-crawler-container';
 import { CrawlerRunOptions } from './types/crawler-run-options';
 import { iocTypes } from './types/ioc-types';
 
@@ -30,6 +31,28 @@ describe(setupLocalCrawlerContainer, () => {
         expect(container.get(iocTypes.ReporterFactory)).toBeDefined();
         expect(container.get(iocTypes.ApifyRequestQueueProvider)).toBeDefined();
         expect(container.get(iocTypes.PageProcessorFactory)).toBeDefined();
+        expect(container.get(iocTypes.CrawlerEngine)).toBeDefined();
+    });
+});
+
+describe(setupCloudCrawlerContainer, () => {
+    it('resolves dependencies', () => {
+        const crawlerRunOptions = {
+            baseUrl: 'baseUrl',
+            restartCrawl: false,
+            inputFile: undefined,
+            existingUrls: undefined,
+            simulate: false,
+        } as CrawlerRunOptions;
+
+        const container = new inversify.Container({ autoBindInjectable: true });
+        registerLoggerToContainer(container);
+        setupCloudCrawlerContainer(container);
+        container.bind(iocTypes.CrawlerRunOptions).toConstantValue(crawlerRunOptions);
+
+        expect(container.get(CrawlerConfiguration)).toBeDefined();
+        expect(container.get(iocTypes.ApifyRequestQueueProvider)).toBeDefined();
+        expect(container.get(iocTypes.RequestProcessor)).toBeDefined();
         expect(container.get(iocTypes.CrawlerEngine)).toBeDefined();
     });
 });
