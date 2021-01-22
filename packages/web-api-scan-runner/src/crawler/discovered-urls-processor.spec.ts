7c7ff8a46beba0a8f4a56328e05e46d108642426
Kelsey Pownell <kepownel@microsoft.com>
2021-01-22 12:07:28 -0800

    code cleanup, export function instead of creating a class

--- packages/web-api-scan-runner/src/crawler/discovered-urls-processor.spec.ts
+++ packages/web-api-scan-runner/src/crawler/discovered-urls-processor.spec.ts
@@ -3,9 +3,9 @@
 import 'reflect-metadata';
 
 import { WebsiteScanResult } from 'storage-documents';
-import { DiscoveredUrlsProcessor } from './discovered-urls-processor';
+import { processDiscoveredUrls } from './discovered-urls-processor';
 
-describe(DiscoveredUrlsProcessor, () => {
+describe(processDiscoveredUrls, () => {
     const urlsList = ['url1', 'url2', 'url3', 'url4'];
     const knownUrls = ['url1', 'url2', 'anotherUrl'];
     const maxUrlsLimit = 10;
@@ -13,16 +13,10 @@ describe(DiscoveredUrlsProcessor, () => {
         knownPages: knownUrls,
     } as WebsiteScanResult;
 
-    let testSubject: DiscoveredUrlsProcessor;
-
-    beforeEach(() => {
-        testSubject = new DiscoveredUrlsProcessor();
-    });
-
     it('filters out known urls', () => {
         const expectedUrls = ['url3', 'url4'];
 
-        const processedUrls = testSubject.getProcessedUrls(urlsList, maxUrlsLimit, websiteScanResultStub);
+        const processedUrls = processDiscoveredUrls(urlsList, maxUrlsLimit, websiteScanResultStub);
 
         expect(processedUrls).toEqual(expectedUrls);
     });
@@ -30,7 +24,7 @@ describe(DiscoveredUrlsProcessor, () => {
     it('limits the number of urls according to config and count of knownUrls', () => {
         const urlCrawlLimit = 3;
 
-        const processedUrls = testSubject.getProcessedUrls(urlsList, urlCrawlLimit, { knownPages: ['some url'] } as WebsiteScanResult);
+        const processedUrls = processDiscoveredUrls(urlsList, urlCrawlLimit, { knownPages: ['some url'] } as WebsiteScanResult);
 
         expect(processedUrls.length).toBe(2);
     });
@@ -38,20 +32,20 @@ describe(DiscoveredUrlsProcessor, () => {
     it('filters and applies limit in correct order', () => {
         const urlCrawlLimit = knownUrls.length + 1;
 
-        const processedUrls = testSubject.getProcessedUrls(urlsList, urlCrawlLimit, websiteScanResultStub);
+        const processedUrls = processDiscoveredUrls(urlsList, urlCrawlLimit, websiteScanResultStub);
 
         expect(processedUrls.length).toBe(1);
         expect(knownUrls).not.toContain(processedUrls[0]);
     });
 
     it('handles missing websiteScanResults', () => {
-        const processedUrls = testSubject.getProcessedUrls(urlsList, maxUrlsLimit, null);
+        const processedUrls = processDiscoveredUrls(urlsList, maxUrlsLimit, null);
 
         expect(processedUrls).toEqual(urlsList);
     });
 
     it('handles missing knownPages', () => {
-        const processedUrls = testSubject.getProcessedUrls(urlsList, maxUrlsLimit, {} as WebsiteScanResult);
+        const processedUrls = processDiscoveredUrls(urlsList, maxUrlsLimit, {} as WebsiteScanResult);
 
         expect(processedUrls).toEqual(urlsList);
     });
@@ -59,7 +53,7 @@ describe(DiscoveredUrlsProcessor, () => {
     it('handles knownUrls.length > urlCrawlLimit', () => {
         const urlCrawlLimit = knownUrls.length - 1;
 
-        const processedUrls = testSubject.getProcessedUrls(urlsList, urlCrawlLimit, websiteScanResultStub);
+        const processedUrls = processDiscoveredUrls(urlsList, urlCrawlLimit, websiteScanResultStub);
 
         expect(processedUrls.length).toBe(0);
     });
--- packages/web-api-scan-runner/src/crawler/discovered-urls-processor.ts
+++ packages/web-api-scan-runner/src/crawler/discovered-urls-processor.ts
@@ -1,37 +1,32 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { injectable } from 'inversify';
 import _, { isNil } from 'lodash';
 import { WebsiteScanResult } from 'storage-documents';
 
-@injectable()
-export class DiscoveredUrlsProcessor {
-    public getProcessedUrls(discoveredUrls: string[], urlCrawlLimit: number, websiteScanResult?: WebsiteScanResult): string[] {
-        let processedUrls = discoveredUrls;
-        if (isNil(discoveredUrls)) {
-            return [];
-        }
-
-        if (!isNil(websiteScanResult?.knownPages)) {
-            processedUrls = this.removeUrlsFromList(discoveredUrls, websiteScanResult.knownPages);
-        }
-        processedUrls = this.limitNumUrls(processedUrls, this.getUrlLimit(urlCrawlLimit, websiteScanResult));
-
-        return processedUrls;
+export function processDiscoveredUrls(discoveredUrls: string[], urlCrawlLimit: number, websiteScanResult?: WebsiteScanResult): string[] {
+    let processedUrls = discoveredUrls;
+    if (isNil(discoveredUrls)) {
+        return [];
     }
 
-    private removeUrlsFromList(urlList: string[], removeUrls: string[]): string[] {
-        return _.pullAll(urlList, removeUrls);
+    if (!isNil(websiteScanResult?.knownPages)) {
+        processedUrls = removeUrlsFromList(discoveredUrls, websiteScanResult.knownPages);
     }
 
-    private limitNumUrls(urlList: string[], numUrls: number): string[] {
-        return _.take(urlList, numUrls);
-    }
+    return limitNumUrls(processedUrls, getUrlLimit(urlCrawlLimit, websiteScanResult));
+}
 
-    private getUrlLimit(urlCrawlLimit: number, websiteScanResult?: WebsiteScanResult): number {
-        const numKnownPages = isNil(websiteScanResult?.knownPages) ? 0 : websiteScanResult.knownPages.length;
+function removeUrlsFromList(urlList: string[], removeUrls: string[]): string[] {
+    return _.pullAll(urlList, removeUrls);
+}
 
-        return Math.max(urlCrawlLimit - numKnownPages, 0);
-    }
+function limitNumUrls(urlList: string[], numUrls: number): string[] {
+    return _.take(urlList, numUrls);
+}
+
+function getUrlLimit(urlCrawlLimit: number, websiteScanResult?: WebsiteScanResult): number {
+    const numKnownPages = isNil(websiteScanResult?.knownPages) ? 0 : websiteScanResult.knownPages.length;
+
+    return Math.max(urlCrawlLimit - numKnownPages, 0);
 }
