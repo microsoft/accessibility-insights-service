25b5ac4db0976ce026c6dd5a239166c904cf78dc
karan <karansin@microsoft.com>
2021-02-10 14:10:34 -0800

    remove ws-endpoint-fetcher for now

--- packages/web-api-scan-runner/src/runner/ws-endpoint-fetcher.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-import got, { Agents, Got, Options } from 'got';
-
-export const fetchWSEndpoint = (): string => {
-    return '';
-};
