47c1d2ad2cfa02414b026dc65963e8a1995c7257
Kelsey Pownell <kepownel@microsoft.com>
2021-03-04 17:06:18 -0800

    increase memory to chromium

--- packages/scanner-global-library/src/puppeteer-options.ts
+++ packages/scanner-global-library/src/puppeteer-options.ts
@@ -12,6 +12,6 @@ export const defaultBrowserOptions: Puppeteer.BrowserOptions = {
 
 export const defaultLaunchOptions: Puppeteer.LaunchOptions = {
     headless: true,
-    args: ['--disable-dev-shm-usage', '--no-sandbox', '--disable-setuid-sandbox'],
+    args: ['--disable-dev-shm-usage', '--no-sandbox', '--disable-setuid-sandbox', '--js-flags=--max-old-space-size=8192'],
     ...defaultBrowserOptions,
 };
