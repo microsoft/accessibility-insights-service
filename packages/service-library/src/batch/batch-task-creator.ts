2d1eb25987664c256b3f7166e8e5e824ee9bba53
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2021-03-05 13:47:14 -0800

    lint fix

--- packages/service-library/src/batch/batch-task-creator.ts
+++ packages/service-library/src/batch/batch-task-creator.ts
@@ -46,7 +46,7 @@ export abstract class BatchTaskCreator {
 
         this.activeScanMessages = [];
         const restartAfterTime = moment()
-            .add(this.jobManagerConfig.maxWallClockTimeInHours / 2.0, 'hour')
+            .add(this.jobManagerConfig.maxWallClockTimeInHours / 2, 'hour')
             .toDate();
 
         // eslint-disable-next-line no-constant-condition
