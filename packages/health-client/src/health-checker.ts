9d6d1737ccc842248a5e2ffa19139253b12ea766
Maxim Laikine <40576301+lamaks@users.noreply.github.com>
2021-05-10 17:15:19 -0700

    Update packages/health-client/src/health-checker.ts

--- packages/health-client/src/health-checker.ts
+++ packages/health-client/src/health-checker.ts
@@ -19,7 +19,7 @@ type Argv = {
     evaluationIntervalInMinutes: string;
     releaseId: string;
     baseUrl: string;
-    reportDownloadDest: string;
+    reportDownloadLocation: string;
 };
 
 const testTimeoutInMinutes = 75;
