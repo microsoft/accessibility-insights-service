61f30650d7ceec4fa38c43d271c8c70e952f34b9
Dan Bjorge <danielbj@microsoft.com>
2021-05-18 19:10:27 -0400

    Use top-level axe-core by default, not axe-core/puppeteer's version

--- packages/scanner-global-library/src/factories/axe-puppeteer-factory.ts
+++ packages/scanner-global-library/src/factories/axe-puppeteer-factory.ts
@@ -18,16 +18,27 @@ export class AxePuppeteerFactory {
     ) {}
 
     public async createAxePuppeteer(page: Puppeteer.Page, contentSourcePath?: string): Promise<AxePuppeteer> {
-        let maybeContentSource: string;
+        const axeSource = await this.getAxeSource(contentSourcePath);
 
-        if (!isEmpty(contentSourcePath)) {
-            // eslint-disable-next-line security/detect-non-literal-fs-filename
-            const contentBuffer = this.fileSystemObj.readFileSync(contentSourcePath);
-            maybeContentSource = contentBuffer.toString();
-        }
-
-        return new AxePuppeteer(page, maybeContentSource)
+        return new AxePuppeteer(page, axeSource)
             .configure(this.axeConfiguration)
             .disableRules(this.ruleExclusion.accessibilityRuleExclusionList);
+    }
+
+    private async getAxeSource(contentSourcePath?: string): Promise<string> {
+        if (isEmpty(contentSourcePath)) {
+            // It's important that we default to this rather than allowing axe-puppeteer to use its
+            // own default because axe-puppeteer uses a transitive dependency like "axe-core": "^1.2.3",
+            // and we want to to instead force the use of the specific axe-core version we and the report
+            // package are pinned to.
+            //
+            // We can't rely on a resolution/lockfile for this because it's still true from the context
+            // of an external consumer of the CLI package.
+            return (await import('axe-core')).source;
+        } else {
+            // eslint-disable-next-line security/detect-non-literal-fs-filename
+            const contentBuffer = this.fileSystemObj.readFileSync(contentSourcePath);
+            return contentBuffer.toString();
+        }
     }
 }
