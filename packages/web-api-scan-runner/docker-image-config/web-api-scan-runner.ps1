fd7f5f8945152458a36c3684b5714e44b552468c
Maxim Laikine <mlaikine@microsoft.com>
2021-05-19 23:26:42 -0700

    Convert to multi-stage docker build to use fonts source

--- packages/web-api-scan-runner/docker-image-config/web-api-scan-runner.ps1
+++ packages/web-api-scan-runner/docker-image-config/web-api-scan-runner.ps1
@@ -1,8 +1,10 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-Write-Host "adding route for Managed Service Identity"
+
+Write-Host "Adding route for Managed Service Identity"
 $gateway = (route print | ?{$_ -like "*0.0.0.0*0.0.0.0*"} | %{$_ -split " "} | ?{$_.trim() -ne "" } | ?{$_ -ne "0.0.0.0" })[0]
 $arguments = 'add','169.254.169.0','mask','255.255.255.0',$gateway
 &'route' $arguments
 
+Write-Host "Starting runner..."
 node ./web-api-scan-runner.js
