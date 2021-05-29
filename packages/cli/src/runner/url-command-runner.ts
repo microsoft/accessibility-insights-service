e137720ca376e6be141bb7d8ad8bfddc7dad0b77
Maxim Laikine <la.maks@hotmail.com>
2021-05-28 20:26:09 -0700

    Fix axe error handling

--- packages/cli/src/runner/url-command-runner.ts
+++ packages/cli/src/runner/url-command-runner.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+
 import { Spinner } from 'cli-spinner';
 import { inject, injectable } from 'inversify';
-import { AxeScanResults } from 'scanner-global-library';
 import { ReportDiskWriter } from '../report/report-disk-writer';
 import { ReportGenerator } from '../report/report-generator';
 import { AIScanner } from '../scanner/ai-scanner';
@@ -19,17 +19,19 @@ export class UrlCommandRunner implements CommandRunner {
 
     public async runCommand(scanArguments: ScanArguments): Promise<void> {
         const spinner = new Spinner(`Running scanner... %s \t`);
-        let axeResults: AxeScanResults;
 
         try {
             spinner.start();
 
-            axeResults = await this.scanner.scan(scanArguments.url);
+            const axeResults = await this.scanner.scan(scanArguments.url);
+            if (axeResults.error !== undefined) {
+                throw new Error(JSON.stringify(axeResults.error));
+            }
+
+            const reportContent = this.reportGenerator.generateReport(axeResults);
+            this.reportDiskWriter.writeToDirectory(scanArguments.output, scanArguments.url, 'html', reportContent);
         } finally {
             spinner.stop();
         }
-
-        const reportContent = this.reportGenerator.generateReport(axeResults);
-        this.reportDiskWriter.writeToDirectory(scanArguments.output, scanArguments.url, 'html', reportContent);
     }
 }
