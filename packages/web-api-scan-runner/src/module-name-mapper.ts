242379b33f465dcfa15359e5857cacac4bc70589
Maxim Laikine <la.maks@hotmail.com>
2021-05-28 19:35:40 -0700

    Fix package path under package content only

--- packages/web-api-scan-runner/src/module-name-mapper.ts
+++ packages/web-api-scan-runner/src/module-name-mapper.ts
@@ -9,14 +9,20 @@ moduleRef._resolveFilename = new Proxy(moduleRef._resolveFilename, {
     apply(target: any, thisArg: any, argumentsList: any): any {
         const moduleName = argumentsList[0] as string;
         let path = Reflect.apply(target, thisArg, argumentsList) as string;
-        if (moduleName.startsWith('@uifabric/styling')) {
-            if (require('os').type() === 'Windows_NT') {
-                path = path.replace('\\lib\\', '\\lib-commonjs\\');
-            } else {
-                path = path.replace('/lib/', '/lib-commonjs/');
-            }
+        if (moduleName.indexOf('@uifabric/styling') >= 0) {
+            path = fixModulePath('@uifabric', path);
         }
 
         return path;
     },
 });
+
+function fixModulePath(moduleName: string, path: string): string {
+    // fix path of the package content only
+    const index = path.lastIndexOf(moduleName);
+    if (require('os').type() === 'Windows_NT') {
+        return path.slice(0, index) + path.slice(index).replace('\\lib\\', '\\lib-commonjs\\');
+    } else {
+        return path.slice(0, index) + path.slice(index).replace('/lib/', '/lib-commonjs/');
+    }
+}
