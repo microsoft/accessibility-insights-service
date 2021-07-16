36f611544d7aa1c3dba6566cc7ea798cb74c04b9
Maxim Laikine <40576301+lamaks@users.noreply.github.com>
2021-07-16 16:53:19 -0700

    Fallback to load partial page resources (#1564)
    * Fallback to load partial page resources
    
    * Rewert import

--- packages/scanner-global-library/src/page-navigator.ts
+++ packages/scanner-global-library/src/page-navigator.ts
@@ -1,13 +1,15 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { Page, Response } from 'puppeteer';
+import { Page, Response, LoadEvent } from 'puppeteer';
 import { injectable, inject } from 'inversify';
-import _ from 'lodash';
+import { isNil } from 'lodash';
 import { PageConfigurator } from './page-configurator';
 import { PageResponseProcessor } from './page-response-processor';
 import { BrowserError } from './browser-error';
 import { PageHandler } from './page-handler';
+
+export type OnNavigationError = (browserError: BrowserError, error?: unknown) => Promise<void>;
 
 @injectable()
 export class PageNavigator {
@@ -23,28 +25,30 @@ export class PageNavigator {
         @inject(PageHandler) protected readonly pageRenderingHandler: PageHandler,
     ) {}
 
-    public async navigate(
-        url: string,
-        page: Page,
-        onNavigationError: (browserError: BrowserError, error?: unknown) => Promise<void> = () => Promise.resolve(),
-    ): Promise<Response> {
+    public async navigate(url: string, page: Page, onNavigationError: OnNavigationError = () => Promise.resolve()): Promise<Response> {
         // Configure page settings before navigating to URL
         await this.pageConfigurator.configurePage(page);
 
-        let response: Response;
-        try {
-            response = await page.goto(url, {
-                waitUntil: 'networkidle0',
-                timeout: this.gotoTimeoutMsecs,
-            });
-        } catch (err) {
-            const navigationError = this.pageResponseProcessor.getNavigationError(err as Error);
-            onNavigationError(navigationError, err);
+        // Try load all page resources
+        let navigationResult = await this.navigateToUrl(url, page, 'networkidle0');
+        if (navigationResult.browserError?.errorType === 'UrlNavigationTimeout') {
+            // Fallback to load partial page resources on navigation timeout.
+            // This will help in cases when page has a streaming video controls.
+            //
+            // The 'load' event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+            // However any dynamic contents may not be available if it is loaded after window.onload() event.
+            // Since we reuse page instance from the first navigation attempt some contents could be already loaded and available which
+            // mitigates dynamic content rendering issue above.
+            navigationResult = await this.navigateToUrl(url, page, 'load');
+        }
+
+        if (!isNil(navigationResult.browserError)) {
+            onNavigationError(navigationResult.browserError, navigationResult.error);
 
             return undefined;
         }
 
-        if (_.isNil(response)) {
+        if (isNil(navigationResult.response)) {
             onNavigationError({
                 errorType: 'NavigationError',
                 message: 'Unable to get a page response from the browser.',
@@ -55,7 +59,7 @@ export class PageNavigator {
         }
 
         // Validate HTTP response
-        const responseError = this.pageResponseProcessor.getResponseError(response);
+        const responseError = this.pageResponseProcessor.getResponseError(navigationResult.response);
         if (responseError !== undefined) {
             onNavigationError(responseError);
 
@@ -64,6 +68,28 @@ export class PageNavigator {
 
         await this.pageRenderingHandler.waitForPageToCompleteRendering(page, this.pageRenderingTimeoutMsecs);
 
-        return response;
+        return navigationResult.response;
+    }
+
+    private async navigateToUrl(
+        url: string,
+        page: Page,
+        condition: LoadEvent,
+    ): Promise<{ response: Response; browserError?: BrowserError; error?: unknown }> {
+        let response: Response;
+        let browserError: BrowserError;
+        try {
+            const options = {
+                waitUntil: condition,
+                timeout: this.gotoTimeoutMsecs,
+            };
+            response = await page.goto(url, options);
+
+            return { response };
+        } catch (error) {
+            browserError = this.pageResponseProcessor.getNavigationError(error as Error);
+
+            return { response, browserError, error };
+        }
     }
 }
