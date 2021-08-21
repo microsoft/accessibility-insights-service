263dbeb10fbe4492205d8281611d665923f0520c
Maxim Laikine <la.maks@hotmail.com>
2021-08-21 10:46:30 -0700

    Combine dependabot's prs. Fix TSA issues.

--- packages/service-library/src/web-api/scan-run-error-codes.ts
+++ packages/service-library/src/web-api/scan-run-error-codes.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-/* eslint-disable @typescript-eslint/no-extraneous-class, @typescript-eslint/naming-convention,no-underscore-dangle,id-blacklist,id-match */
+/* eslint-disable @typescript-eslint/no-extraneous-class, @typescript-eslint/naming-convention,no-underscore-dangle,id-match */
 
 export declare type ScanRunErrorCodeName =
     | 'InternalError'
