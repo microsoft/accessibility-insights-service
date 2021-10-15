242a1e3747c13d3ec6d23b62eb637b03a7925e19
Maxim Laikine <la.maks@hotmail.com>
2021-10-15 12:00:33 -0700

    Update packages

--- packages/service-library/src/data-providers/combined-scan-results-provider.spec.ts
+++ packages/service-library/src/data-providers/combined-scan-results-provider.spec.ts
@@ -157,9 +157,7 @@ describe(CombinedScanResultsProvider, () => {
             const actualResults = await testSubject.readCombinedResults(fileId);
 
             expect(actualResults.error.errorCode).toEqual('jsonParseError');
-            expect(actualResults.error.data).toStartWith(
-                `{\n  name: 'SyntaxError',\n  message: 'Unexpected token u in JSON at position 2',`,
-            );
+            expect(actualResults.error.data).toContain(`{\n  name: 'SyntaxError',\n  message: 'Unexpected token u in JSON at position 2',`);
         });
     });
 
