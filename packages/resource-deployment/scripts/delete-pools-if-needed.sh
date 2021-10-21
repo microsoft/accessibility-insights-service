582289c2982b9a3c463e0409491d5efbf5bd54cf
pownkel <55459788+pownkel@users.noreply.github.com>
2021-10-21 15:37:02 -0700

    chore: update VMs to Ubuntu 20.04 (#1791)
    * use ubuntu 20.04 image instead of 16.04
    
    * azsecpack tag
    
    * Revert "azsecpack tag"
    
    This reverts commit 17bf38dd38b1622a6eb58997f2ba827c9b5ebaf5.

--- packages/resource-deployment/scripts/delete-pools-if-needed.sh
+++ packages/resource-deployment/scripts/delete-pools-if-needed.sh
@@ -177,7 +177,7 @@ function deletePoolsWhenNodesAreIdle() {
     local commandName="Delete pool VMSS"
     . "${0%/*}/run-command-when-batch-nodes-are-idle.sh"
 
-    echo "Successfully deleted Btach pools"
+    echo "Successfully deleted Batch pools"
 }
 
 function deletePoolsIfNeeded() {
