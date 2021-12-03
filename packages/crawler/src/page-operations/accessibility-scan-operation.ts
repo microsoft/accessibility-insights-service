9e29da677db09a7e2e294c3f53124317db624e52
Maxim Laikine <40576301+lamaks@users.noreply.github.com>
2021-12-02 17:19:48 -0800

    Fix axe engine scan timeout when webpage has pending operation (#1859)
    * Scan result mock
    
    * Revert "Scan result mock"
    
    This reverts commit ebf278defdc3b557215c6053957f7d2475865f26.
    
    * Workaround to run axe after timeout

--- packages/crawler/src/page-operations/accessibility-scan-operation.ts
+++ packages/crawler/src/page-operations/accessibility-scan-operation.ts
@@ -4,21 +4,29 @@
 import { inject, injectable } from 'inversify';
 import { Page } from 'puppeteer';
 import { AxeResults } from 'axe-core';
+import { System, PromiseUtils } from 'common';
 import { PageScanner } from '../scanners/page-scanner';
 import { BlobStore } from '../storage/store-types';
 import { ReportGenerator } from '../reports/report-generator';
 import { LocalBlobStore } from '../storage/local-blob-store';
 
+declare type AxeScanError = 'ScanTimeout';
+
 @injectable()
 export class AccessibilityScanOperation {
+    public static axeScanTimeoutSec = 180;
+
+    public static waitForPageScrollSec = 15;
+
     constructor(
         @inject(PageScanner) private readonly scanner: PageScanner,
         @inject(ReportGenerator) private readonly reportGenerator: ReportGenerator,
         @inject(LocalBlobStore) protected readonly blobStore: BlobStore,
+        @inject(PromiseUtils) private readonly promiseUtils: PromiseUtils,
     ) {}
 
     public async run(page: Page, id: string, axeSourcePath?: string): Promise<AxeResults> {
-        const axeResults = await this.scanner.scan(page, axeSourcePath);
+        const axeResults = await this.scanForA11yIssues(page, axeSourcePath);
         const report = this.reportGenerator.generateReport(axeResults, page.url(), await page.title());
 
         await this.blobStore.setValue(`${id}.axe`, axeResults);
@@ -29,5 +37,40 @@ export class AccessibilityScanOperation {
         }
 
         return axeResults;
+    }
+
+    private async scanForA11yIssues(page: Page, axeSourcePath?: string): Promise<AxeResults> {
+        let axeResults = await this.runA11yScan(page, axeSourcePath);
+        if (axeResults === 'ScanTimeout') {
+            console.log(
+                'The accessibility scanner has timed out. Scrolling down to the bottom of the page to resolve pending page operations.',
+            );
+
+            // Scrolling down to the bottom of the page to resolve pending page operations that prevent axe engine from completing the scan
+            await page.evaluate(() => {
+                window.scrollBy(0, window.document.body.scrollHeight);
+            });
+            await System.wait(AccessibilityScanOperation.waitForPageScrollSec * 1000);
+
+            axeResults = await this.runA11yScan(page, axeSourcePath);
+        }
+
+        if (axeResults === 'ScanTimeout') {
+            throw new Error(`Accessibility scan timed out after ${AccessibilityScanOperation.axeScanTimeoutSec} seconds.`);
+        }
+
+        return axeResults;
+    }
+
+    private async runA11yScan(page: Page, axeSourcePath?: string): Promise<AxeResults | AxeScanError> {
+        return this.promiseUtils.waitFor(
+            this.scanner.scan(page, axeSourcePath),
+            AccessibilityScanOperation.axeScanTimeoutSec * 1000,
+            () => {
+                console.log(`Accessibility scan timed out after ${AccessibilityScanOperation.axeScanTimeoutSec} seconds.`);
+
+                return Promise.resolve('ScanTimeout');
+            },
+        );
     }
 }
