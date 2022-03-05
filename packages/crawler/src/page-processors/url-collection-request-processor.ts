e25c8a247dae4f48ba681b86230b1ee21d7aabbf
Maxim Laikine <la.maks@hotmail.com>
2022-03-04 17:23:43 -0800

    Filter out non-html links

--- packages/crawler/src/page-processors/url-collection-request-processor.ts
+++ packages/crawler/src/page-processors/url-collection-request-processor.ts
@@ -1,10 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+import path from 'path';
 import { HandleRequestInputs, HandleFailedRequestInput } from 'apify';
 import { System } from 'common';
 import { inject, injectable } from 'inversify';
 import { GlobalLogger } from 'logger';
+import { isEmpty } from 'lodash';
 import { CrawlRequestProcessor } from './crawl-request-processor';
 
 /* eslint-disable no-invalid-this */
@@ -12,7 +14,11 @@ import { CrawlRequestProcessor } from './crawl-request-processor';
 @injectable()
 export class UrlCollectionRequestProcessor implements CrawlRequestProcessor {
     public handleRequest = async (inputs: HandleRequestInputs): Promise<void> => {
-        this.urlList.push(inputs.request.url);
+        // filter out non-html link
+        const ext = path.extname(inputs.request.url);
+        if (isEmpty(ext) || ext === '.htm' || ext === '.html' || ext.length > 4) {
+            this.urlList.push(inputs.request.url);
+        }
     };
 
     public handleRequestError = async (inputs: HandleFailedRequestInput): Promise<void> => {
