65c118e137c8d08ee5ce9ad7a864d01342c7bf1d
Maxim Laikine <la.maks@hotmail.com>
2022-04-01 13:45:43 -0700

    Converted to wrapper for ManagedIdentityCredential

--- packages/azure-services/src/credentials/azure-managed-credential.spec.ts
+++ packages/azure-services/src/credentials/azure-managed-credential.spec.ts
@@ -3,48 +3,27 @@
 
 import 'reflect-metadata';
 
-import { Got, CancelableRequest, Response } from 'got';
 import NodeCache from 'node-cache';
 import { IMock, Mock, Times } from 'typemoq';
 import { Mutex } from 'async-mutex';
-import { AccessToken } from '@azure/core-auth';
-import { System } from 'common';
+import { ManagedIdentityCredential } from '@azure/identity';
 import { AzureManagedCredential } from './azure-managed-credential';
 
-const resource = 'https://vault.azure.net';
-const requestUrlBase = 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2021-10-01';
+const scopes = 'https://vault.azure.net/default';
+const resourceUrl = 'vault.azure.net';
+const accessToken = { token: 'eyJ0e_3g', expiresOnTimestamp: 1633500000 };
+const accessTokenOptions = {};
+const cacheCheckPeriodInSeconds = 60;
 
 describe(AzureManagedCredential, () => {
-    let scopes = 'https://vault.azure.net/default';
-    let requestUrl = `${requestUrlBase}&resource=${resource}`;
-    const getTokenOptions = {
-        requestOptions: {
-            timeout: 30,
-        },
-    };
-    const httpClientOptions = {
-        headers: {
-            Metadata: 'true',
-        },
-    };
-
-    const expires_in = 85496;
-    const accessToken = { token: 'eyJ0e_3g', expiresOnTimestamp: 1633500000 };
-    const imdsTokenString = `{"access_token":"${accessToken.token}","refresh_token":"","expires_in":${expires_in},"expires_on":${accessToken.expiresOnTimestamp},"not_before":${accessToken.expiresOnTimestamp},"resource":"https://vault.azure.net","token_type":"Bearer"}`;
-
-    let httpClientBaseMock: IMock<Got>;
     let tokenCacheMock: IMock<NodeCache>;
+    let managedIdentityCredentialMock: IMock<ManagedIdentityCredential>;
     let azureManagedCredential: AzureManagedCredential;
 
     beforeEach(() => {
-        httpClientBaseMock = Mock.ofType<Got>();
         tokenCacheMock = Mock.ofType<NodeCache>();
-        httpClientBaseMock
-            .setup((o) => o.extend({ ...httpClientOptions }))
-            .returns(() => httpClientBaseMock.object)
-            .verifiable();
-
-        azureManagedCredential = new AzureManagedCredential(httpClientBaseMock.object, tokenCacheMock.object, new Mutex());
+        managedIdentityCredentialMock = Mock.ofType<ManagedIdentityCredential>();
+        azureManagedCredential = new AzureManagedCredential(managedIdentityCredentialMock.object, tokenCacheMock.object, new Mutex());
         azureManagedCredential.backOffOptions = {
             delayFirstAttempt: false,
             numOfAttempts: 2,
@@ -54,142 +33,60 @@ describe(AzureManagedCredential, () => {
     });
 
     afterEach(() => {
-        httpClientBaseMock.verifyAll();
+        managedIdentityCredentialMock.verifyAll();
         tokenCacheMock.verifyAll();
     });
 
-    it('get msi token from a service', async () => {
+    it('get token from a service', async () => {
         tokenCacheMock
-            .setup((o) => o.get(requestUrl))
+            .setup((o) => o.get(resourceUrl))
             .returns(() => undefined)
             .verifiable();
         tokenCacheMock
-            .setup((o) => o.set(requestUrl, JSON.parse(imdsTokenString), expires_in - 600 * 2))
+            .setup((o) => o.set(resourceUrl, accessToken, accessToken.expiresOnTimestamp - cacheCheckPeriodInSeconds * 1000 * 3))
             .returns(() => true)
             .verifiable();
-        const response = { statusCode: 200, body: imdsTokenString } as unknown as CancelableRequest<Response<string>>;
-        httpClientBaseMock
-            .setup((o) => o.get(requestUrl, { timeout: getTokenOptions.requestOptions.timeout }))
-            .returns(() => response)
+        managedIdentityCredentialMock
+            .setup((o) => o.getToken(scopes, accessTokenOptions))
+            .returns(() => Promise.resolve(accessToken))
             .verifiable();
 
-        const actualAccessToken = await azureManagedCredential.getToken(scopes, getTokenOptions);
+        const actualAccessToken = await azureManagedCredential.getToken(scopes, accessTokenOptions);
 
         expect(actualAccessToken).toEqual(accessToken);
     });
 
-    it('get msi token from a cache', async () => {
+    it('get token from a cache', async () => {
         tokenCacheMock
-            .setup((o) => o.get(requestUrl))
-            .returns(() => JSON.parse(imdsTokenString))
+            .setup((o) => o.get(resourceUrl))
+            .returns(() => accessToken)
             .verifiable();
         tokenCacheMock
-            .setup((o) => o.set(requestUrl, JSON.parse(imdsTokenString), expires_in - 600 * 2))
+            .setup((o) => o.set(resourceUrl, accessToken, accessToken.expiresOnTimestamp - cacheCheckPeriodInSeconds * 1000 * 3))
             .returns(() => true)
             .verifiable(Times.never());
-        const response = { body: imdsTokenString } as unknown as CancelableRequest<Response<string>>;
-        httpClientBaseMock
-            .setup((o) => o.get(requestUrl, { timeout: getTokenOptions.requestOptions.timeout }))
-            .returns(() => response)
-            .verifiable(Times.never());
 
-        const actualAccessToken = await azureManagedCredential.getToken(scopes, getTokenOptions);
+        const actualAccessToken = await azureManagedCredential.getToken(scopes, accessTokenOptions);
 
         expect(actualAccessToken).toEqual(accessToken);
     });
 
-    it('failed to get msi token from a service', async () => {
+    it('failed to get token from a service', async () => {
         tokenCacheMock
-            .setup((o) => o.get(requestUrl))
+            .setup((o) => o.get(resourceUrl))
             .returns(() => undefined)
             .verifiable();
         tokenCacheMock
-            .setup((o) => o.set(requestUrl, JSON.parse(imdsTokenString), expires_in - 600 * 2))
+            .setup((o) => o.set(resourceUrl, accessToken, accessToken.expiresOnTimestamp - cacheCheckPeriodInSeconds * 1000 * 3))
             .returns(() => true)
             .verifiable(Times.never());
-        const response = { statusCode: 200 } as unknown as CancelableRequest<Response<string>>;
-        httpClientBaseMock
-            .setup((o) => o.get(requestUrl, { timeout: getTokenOptions.requestOptions.timeout }))
-            .returns(() => response)
+        managedIdentityCredentialMock
+            .setup((o) => o.getToken(scopes, accessTokenOptions))
+            .returns(() => Promise.reject(new Error('msi service error')))
             .verifiable(Times.exactly(2));
 
-        await expect(azureManagedCredential.getToken(scopes, getTokenOptions)).rejects.toThrowError(/IMDS has return failed response./);
-        expect(azureManagedCredential.lastErrors.length).toEqual(2);
-    });
-
-    it('failed to get success response a service', async () => {
-        tokenCacheMock
-            .setup((o) => o.get(requestUrl))
-            .returns(() => undefined)
-            .verifiable();
-        tokenCacheMock
-            .setup((o) => o.set(requestUrl, JSON.parse(imdsTokenString), expires_in - 600 * 2))
-            .returns(() => true)
-            .verifiable(Times.never());
-        const response = { statusCode: 429, body: imdsTokenString } as unknown as CancelableRequest<Response<string>>;
-
-        httpClientBaseMock
-            .setup((o) => o.get(requestUrl, { timeout: getTokenOptions.requestOptions.timeout }))
-            .returns(() => response)
-            .verifiable(Times.exactly(2));
-
-        await expect(azureManagedCredential.getToken(scopes, getTokenOptions)).rejects.toThrowError(/IMDS has return failed response./);
-        expect(azureManagedCredential.lastErrors.length).toEqual(2);
-    });
-
-    it('synchronize get token async calls', async () => {
-        const requestCount = 10;
-        const actualRequestSequence: string[] = [];
-        const expectedRequestSequence: string[] = [];
-
-        for (let i = 1; i <= requestCount; i++) {
-            requestUrl = getRequestUrl(i);
-
-            // skip first request since it should be served from IMDS
-            if (i !== 1) {
-                expectedRequestSequence.push(requestUrl);
-                tokenCacheMock
-                    .setup((o) => o.get(requestUrl))
-                    .returns(async (url) => {
-                        actualRequestSequence.push(url);
-                        await System.wait(Math.random() * 100);
-
-                        return JSON.parse(imdsTokenString);
-                    })
-                    .verifiable();
-            }
-        }
-
-        // first request is served from IMDS
-        requestUrl = getRequestUrl(1);
-        tokenCacheMock
-            .setup((o) => o.set(requestUrl, JSON.parse(imdsTokenString), expires_in - 600 * 2))
-            .returns(() => true)
-            .verifiable(Times.once());
-
-        const response = { statusCode: 200, body: imdsTokenString } as unknown as CancelableRequest<Response<string>>;
-        httpClientBaseMock
-            .setup((o) => o.get(requestUrl, { timeout: getTokenOptions.requestOptions.timeout }))
-            .returns(() => response)
-            .verifiable(Times.once());
-
-        const calls: Promise<AccessToken>[] = [];
-        for (let i = 1; i <= requestCount; i++) {
-            scopes = `https://${i}.vault.azure.net/default`;
-            calls.push(azureManagedCredential.getToken(scopes, getTokenOptions));
-        }
-
-        await Promise.all(calls);
-
-        // the call sequence should match
-        for (let i = 0; i < expectedRequestSequence.length; i++) {
-            expect(actualRequestSequence[i]).toEqual(expectedRequestSequence[i]);
-        }
+        await expect(azureManagedCredential.getToken(scopes, accessTokenOptions)).rejects.toThrowError(
+            /MSI credential provider has failed./,
+        );
     });
 });
-
-function getRequestUrl(resourceId: number): string {
-    const resourceWithId = `https://${resourceId}.vault.azure.net`;
-
-    return `${requestUrlBase}&resource=${resourceWithId}`;
-}
--- packages/azure-services/src/credentials/azure-managed-credential.ts
+++ packages/azure-services/src/credentials/azure-managed-credential.ts
@@ -4,113 +4,72 @@
 import * as nodeUrl from 'url';
 import { injectable } from 'inversify';
 import { AccessToken } from '@azure/core-auth';
-import { TokenCredential, GetTokenOptions } from '@azure/identity';
-import got, { Got, Options } from 'got';
+import { ManagedIdentityCredential, TokenCredential, GetTokenOptions } from '@azure/identity';
 import NodeCache from 'node-cache';
 import { Mutex } from 'async-mutex';
 import { backOff, IBackOffOptions } from 'exponential-backoff';
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-export interface ImdsToken {
-    access_token: string;
-    refresh_token: string;
-    expires_in: number;
-    expires_on: number;
-    not_before: number;
-    resource: string;
-    token_type: string;
-}
 
 // Get a token using HTTP
 // https://docs.microsoft.com/en-us/azure/active-directory/managed-identities-azure-resources/how-to-use-vm-token#get-a-token-using-http
 
 @injectable()
 export class AzureManagedCredential implements TokenCredential {
-    private static readonly imdsEndpoint = 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2021-10-01';
-
-    private static readonly cacheCheckPeriod = 600;
-
-    public lastErrors: any[] = [];
+    private static readonly cacheCheckPeriodInSeconds = 60;
 
     public backOffOptions: Partial<IBackOffOptions> = {
         delayFirstAttempt: false,
         numOfAttempts: 5,
         maxDelay: 6000,
         startingDelay: 0,
-    };
-
-    private readonly httpClient: Got;
-
-    private readonly options: Options = {
-        headers: {
-            Metadata: 'true',
-        },
+        retry: () => true,
     };
 
     constructor(
-        private readonly httpClientBase: Got = got,
-        private readonly tokenCache: NodeCache = new NodeCache({ checkperiod: AzureManagedCredential.cacheCheckPeriod }),
+        private readonly managedIdentityCredential: ManagedIdentityCredential = new ManagedIdentityCredential(),
+        private readonly tokenCache: NodeCache = new NodeCache({ checkperiod: AzureManagedCredential.cacheCheckPeriodInSeconds }),
         private readonly mutex: Mutex = new Mutex(),
-    ) {
-        this.httpClient = this.httpClientBase.extend({
-            ...this.options,
-        });
-    }
+    ) {}
 
     public async getToken(scopes: string | string[], options?: GetTokenOptions): Promise<AccessToken> {
         // Prevent multiple async calls to IMDS to avoid request rejection
         // The subsequent calls for the same scope will use token from a cache
-        const token = await this.mutex.runExclusive(async () => this.getMsiToken(scopes, options?.requestOptions?.timeout));
-
-        return { token: token.access_token, expiresOnTimestamp: token.expires_on };
+        return this.mutex.runExclusive(async () => this.getMsiToken(scopes, options));
     }
 
-    private async getMsiToken(scopes: string | string[], timeout: number): Promise<ImdsToken> {
-        const requestUrl = this.getRequestUrl(scopes);
+    private async getMsiToken(scopes: string | string[], options?: GetTokenOptions): Promise<AccessToken> {
+        const resourceUrl = this.getResourceUrl(scopes);
 
         // Try get token from the cache
-        const cachedImdsToken = this.tokenCache.get<ImdsToken>(requestUrl);
-        if (cachedImdsToken !== undefined) {
-            return cachedImdsToken;
+        const cachedAccessToken = this.tokenCache.get<AccessToken>(resourceUrl);
+        if (cachedAccessToken !== undefined) {
+            return cachedAccessToken;
         }
 
-        const imdsToken = await this.getImdsToken(scopes, requestUrl, timeout);
+        const accessToken = await this.getAccessToken(scopes, options);
         // Add token to the cache with reduced TTL to ensure that a cache item is deleted before token expiration time
-        this.tokenCache.set<ImdsToken>(requestUrl, imdsToken, imdsToken.expires_in - AzureManagedCredential.cacheCheckPeriod * 2);
+        this.tokenCache.set<AccessToken>(
+            resourceUrl,
+            accessToken,
+            accessToken.expiresOnTimestamp - AzureManagedCredential.cacheCheckPeriodInSeconds * 1000 * 3,
+        );
 
-        return imdsToken;
+        return accessToken;
     }
 
-    private async getImdsToken(scopes: string | string[], requestUrl: string, timeout: number): Promise<ImdsToken> {
-        this.lastErrors = [];
-        this.backOffOptions.retry = (e) => {
-            this.lastErrors.push(e);
-
-            return true;
-        };
-
-        const response = await backOff(async () => {
-            const imdsResponse = await this.httpClient.get(requestUrl, { timeout });
-
-            if (
-                imdsResponse.statusCode === undefined ||
-                imdsResponse.statusCode < 200 ||
-                imdsResponse.statusCode > 299 ||
-                imdsResponse.body === undefined
-            ) {
-                throw new Error(
-                    `IMDS has return failed response. Resource: ${JSON.stringify(scopes)} Response: ${JSON.stringify(imdsResponse)}`,
-                );
+    private async getAccessToken(scopes: string | string[], options?: GetTokenOptions): Promise<AccessToken> {
+        return backOff(async () => {
+            let token;
+            try {
+                token = await this.managedIdentityCredential.getToken(scopes, options);
+            } catch (error) {
+                throw new Error(`MSI credential provider has failed. ${JSON.stringify(error)}`);
             }
 
-            return imdsResponse;
+            return token;
         }, this.backOffOptions);
-
-        return JSON.parse(response.body) as ImdsToken;
     }
 
-    private getRequestUrl(scopes: string | string[]): string {
+    private getResourceUrl(scopes: string | string[]): string {
         let scope;
         if (typeof scopes === 'string') {
             scope = scopes;
@@ -118,8 +77,7 @@ export class AzureManagedCredential implements TokenCredential {
             scope = scopes[0];
         }
         const scopeUrl = nodeUrl.parse(scope);
-        const resource = `https://${scopeUrl.hostname}`;
 
-        return `${AzureManagedCredential.imdsEndpoint}&resource=${encodeURI(resource)}`;
+        return scopeUrl.hostname;
     }
 }
