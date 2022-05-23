581e90278bb91785c35fe4b42559b658a7da7b55
madalynrose <madparker@microsoft.com>
2022-05-23 13:50:23 -0700

    use method interface in authenticator

--- packages/crawler/src/authenticator/authenticator.spec.ts
+++ packages/crawler/src/authenticator/authenticator.spec.ts
@@ -3,125 +3,37 @@
 
 import 'reflect-metadata';
 
-import Apify from 'apify';
 import Puppeteer from 'puppeteer';
-import { IMock, It, Mock, Times } from 'typemoq';
+import { IMock, Mock } from 'typemoq';
 import { getPromisableDynamicMock } from '../test-utilities/promisable-mock';
-import { AuthenticationFlow, AuthenticationStep } from './authentication-flow';
+import { AuthenticationMethod } from './authentication-method';
 import { Authenticator } from './authenticator';
 
 describe(Authenticator, () => {
-    const testAccountName = 'testServiceAccount';
-    const testAccountPassword = 'test123';
-    const testAuthenticationSteps: AuthenticationStep[] = [
-        {
-            operation: 'type',
-            selector: '#username',
-            credential: 'name',
-            value: testAccountName,
-        },
-        {
-            operation: 'click',
-            selector: '#1',
-        },
-        {
-            operation: 'waitForNavigation',
-        },
-        {
-            operation: 'type',
-            selector: '#password',
-            credential: 'password',
-            value: testAccountPassword,
-        },
-        {
-            operation: 'enter',
-        },
-        {
-            operation: 'waitForNavigation',
-        },
-    ];
-
-    const testAuthenticationFlow: AuthenticationFlow = {
-        startingUrl: 'https://example.com',
-        authenticatedUrl: 'https://example.com/en',
-        steps: testAuthenticationSteps,
-    };
-
-    function setupAuthenticationFlow(
-        flow: AuthenticationFlow,
-        pageMock: IMock<Puppeteer.Page>,
-        keyboardMock: IMock<Puppeteer.Keyboard>,
-        success: boolean = true,
-        times: number = 1,
-    ): void {
-        pageMock.setup((o) => o.goto(flow.startingUrl)).verifiable(Times.exactly(times));
-        const operationCounts = flow.steps.reduce((counts: { [operation: string]: number }, currentStep: AuthenticationStep) => {
-            counts[currentStep.operation] = (counts[currentStep.operation] || 0) + 1;
-
-            return counts;
-        }, {});
-        keyboardMock.setup((o) => o.press('Enter')).verifiable(Times.exactly(operationCounts.enter * times));
-        pageMock
-            .setup((o) => o.waitForSelector(It.isAnyString()))
-            .verifiable(Times.exactly((operationCounts.type + operationCounts.click) * times));
-        pageMock.setup((o) => o.type(It.isAnyString(), testAccountName)).verifiable(Times.exactly(times));
-        pageMock.setup((o) => o.type(It.isAnyString(), testAccountPassword)).verifiable(Times.exactly(times));
-        pageMock.setup((o) => o.click(It.isAnyString())).verifiable(Times.exactly(operationCounts.click * times));
-        pageMock
-            .setup((o) => o.waitForNavigation({ waitUntil: 'networkidle0' }))
-            .verifiable(Times.exactly(operationCounts.waitForNavigation * times));
-        pageMock.setup((o) => o.$eval('#errorText', It.isAny())).returns(() => Promise.resolve(success ? '' : 'this is an error'));
-        pageMock
-            .setup((o) => o.url())
-            .returns(() => (success ? flow.authenticatedUrl : 'https://not.logged.in'))
-            .verifiable(Times.exactly(times));
-        pageMock.setup((o) => o.close()).verifiable(Times.once());
-    }
-
-    function setupLogger(loggerMock: IMock<typeof Apify.utils.log>, success: boolean = true, times: number = 1): void {
-        if (success) {
-            loggerMock.setup((o) => o.info(It.isAnyString())).verifiable(Times.once());
-        } else {
-            loggerMock.setup((o) => o.warning(It.isAnyString())).verifiable(Times.exactly(times - 1));
-            loggerMock.setup((o) => o.error(It.isAnyString())).verifiable(Times.once());
-        }
-    }
-
     let pageMock: IMock<Puppeteer.Page>;
     let browserMock: IMock<Puppeteer.Browser>;
-    let keyboardMock: IMock<Puppeteer.Keyboard>;
-    let loggerMock: IMock<typeof Apify.utils.log>;
+    let authenticationFlowMock: IMock<AuthenticationMethod>;
     let authenticator: Authenticator;
     beforeEach(() => {
-        loggerMock = Mock.ofType<typeof Apify.utils.log>();
-        keyboardMock = getPromisableDynamicMock(Mock.ofType<Puppeteer.Keyboard>());
         pageMock = getPromisableDynamicMock(Mock.ofType<Puppeteer.Page>());
-        pageMock.setup((o) => o.keyboard).returns(() => keyboardMock.object);
+        pageMock.setup((o) => o.close()).verifiable();
         browserMock = getPromisableDynamicMock(Mock.ofType<Puppeteer.Browser>());
         browserMock
             .setup(async (o) => o.newPage())
             .returns(() => Promise.resolve(pageMock.object))
             .verifiable();
-
-        authenticator = new Authenticator(testAuthenticationFlow, loggerMock.object);
+        authenticationFlowMock = Mock.ofType<AuthenticationMethod>();
+        authenticationFlowMock.setup((o) => o.authenticate(pageMock.object)).verifiable();
+        authenticator = new Authenticator(authenticationFlowMock.object);
     });
 
     afterEach(() => {
         browserMock.verifyAll();
         pageMock.verifyAll();
-        keyboardMock.verifyAll();
-        loggerMock.verifyAll();
+        authenticationFlowMock.verifyAll();
     });
 
-    it('executes authentication steps for authentication flow', async () => {
-        setupAuthenticationFlow(testAuthenticationFlow, pageMock, keyboardMock);
-        setupLogger(loggerMock);
-        await authenticator.run(browserMock.object);
-    });
-
-    it('retries four times if it detects authentication failed', async () => {
-        setupAuthenticationFlow(testAuthenticationFlow, pageMock, keyboardMock, false, 5);
-        setupLogger(loggerMock, false, 5);
+    it('opens a new page, authenticates the page, and closes the page', async () => {
         await authenticator.run(browserMock.object);
     });
 });
--- packages/crawler/src/authenticator/authenticator.ts
+++ packages/crawler/src/authenticator/authenticator.ts
@@ -1,67 +1,15 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import Apify from 'apify';
 import * as Puppeteer from 'puppeteer';
-import { AuthenticationStep, AuthenticationFlow } from './authentication-flow';
+import { AuthenticationMethod } from './authentication-method';
 
 export class Authenticator {
-    public constructor(
-        protected authenticationFlow: AuthenticationFlow,
-        private readonly logger: typeof Apify.utils.log = Apify.utils.log,
-    ) {}
+    public constructor(protected authenticationMethod: AuthenticationMethod) {}
 
     public async run(browser: Puppeteer.Browser): Promise<void> {
         const page = await browser.newPage();
-        await this.attemptAuthentication(page);
+        await this.authenticationMethod.authenticate(page);
         await page.close();
-    }
-
-    private async executeAuthenticationStep(page: Puppeteer.Page, step: AuthenticationStep): Promise<void> {
-        switch (step.operation) {
-            case 'click':
-                await page.waitForSelector(step.selector);
-                await page.click(step.selector);
-                break;
-            case 'type':
-                await page.waitForSelector(step.selector);
-                await page.type(step.selector, step.value);
-                break;
-            case 'enter':
-                await page.keyboard.press('Enter');
-                break;
-            case 'waitForNavigation':
-                await page.waitForNavigation({ waitUntil: 'networkidle0' });
-                break;
-            default:
-                throw new Error(`${step.operation} is not a valid authentication step operation`);
-        }
-    }
-
-    private async attemptAuthentication(page: Puppeteer.Page, attemptNumber: number = 1): Promise<void> {
-        await page.goto(this.authenticationFlow.startingUrl);
-        for (const step of this.authenticationFlow.steps) {
-            await this.executeAuthenticationStep(page, step);
-        }
-
-        if (!this.checkAuthenticationSucceeded(page, this.authenticationFlow.authenticatedUrl)) {
-            const errorText: string = await page.$eval('#errorText', (el) => el.textContent);
-            if (attemptNumber > 4) {
-                this.logger.error(`Attempted authentication ${attemptNumber} times and ultimately failed.`);
-
-                return;
-            }
-            if (errorText !== '') {
-                this.logger.warning(`Authentication failed with error: ${errorText}`);
-            }
-            await this.attemptAuthentication(page, attemptNumber + 1);
-
-            return;
-        }
-        this.logger.info('Authentication succeeded');
-    }
-
-    private checkAuthenticationSucceeded(page: Puppeteer.Page, url: string): boolean {
-        return page.url().match(`^${url}`) != null;
     }
 }
