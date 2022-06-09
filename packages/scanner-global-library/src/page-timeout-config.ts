71d7acf2f8f741b037ae6b3563178aab04ccf799
Maxim Laikine <mlaikine@microsoft.com>
2022-06-08 23:13:18 -0700

    Reduce page navigation timeout

--- packages/scanner-global-library/src/page-timeout-config.ts
+++ packages/scanner-global-library/src/page-timeout-config.ts
@@ -8,26 +8,28 @@ export interface PageNavigationTiming {
     render: number;
 }
 
+/**
+ * The total page navigation timeout should correlate with Batch scan task 'max wall-clock time' constrain.
+ * Refer to service configuration TaskRuntimeConfig.taskTimeoutInMinutes property.
+ */
 export const puppeteerTimeoutConfig = {
     /**
-     * Maximum wait time, in milliseconds, to scroll to the bottom of the page.
+     * Maximum wait time, in milliseconds, to complete page navigation.
      */
-    scrollTimeoutMsecs: 3000,
+    navigationTimeoutMsecs: 20000,
 
     /**
      * Maximum wait time, in milliseconds, to complete async rendering operations of the page.
      */
-    pageRenderingTimeoutMsecs: 60000,
+    pageRenderingTimeoutMsecs: 20000,
 
     /**
-     * Maximum wait time, in milliseconds, to complete page navigation.
-     * The total page navigation timeout should correlate with Batch scan task 'max wall-clock time' constrain
-     * Refer to service configuration TaskRuntimeConfig.taskTimeoutInMinutes property
-     */
-    navigationTimeoutMsecs: 60000,
-
-    /**
-     * The minimum time the HTML DOM should be stable to accept page rendering.
+     * The minimum time the HTML DOM should be stable to accept page rendered.
      */
     pageDomStableTimeMsecs: 3000,
+
+    /**
+     * Maximum wait time, in milliseconds, to scroll to the bottom of the page.
+     */
+    scrollTimeoutMsecs: 10000,
 };
