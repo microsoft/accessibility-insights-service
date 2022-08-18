6fc25c7997f81adfc7a131775fbd993bdfcd2704
Maxim Laikine <la.maks@hotmail.com>
2022-08-17 20:55:52 -0700

    Added logging

--- packages/scanner-global-library/src/page-navigator.ts
+++ packages/scanner-global-library/src/page-navigator.ts
@@ -208,14 +208,19 @@ export class PageNavigator {
         page: Puppeteer.Page,
         navigationCondition: Puppeteer.PuppeteerLifeCycleEvent,
     ): Promise<Puppeteer.HTTPResponse> {
-        let count = 3;
+        const maxRetryCount = 3;
+
+        let count = 0;
         let response;
         do {
-            count--;
+            count++;
             await page.goto(`file:///${__dirname}/blank-page.html`);
+            await System.wait(500);
 
             response = await page.goBack({ waitUntil: navigationCondition, timeout: puppeteerTimeoutConfig.navigationTimeoutMsecs });
-        } while (count > 0 && response.status() === 304);
+        } while (count < maxRetryCount && response.status() === 304);
+
+        this.logger.logWarn('Reload page on HTTP 304 (Not Modified) website response.', { retryCount: `${count}` });
 
         return response;
     }
