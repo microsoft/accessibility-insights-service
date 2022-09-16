c35466a21852078b21a896660157bd670e3383ef
Maxim Laikine <la.maks@hotmail.com>
2022-09-16 15:52:12 -0700

    Reduce retry count

--- packages/common/src/configuration/service-configuration.ts
+++ packages/common/src/configuration/service-configuration.ts
@@ -251,7 +251,7 @@ export class ServiceConfiguration {
                 },
                 maxFailedScanRetryCount: {
                     format: 'int',
-                    default: 3,
+                    default: 2,
                     doc: 'Maximum number of retries (additional times to re-run a scan) allowed for a failed scan request.',
                 },
                 maxSendNotificationRetryCount: {
