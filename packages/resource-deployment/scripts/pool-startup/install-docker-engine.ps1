f962596b7ccc235f2bbaf930fbc2ec565a3b2509
Maxim Laikine <la.maks@hotmail.com>
2023-03-13 10:17:50 -0700

    Switch to DockerMsftProvider

---
+++ packages/resource-deployment/scripts/pool-startup/install-docker-engine.ps1
@@ -0,0 +1,21 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+function installDockerEngine() {
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+    Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
+    Install-Package -Name docker -ProviderName DockerMsftProvider -Force
+    Start-Service Docker
+    
+    shutdown /r /t 0 /d p:4:2
+}
+
+Get-Process "com.docker.service" -ErrorAction SilentlyContinue 
+if ($? -eq "True") {
+    Write-Output ""
+    docker --version
+}
+else {
+    Write-Output "Docker service processes is not running. Installing Docker Engine..."
+    installDockerEngine
+}
