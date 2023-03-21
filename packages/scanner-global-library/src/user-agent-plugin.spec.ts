9bbfb2cf2961e41de2f125a87e5a96c68b5acf63
Maxim Laikine <la.maks@hotmail.com>
2023-03-21 14:57:49 -0700

    Override browser user agent string

--- packages/scanner-global-library/src/user-agent-plugin.spec.ts
+++ packages/scanner-global-library/src/user-agent-plugin.spec.ts
@@ -4,19 +4,31 @@
 import 'reflect-metadata';
 
 import Puppeteer from 'puppeteer';
-import { IMock, Mock } from 'typemoq';
+import { IMock, It, Mock, Times } from 'typemoq';
 import { UserAgentPlugin } from './user-agent-plugin';
 
-const windowsUserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36';
-const linuxUserAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36';
+const version = 'Chrome/107.1.2.3';
+const windowsUserAgent =
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/107.1.2.3 Safari/537.36';
+const linuxUserAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.1.2.3 Safari/537.36';
 
 let puppeteerPageMock: IMock<Puppeteer.Page>;
 let puppeteerBrowserStub: Puppeteer.Browser;
 let userAgentPlugin: UserAgentPlugin;
+let userAgentMetadata = It.isAny();
 
 describe(UserAgentPlugin, () => {
     beforeEach(() => {
         puppeteerPageMock = Mock.ofType<Puppeteer.Page>();
+        puppeteerBrowserStub = {
+            userAgent: () => Promise.resolve(windowsUserAgent),
+            version: () => Promise.resolve(version),
+        } as Puppeteer.Browser;
+        puppeteerPageMock
+            .setup((o) => o.browser())
+            .returns(() => puppeteerBrowserStub)
+            .verifiable(Times.atLeastOnce());
+
         userAgentPlugin = new UserAgentPlugin();
     });
 
@@ -24,18 +36,35 @@ describe(UserAgentPlugin, () => {
         puppeteerPageMock.verifyAll();
     });
 
-    it('should replace Windows platform', async () => {
-        puppeteerBrowserStub = {
-            userAgent: () => Promise.resolve(windowsUserAgent),
-        } as Puppeteer.Browser;
+    it('validate user agent string', async () => {
         puppeteerPageMock
-            .setup((o) => o.browser())
-            .returns(() => puppeteerBrowserStub)
-            .verifiable();
-        puppeteerPageMock
-            .setup((o) => o.setUserAgent(linuxUserAgent))
+            .setup((o) => o.setUserAgent(linuxUserAgent, userAgentMetadata))
             .returns(() => Promise.resolve())
-            .verifiable();
+            .verifiable(Times.atLeastOnce());
+
+        await userAgentPlugin.onPageCreated(puppeteerPageMock.object);
+    });
+
+    it('validate userAgentMetadata', async () => {
+        userAgentMetadata = {
+            brands: [
+                { brand: 'Google Chrome', version: '107' },
+                { brand: 'Chromium', version: '107' },
+                { brand: 'Not(A.Brand', version: '8' },
+            ],
+            fullVersion: '107.1.2.3',
+            platform: 'Linux',
+            platformVersion: '',
+            architecture: 'x86',
+            model: '',
+            mobile: false,
+            bitness: '64',
+        };
+
+        puppeteerPageMock
+            .setup((o) => o.setUserAgent(linuxUserAgent, userAgentMetadata))
+            .returns(() => Promise.resolve())
+            .verifiable(Times.atLeastOnce());
 
         await userAgentPlugin.onPageCreated(puppeteerPageMock.object);
     });
