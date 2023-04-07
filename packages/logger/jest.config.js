7c46af0bf5f1cf52500ec0081f7a371bb6c9f588
Maxim Laikine <la.maks@hotmail.com>
2023-04-07 08:48:39 -0700

    Fix lint

--- packages/logger/jest.config.js
+++ packages/logger/jest.config.js
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const package = require("./package");
+const package = require('./package');
 
 module.exports = {
-  preset: "../../jest.config.base.js",
-  displayName: package.name,
+    preset: '../../jest.config.base.js',
+    displayName: package.name,
 };
