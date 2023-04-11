0d13f4c8b436f7713aa54949a785141a1af0fa29
lamaks <la.maks@hotmail.com>
2023-04-11 11:49:48 -0700

    Added page clear browser cache

--- packages/scanner-global-library/src/browser-cache.ts
+++ packages/scanner-global-library/src/browser-cache.ts
@@ -3,12 +3,19 @@
 
 import * as fs from 'fs';
 import { injectable } from 'inversify';
+import * as Puppeteer from 'puppeteer';
 
 @injectable()
 export class BrowserCache {
     public readonly dirname = `${__dirname}/browser-cache`;
 
     constructor(private readonly filesystem: typeof fs = fs) {}
+
+    public async clearPage(page: Puppeteer.Page): Promise<void> {
+        const session = await page.target().createCDPSession();
+        await session.send('Network.clearBrowserCache');
+        await session.detach();
+    }
 
     public clear(): void {
         this.filesystem.rmSync(this.dirname, { recursive: true, force: true });
