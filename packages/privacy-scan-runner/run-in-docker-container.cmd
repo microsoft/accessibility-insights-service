2e271d0da59c17e7456e8899601e9308d6a459fd
Maxim Laikine <la.maks@hotmail.com>
2023-08-30 13:55:51 -0700

    Updated network route script

--- packages/privacy-scan-runner/run-in-docker-container.cmd
+++ packages/privacy-scan-runner/run-in-docker-container.cmd
@@ -5,11 +5,11 @@ echo off
 
 rem The script builds and runs the docker image
 
-copy ..\resource-deployment\runtime-config\runtime-config.dev.json .\dist\runtime-config.json
-copy .\.env .\dist\
-
+copy ..\resource-deployment\runtime-config\runtime-config.dev.json .\dist\runtime-config.json &&^
 yarn build &&^
 cd .\dist &&^
-docker build --tag privacy-scan-runner . &&^
-docker run --init --ipc=host --env-file .env privacy-scan-runner &&^
-cd ..
+docker build --tag privacy-scan-runner:prescanner . &&^
+cd ..\..\resource-deployment\scripts\docker-scanner-image &&^
+powershell .\build-scanner-image.ps1 &&^
+cd ..\..\..\privacy-scan-runner &&^
+docker run --init --ipc=host --env-file .env privacy-scan-runner
---
+++ packages/web-api-scan-runner/run-in-terminal.ps1
@@ -0,0 +1,14 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+$variables = Select-String -Path './.env' -Pattern '^\s*[^\s=#]+=[^\s]+$' | Select-Object -ExpandProperty Line
+
+foreach ($var in $variables) {
+    $keyVal = $var -split '=', 2
+    $key = $keyVal[0].Trim()
+    $val = $keyVal[1]
+    [Environment]::SetEnvironmentVariable($key, $val)
+    Write-Host "$key=$([Environment]::GetEnvironmentVariable($key))"
+}
+
+node ./web-api-scan-runner.js
