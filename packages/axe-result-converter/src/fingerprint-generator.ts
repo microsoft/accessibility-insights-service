be8278a9325cfda497e6480167ebfadf96131585
Maxim Laikine <la.maks@hotmail.com>
2023-10-30 18:21:48 -0700

    Revert

---
+++ packages/axe-result-converter/src/fingerprint-generator.ts
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { HashGenerator } from 'accessibility-insights-crawler';
+import { injectable, inject } from 'inversify';
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export interface FingerprintParameters {
+    rule: string;
+    snippet: string;
+    cssSelector: string;
+    xpathSelector?: string;
+}
+
+@injectable()
+export class FingerprintGenerator {
+    constructor(@inject(HashGenerator) private readonly hashGenerator: HashGenerator) {}
+
+    public getFingerprint(fingerprintParameters: FingerprintParameters): string {
+        const valuesToHash = [fingerprintParameters.rule, fingerprintParameters.snippet, fingerprintParameters.cssSelector];
+
+        if (fingerprintParameters.xpathSelector) {
+            valuesToHash.push(fingerprintParameters.xpathSelector);
+        }
+
+        return this.hashGenerator.generateBase64Hash(...valuesToHash);
+    }
+}
