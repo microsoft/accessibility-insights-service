8f212ce008e13e3c79488d3dec0daefc543e2ea0
Vikash Yadav <v-viyada@microsoft.com>
2024-02-15 13:49:25 -0500

    updated comment as per review comment

--- packages/crawler/src/page-handler/page-navigation-hooks.ts
+++ packages/crawler/src/page-handler/page-navigation-hooks.ts
@@ -40,7 +40,7 @@ export class PageNavigationHooks {
             });
         }
 
-        // Validate HTTP response, hashUrls will have null response as crawler treats it same page load
+        // Validate HTTP response, hashUrls will have null response from page.goto navigation as crawler treats it same page load
         const responseError = isHashUrl && response === null ? undefined : this.pageResponseProcessor.getResponseError(response);
         if (responseError !== undefined) {
             await onNavigationError(responseError);
