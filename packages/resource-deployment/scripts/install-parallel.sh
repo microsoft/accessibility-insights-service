52f2e4ecafc7405a54c015ec96aaa0b37548a45a
Vikash Yadav <v-viyada@microsoft.com>
2024-05-03 14:47:42 -0400

    moved creation of api-management before e2e test site deployment

--- packages/resource-deployment/scripts/install-parallel.sh
+++ packages/resource-deployment/scripts/install-parallel.sh
@@ -181,12 +181,12 @@ function install() {
     . "${0%/*}/wait-for-pending-deployments.sh"
     . "${0%/*}/create-storage-account.sh"
     . "${0%/*}/get-resource-names.sh"
+    . "${0%/*}/create-api-management.sh" &
+    apiManagmentProcessId="$!"
     . "${0%/*}/deploy-e2e-test-site.sh"
 
     echo "Starting parallel processes..."
 
-    . "${0%/*}/create-api-management.sh" &
-    apiManagmentProcessId="$!"
 
     parallelProcesses=(
         "${0%/*}/upload-files.sh"
