b175365c9a4980f6682e5d7c13c7b4b353207bb8
Maxim Laikine <la.maks@hotmail.com>
2024-06-11 15:12:27 -0700

    Converted to use process CPU usage

--- packages/scanner-global-library/src/network/page-cpu-usage.ts
+++ packages/scanner-global-library/src/network/page-cpu-usage.ts
@@ -1,27 +1,43 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+import os from 'os';
 import { System } from 'common';
 import { injectable, inject, optional } from 'inversify';
 import { GlobalLogger } from 'logger';
 import * as Puppeteer from 'puppeteer';
-
-interface CpuActiveTime {
-    timestamp: number;
-    activeTime: number;
-}
-
-interface CpuUsageSnapshot {
-    activeTime: CpuActiveTime;
-    usage: number;
-}
+import pidusage from 'pidusage';
+import { meanBy } from 'lodash';
 
 export interface CpuUsage {
+    /**
+     * percentage (from 0 to 100*vcore)
+     */
+    cpu: number;
+
+    /**
+     * bytes
+     */
+    memory: number;
+
+    /**
+     * PPID
+     */
+    ppid: number;
+
+    /**
+     * PID
+     */
+    pid: number;
+
+    /**
+     * msec since epoch
+     */
     timestamp: number;
-    usage: number;
 }
 
 export interface CpuUsageStats {
+    cpus: number;
     average: number;
     snapshots: CpuUsage[];
 }
@@ -33,61 +49,34 @@ export class PageCpuUsage {
     // to do CPU throttling
 
     public async getCpuUsage(page: Puppeteer.Page, intervalMsec: number, sampleIntervalMsec: number = 1000): Promise<CpuUsageStats> {
-        const cdp = await page.createCDPSession();
-        await cdp.send('Performance.enable', {
-            timeDomain: 'timeTicks',
-        });
-
         const snapshots: CpuUsage[] = [];
-        const initialActiveTime = await this.getCpuActiveTime(cdp);
-        let lastActiveTime = initialActiveTime;
+        const pid = page.browser().process().pid;
 
         const timestamp = System.getTimestamp();
         while (System.getTimestamp() < timestamp + intervalMsec) {
-            await System.wait(sampleIntervalMsec);
-            const usage = await this.getCpuUsageSnapshot(cdp, lastActiveTime);
-            lastActiveTime = usage.activeTime;
+            const usage = await pidusage(pid);
             snapshots.push({
-                timestamp: usage.activeTime.timestamp,
-                usage: usage.usage,
+                cpu: usage.cpu,
+                memory: usage.memory,
+                ppid: usage.ppid,
+                pid: usage.pid,
+                timestamp: usage.timestamp,
             });
+
+            await System.wait(sampleIntervalMsec);
         }
 
-        await cdp.detach();
-
-        const average = lastActiveTime.activeTime / (lastActiveTime.timestamp - initialActiveTime.timestamp);
-        this.logger?.logInfo(`Page CPU load average load ${average * 100} %`, {
-            average: average.toString(),
-            snapshots: JSON.stringify(snapshots),
-        });
-
-        return {
+        const average = meanBy(snapshots, (s) => s.cpu);
+        const stats = {
+            cpus: os.cpus().length,
             average,
             snapshots,
         };
-    }
 
-    private async getCpuUsageSnapshot(cdp: Puppeteer.CDPSession, lastActiveTime: CpuActiveTime): Promise<CpuUsageSnapshot> {
-        const sampleActiveTime = await this.getCpuActiveTime(cdp);
-        const sampleDuration = sampleActiveTime.timestamp - lastActiveTime.timestamp;
-        const usage = (sampleActiveTime.activeTime - lastActiveTime.activeTime) / sampleDuration;
+        this.logger?.logInfo(`Browser process ID ${pid} usage statistics.`, {
+            snapshots: JSON.stringify(stats),
+        });
 
-        return {
-            activeTime: sampleActiveTime,
-            usage: usage > 1 ? 1 : usage,
-        };
-    }
-
-    private async getCpuActiveTime(cdp: Puppeteer.CDPSession): Promise<CpuActiveTime> {
-        const metrics = await cdp.send('Performance.getMetrics');
-        const activeTime = metrics.metrics
-            .filter((m) => m.name.includes('Duration'))
-            .map((m) => m.value)
-            .reduce((a, b) => a + b);
-
-        return {
-            timestamp: metrics.metrics.find((m) => m.name === 'Timestamp')?.value || 0,
-            activeTime,
-        };
+        return stats;
     }
 }
