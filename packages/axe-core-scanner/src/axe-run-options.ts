b7b259a14233ee27985e4904c2db6906a703ca91
v-rakeshsh <155614445+v-rakeshsh@users.noreply.github.com>
2025-01-13 23:37:56 +0530

    feat: Update axe core versions to 4.10.2 and other related dependencies (#2588)
    #### Details
    
    Upgraded axe core to version 4.10.2
    Updated puppeteer version 4.10.1
    
    #### Pull request checklist
    <!-- If a checklist item is not applicable to this change, write "n/a"
    in the checkbox -->
    
    - [ ] Addresses an existing issue: Fixes #0000
    - [ ] Added relevant unit test for your changes. (`yarn test`)
    - [ ] Verified code coverage for the changes made. Check coverage report
    at: `<rootDir>/test-results/unit/coverage`
    - [ ] Ran precheckin (`yarn precheckin`)
    - [x] Validated in an Azure resource group
    
    ---------
    
    Co-authored-by: Anjali Singh <150343275+v-singhanjal@users.noreply.github.com>
    Co-authored-by: Anjali Singh <v-singhanjal@microsoft.com>

--- packages/axe-core-scanner/src/axe-run-options.ts
+++ packages/axe-core-scanner/src/axe-run-options.ts
@@ -64,6 +64,7 @@ export const webAxeRunOptions: AxeRunOptions = {
             'role-img-alt',
             'select-name',
             'server-side-image-map',
+            'summary-name',
             'svg-img-alt',
             'td-headers-attr',
             'th-has-data-cells',
