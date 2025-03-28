0e8462a29ee89307d910a46a6aa46da29c68420a
Maxim Laikine <la.maks@hotmail.com>
2025-03-28 16:54:58 -0700

    fix: update Windows share paths for font installation and scanner image build scripts

--- packages/resource-deployment/scripts/docker-scanner-image/install-font-packages.ps1
+++ packages/resource-deployment/scripts/docker-scanner-image/install-font-packages.ps1
@@ -1,24 +1,29 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+Import-Module DISM
+
 $gateway = (Get-NetRoute '0.0.0.0/0').NextHop
-net use S: \\$gateway\WinSxS /user:DockerBuild "${env:BUILD_KEY}" | Out-Null
-Write-Host "Windows font source contains:" ( Get-ChildItem S: | Measure-Object ).Count "files"
+net use W: \\$gateway\WinSxS /user:DockerBuild "${env:BUILD_KEY}" | Out-Null
+net use F: \\$gateway\Fonts /user:DockerBuild "${env:BUILD_KEY}" | Out-Null
+Write-Host "Windows font source contains:" ( Get-ChildItem F: | Measure-Object ).Count "files"
+
+Write-Host "Enabling Windows ServerMediaFoundation feature..."
+Enable-WindowsOptionalFeature -FeatureName "ServerMediaFoundation" -Online -LimitAccess -Source W:\
 
 if ("${env:INSTALLATION_TYPE}" -eq "Server") {
     Write-Host "Enabling Windows fonts feature..."
 
-    Import-Module DISM
     Enable-WindowsOptionalFeature -FeatureName `
         "ServerCoreFonts-NonCritical-Fonts-MinConsoleFonts", `
         "ServerCoreFonts-NonCritical-Fonts-Support", `
         "ServerCoreFonts-NonCritical-Fonts-BitmapFonts", `
         "ServerCoreFonts-NonCritical-Fonts-TrueType", `
         "ServerCoreFonts-NonCritical-Fonts-UAPFonts" `
-        -Online -LimitAccess -Source S:\ | Out-Null
+        -Online -LimitAccess -Source W:\ | Out-Null
 }
 else {
     Write-Host "Copying Windows fonts from the host machine..."
 
-    Copy-Item -Path "S:\*" -Destination "${env:windir}\Fonts" -Include "*.fon", "*.ttf", "*.ttc" -Force
+    Copy-Item -Path "F:\*" -Destination "${env:windir}\Fonts" -Include "*.fon", "*.ttf", "*.ttc" -Force
 }
