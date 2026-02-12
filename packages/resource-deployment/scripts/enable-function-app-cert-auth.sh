3ccc1b9d5359ec00e6cf32d52b3e32e9c01294f3
Maxim Laikine <mlaikine@microsoft.com>
2026-02-11 18:20:20 -0800

    feat: update certificate management scripts and policies for sec-scan

---
+++ packages/resource-deployment/scripts/enable-function-app-cert-auth.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+# shellcheck disable=SC1090
+set -eo pipefail
+
+# Disable POSIX to Windows path conversion
+export MSYS_NO_PATHCONV=1
+
+exitWithUsageInfo() {
+    echo "
+Usage: ${BASH_SOURCE} -r <resource group> [-n <certificate name>]
+"
+    exit 1
+}
+
+# Read script arguments
+while getopts ":r:n:" option; do
+    case ${option} in
+    r) resourceGroupName=${OPTARG} ;;
+    n) certificateName=${OPTARG} ;;
+    *) exitWithUsageInfo ;;
+    esac
+done
+
+if [[ -z ${resourceGroupName} ]]; then
+    exitWithUsageInfo
+fi
+
+if [[ -z ${certificateName} ]]; then
+    certificateName="secScanCert"
+fi
+
+. "${0%/*}/get-resource-names.sh"
+
+echo "Enabling certificate authentication for ${webApiFuncAppName} function app..."
+
+# Get the certificate thumbprint from Key Vault
+thumbprint=$(az keyvault certificate show --name "${certificateName}" --vault-name "${keyVault}" --query "x509ThumbprintHex" -o tsv)
+
+if [[ -z ${thumbprint} ]]; then
+    echo "Unable to get certificate thumbprint for certificate ${certificateName} from key vault ${keyVault}"
+    exit 1
+fi
+
+echo "Using certificate ${certificateName} with thumbprint ${thumbprint}"
+
+# Upload the Key Vault certificate to the function app
+certificateId=$(az keyvault certificate show --name "${certificateName}" --vault-name "${keyVault}" --query "id" -o tsv)
+
+az webapp config ssl import \
+    --resource-group "${resourceGroupName}" \
+    --name "${webApiFuncAppName}" \
+    --key-vault "${keyVault}" \
+    --key-vault-certificate-name "${certificateName}" 1>/dev/null
+
+echo "Imported certificate ${certificateName} to ${webApiFuncAppName}"
+
+# Enable client certificate authentication on the function app
+az functionapp update \
+    --resource-group "${resourceGroupName}" \
+    --name "${webApiFuncAppName}" \
+    --set clientCertEnabled=true \
+    --set clientCertMode=Required 1>/dev/null
+
+echo "Enabled client certificate authentication on ${webApiFuncAppName}"
+
+# Set the certificate thumbprint for client certificate exclusion path filtering (optional)
+# This allows health check endpoints to be accessed without a client certificate
+az functionapp config appsettings set \
+    --resource-group "${resourceGroupName}" \
+    --name "${webApiFuncAppName}" \
+    --settings "WEBSITE_CLIENT_CERT_THUMBPRINT=${thumbprint}" 1>/dev/null
+
+echo "Successfully enabled certificate authentication for ${webApiFuncAppName} function app with certificate ${certificateName}"
--- packages/resource-deployment/scripts/key-vault-rotate-certificate.sh
+++ packages/resource-deployment/scripts/key-vault-rotate-certificate.sh
@@ -5,14 +5,23 @@
 
 set -eo pipefail
 
-certificateName="azSecPackCert"
+certificateName="secScanCert"
+certificatePolicyPrefix="certificate-policy-sec-scan"
 
 # Disable POSIX to Windows path conversion
 export MSYS_NO_PATHCONV=1
 
 exitWithUsageInfo() {
     echo "
-Usage: ${BASH_SOURCE} -r <resource group> [-k <key vault>] [-n <key vault certificate name>] [-s <subscription name or id>] [-e <environment>]
+Usage: ${BASH_SOURCE} -r <resource group> [-s <subscription name or id>] [-k <key vault>] [-n <certificate name>] [-e <environment>] [-p <certificate policy prefix>]
+
+Options:
+    -r  Resource group name (required)
+    -s  Azure subscription name or ID
+    -k  Key vault name
+    -n  Key vault certificate name (default: ${certificateName})
+    -e  Environment name (default: dev). Combined with policy prefix to select template
+    -p  Certificate policy file name prefix (default: ${certificatePolicyPrefix}). Resolves to <prefix>-<environment>.json
 "
     exit 1
 }
@@ -21,7 +30,7 @@ getCurrentUserDetails() {
     echo "Getting logged in user name"
     principalName=$(az account show --query "user.name" -o tsv)
 
-    if [[ -z $principalName ]]; then
+    if [[ -z ${principalName} ]]; then
         echo "Unable to get logged in user name"
         exit 1
     fi
@@ -31,50 +40,51 @@ grantUserAccessToKeyVault() {
     echo "Adding key vault role assignment for logged in user"
     az role assignment create \
         --role "Key Vault Certificates Officer" \
-        --assignee "$principalName" \
-        --scope "/subscriptions/$subscription/resourcegroups/$resourceGroupName/providers/Microsoft.KeyVault/vaults/$keyVault" 1>/dev/null
+        --assignee "${principalName}" \
+        --scope "/subscriptions/${subscription}/resourcegroups/${resourceGroupName}/providers/Microsoft.KeyVault/vaults/${keyVault}" 1>/dev/null
 }
 
 onExit-key-vault-rotate-certificate() {
     echo "Revoking key vault role assignment for logged in user"
     az role assignment delete \
         --role "Key Vault Certificates Officer" \
-        --assignee "$principalName" \
-        --scope "/subscriptions/$subscription/resourcegroups/$resourceGroupName/providers/Microsoft.KeyVault/vaults/$keyVault" >/dev/null 2>&1
+        --assignee "${principalName}" \
+        --scope "/subscriptions/${subscription}/resourcegroups/${resourceGroupName}/providers/Microsoft.KeyVault/vaults/${keyVault}" >/dev/null 2>&1
 }
 
 createNewCertificateVersion() {
     echo "Creating new version of certificate..."
-    thumbprintCurrent=$(az keyvault certificate show --name "$certificateName" --vault-name "$keyVault" --query "x509ThumbprintHex" -o tsv)
-    certificatePolicyFile="${0%/*}/../templates/certificate-policy-$environment.json"
-    az keyvault certificate create --vault-name "$keyVault" --name "$certificateName" --policy "@$certificatePolicyFile"
-    thumbprintNew=$(az keyvault certificate show --name "$certificateName" --vault-name "$keyVault" --query "x509ThumbprintHex" -o tsv)
-    if [[ $thumbprintCurrent == $thumbprintNew ]]; then
+    thumbprintCurrent=$(az keyvault certificate show --name "${certificateName}" --vault-name "${keyVault}" --query "x509ThumbprintHex" -o tsv)
+    certificatePolicyFile="${0%/*}/../templates/${certificatePolicyPrefix}-${environment}.json"
+    az keyvault certificate create --vault-name "${keyVault}" --name "${certificateName}" --policy "@${certificatePolicyFile}"
+    thumbprintNew=$(az keyvault certificate show --name "${certificateName}" --vault-name "${keyVault}" --query "x509ThumbprintHex" -o tsv)
+    if [[ ${thumbprintCurrent} == "${thumbprintNew}" ]]; then
         echo "Error: Failure to create the certificate. Validate command output for details."
         exit 1
     else
-        echo "Created new version of $certificateName certificate with thumbprint $thumbprintNew"
+        echo "Created new version of ${certificateName} certificate with thumbprint ${thumbprintNew}"
     fi
 }
 
 # Read script arguments
-while getopts ":s:r:k:n:e:" option; do
-    case $option in
+while getopts ":s:r:k:n:e:p:" option; do
+    case ${option} in
     s) subscription=${OPTARG} ;;
     r) resourceGroupName=${OPTARG} ;;
     k) keyVault=${OPTARG} ;;
     n) certificateName=${OPTARG} ;;
     e) environment=${OPTARG} ;;
+    p) certificatePolicyPrefix=${OPTARG} ;;
     *) exitWithUsageInfo ;;
     esac
 done
 
-if [[ -z $resourceGroupName ]] || [[ -z $certificateName ]]; then
+if [[ -z ${resourceGroupName} ]] || [[ -z ${keyVault} ]]; then
     exitWithUsageInfo
 fi
 
-if [[ -z $environment ]]; then
-    environment="dev"
+if [[ -z ${environment} ]]; then
+    environment="prod"
 fi
 
 # Login to Azure if required
