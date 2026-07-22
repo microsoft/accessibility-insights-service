66ae510b67572e0378ec1ff583d66bf867e7bf73
Maxim Laikine <mlaikine@microsoft.com>
2026-07-22 16:37:31 -0700

    Update allowed audiences configuration based on environment

--- packages/resource-deployment/scripts/create-function-apps.sh
+++ packages/resource-deployment/scripts/create-function-apps.sh
@@ -67,7 +67,13 @@ getAllowedApplications() {
 }
 
 getAllowedAudiences() {
-    audienceFilePath="${0%/*}/../templates/web-api-aad-audience.txt"
+    if [[ ${environment} == prod* ]]; then
+        audienceFilePath="${0%/*}/../templates/web-api-aad-audience-prod.txt"
+    elif [[ ${environment} == ppe* ]]; then
+        audienceFilePath="${0%/*}/../templates/web-api-aad-audience-ppe.txt"
+    else
+        audienceFilePath="${0%/*}/../templates/web-api-aad-audience-${environment}.txt"
+    fi
 
     if [[ -f ${audienceFilePath} ]]; then
         echo "Using Azure Functions allowed audiences configuration ${audienceFilePath}"
@@ -77,7 +83,7 @@ getAllowedAudiences() {
     fi
 
     # The webApiIdentityClientId is the application (client) id of the user assigned managed identity (${webApiManagedIdentityName})
-    # used by the service. It is not part of the web-api-aad-audience.txt file and must be added implicitly so it remains a
+    # used by the service. It is not part of the web-api-aad-audience-*.txt file and must be added implicitly so it remains a
     # valid token audience for existing same-tenant clients.
     if [[ -z ${allowedAudiences} ]]; then
         allowedAudiences="${webApiIdentityClientId}"
